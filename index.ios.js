5b011de80a132ef7491155e86e73eb8272dd49d4
Pavlo Aksonov <aksonov@MacBook-Pro-Pavlo.local>
2015-11-19 17:39:09 +0100

    initial draft with StropheJS usage

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var React = require('react-native');
-var strophe = require("node-strophe").Strophe;
+var strophe = require("react-native-strophe").Strophe;
 var Strophe = strophe.Strophe;
 
 var {
@@ -15,25 +15,63 @@ var {
   View,
 } = React;
 
+function onMessage(msg){
+    console.log("!!!!!!!!!!!"+msg);
+}
+function onPresence(msg){
+    console.log("!!!!!!!!!!!"+msg);
+}
+var conn = null;
+var send_ping = function (to) {
+    var ping = $iq({
+        to: to,
+        type: "get",
+        id: "ping1"}).c("ping", {xmlns: "urn:xmpp:ping"});
+
+    console.log("Sending ping to " + to + ".");
+
+    conn.send(ping);
+
+    var msg = $msg({to:'pavel@beng.dev.tinyrobot.com',type:'chat'}).c('body').t("Hello world");
+    conn.send(msg);
+
+};
+var handle_pong = function (iq) {
+    console.log("Received pong from server");
+
+    conn.send($pres());
+
+    //Hello.connection.disconnect();
+
+    return false;
+};
+
 var Chat = React.createClass({
-  render: function() {
+    render: function() {
       //var conn = new Strophe.Connection(
       //    "https://beng.dev.tinyrobot.com:5281/http-bind");
       //console.log(conn);
       //
       //conn.connect("user1@beng.dev.tinyrobot.com", "user1", function (status) {
-      var conn = new Strophe.Connection(
-          "https://jabber.hot-chilli.net:5281/http-bind");
+      conn = new Strophe.Connection(
+          "ws://beng.dev.tinyrobot.com:5280/ws-xmpp");
       console.log(conn);
 
-      conn.connect("rntestuser1@jabber.hot-chilli.net", "rntestuser1", function (status) {
+      conn.connect("user2@beng.dev.tinyrobot.com", "user2", function (status) {
           console.log("STATUS:"+status);
           if (status === Strophe.Status.CONNECTED) {
               console.log('connected');
+              conn.addHandler(handle_pong, null, "iq", null, "ping1");
+              conn.addHandler(onPresence, null, 'presence');
+              conn.addHandler(onMessage, null, 'message', null, null);
+              var domain = Strophe.getDomainFromJid(conn.jid);
+              console.log("DOMAIN:"+domain+$pres()+conn);
+              conn.send($pres());
+              send_ping(domain);
           } else if (status === Strophe.Status.DISCONNECTED) {
               console.log('disconnected');
           }
-      }, 10);
+      });
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
