85d2b42b508a52671e305315bb05cad38fb808df
Pavlo Aksonov <aksonov@gmail.com>
2017-02-01 15:21:07 +0100

    fix header for profile details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {StyleSheet,View,TouchableHighlight,Text} from 'react-native';
 import {k, navBarTextColorDay, navBarTextColorNight} from '../globals';
+import location from '../store/location';
 
 export default class Header extends React.Component {
   render(){
     return <View style={{padding: 15*k, flex:1}}>
-        <Text style={{fontFamily:'Roboto-Medium',fontSize:16,color:this.props.isDay ? navBarTextColorDay : navBarTextColorNight }}>{this.props.children}</Text>
+        <Text style={{fontFamily:'Roboto-Medium',fontSize:16,color:location.isDay ? navBarTextColorDay : navBarTextColorNight }}>{this.props.children}</Text>
     </View>
   }
 }
