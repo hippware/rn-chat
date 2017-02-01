48858fbb250548ce1885b0a94e91e393d2d71551
Pavlo Aksonov <aksonov@gmail.com>
2017-02-01 15:31:58 +0100

    use unique ID for user and track image load

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -8,16 +8,32 @@ class Analytics {
     Mixpanel.sharedInstanceWithToken(settings.isStaging ? '5ee41c4ec134d9c7d769d9ddf41ed8eb': '3f62ffcf7a8fc0100157f877af5668a6');
     
     autorun(()=>{
-      if (model.profile && model.profile.loaded && model.profile.phoneNumber){
-        Mixpanel.identify(model.profile.phoneNumber);
-        Mixpanel.set({
-          "$email": model.profile.email,
-          "firstName": model.profile.firstName,
-          "lastName": model.profile.lastName,
-          "phone": model.profile.phoneNumber,
-          "username": model.profile.handle,
-        });
-        Mixpanel.track("profile loaded");
+      if (model.profile){
+        Mixpanel.identify(model.profile.user);
+        if (model.profile.loaded){
+          Mixpanel.set({
+            "$email": model.profile.email,
+            "firstName": model.profile.firstName,
+            "lastName": model.profile.lastName,
+            "phone": model.profile.phoneNumber,
+            "username": model.profile.handle,
+          });
+          Mixpanel.track("profile loaded");
+        } else {
+          Mixpanel.timeEvent("profile loaded");
+        }
+      }
+    });
+    
+    autorun(()=>{
+      if (model.profile && model.profile.avatar) {
+        if (!model.profile.avatar.loaded){
+          console.log("Profile image loading start");
+          Mixpanel.timeEvent("Profile Image Upload");
+        } else {
+          console.log("Profile image loading end");
+          Mixpanel.track("Profile Image Upload");
+        }
       }
     });
   }
