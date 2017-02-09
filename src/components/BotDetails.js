0149c42bf10f57d86d4bc255f4f78eeb882db3e9
Pavlo Aksonov <aksonov@gmail.com>
2017-02-09 16:30:22 +0100

    #371 bot CTA and distance calculation

--- src/components/BotDetails.js
+++ src/components/BotDetails.js
@@ -211,7 +211,18 @@ export default class extends React.Component {
         <View style={{paddingTop:15*k, paddingLeft:20*k, paddingRight:20*k, flexDirection: 'row', alignItems:'center'}}>
           <View style={{paddingRight:11*k}}><Avatar size={36} profile={profile} source={profile.avatar && profile.avatar.source}
                         title={profile.displayName} isDay={location.isDay} disableStatus borderWidth={0} /></View>
-          <Text style={{fontFamily:'Roboto-Italic', fontSize:13,letterSpacing:-0.1,color:'rgb(114,100,109)'}}>@{profile.handle}</Text>
+          <View style={{flex:1}}><Text style={{fontFamily:'Roboto-Italic', fontSize:13,letterSpacing:-0.1,color:'rgb(114,100,109)'}}>@{profile.handle}</Text></View>
+          {location.location && <View>
+            <Image source={require('../../images/buttonViewMapBG.png')}/>
+            <TouchableOpacity style={{position:'absolute',top:0,bottom:0,right:0,left:0, justifyContent:'center',alignItems:'center', flexDirection:'row', backgroundColor:'transparent'}}>
+              <View style={{paddingRight:5}}><Image source={require('../../images/iconBotLocation.png')}/></View>
+              <Text style={{fontFamily:'Roboto-Regular',fontSize:13,color:'rgb(63,50,77)'}}>
+                {location.distanceToString(
+                  location.distance(location.location.latitude, location.location.longitude, bot.location.latitude, bot.location.longitude)
+                )}
+                </Text>
+            </TouchableOpacity>
+          </View>}
         </View>
         <View style={{paddingTop:15*k, backgroundColor:location.isDay ? 'white' : 'rgba(49,37,62,0.90)'}}>
           {!!bot.description && <Header>Note</Header>}
