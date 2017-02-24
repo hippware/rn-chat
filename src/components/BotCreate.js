f7f0367612fce4eb40594a40b4c528f16c8c60ff
Pavlo Aksonov <aksonov@gmail.com>
2017-02-24 11:53:05 +0100

    #404 rework. Not all 'reset' function were removed.

--- src/components/BotCreate.js
+++ src/components/BotCreate.js
@@ -34,7 +34,7 @@ export default class extends React.Component {
     if (data){
       bot.bot = data;
     }
-    statem.createBot.save({reset: true });
+    statem.createBot.save();
   }
   
   componentWillUnmount(){
