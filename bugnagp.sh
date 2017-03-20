00117ce682b0974e0b53ca4736416cd04eaff23d
Pavlo Aksonov <aksonov@gmail.com>
2017-03-20 11:22:07 -0700

    revert unrelated change

--- bugnagp.sh
+++ bugnagp.sh
@@ -1,7 +1,7 @@
 react-native bundle --platform ios --entry-file index.ios.js --dev false --bundle-output ./ios/main.jsbundle --assets-dest ./ios --sourcemap-output ./sourcemap.js
 curl https://upload.bugsnag.com/ \
   -F apiKey=f108fb997359e5519815d5fc58c79ad3 \
-  -F appVersion=1.24.3 \
+  -F appVersion=1.21 \
   -F minifiedUrl="main.jsbundle" \
   -F sourceMap=@./sourcemap.js \
   -F minifiedFile=@./ios/main.jsbundle \
