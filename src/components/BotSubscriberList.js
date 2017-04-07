a879e1e1246161d28fd0ff528a26c6d9490f7c1f
aksonov <aksonov@gmail.com>
2017-04-07 11:21:36 +0200

    quick fix for #416

--- src/components/BotSubscriberList.js
+++ src/components/BotSubscriberList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {View, ListView, Text} from 'react-native';
+import {View, ListView, Text, TouchableOpacity} from 'react-native';
 import {observer} from 'mobx-react/native';
 import autobind from 'autobind-decorator';
 import Screen from './Screen';
@@ -9,6 +9,8 @@ import ProfileItem from './ProfileItem';
 import Separator from './Separator';
 import location from '../store/locationStore';
 import botStore from '../store/botStore';
+import statem from '../../gen/state';
+import {k} from '../globals';
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
@@ -23,12 +25,14 @@ export default class extends Component {
         const bot = botStore.bot;
         const subscribers = bot.subscribers.map(x => x).filter(x => x);
         const dataSource = ds.cloneWithRows(subscribers);
-        return <Screen>
+        return <Screen style={{paddingTop: 70 * k}}>
             <CardList isDay={location.isDay} keyboardShouldPersistTaps="always"
                       enableEmptySections={true}
                       dataSource={dataSource}
                       renderSeparator={(s, r) => <Separator key={r} width={1}/>}
-                      renderRow={row => <ProfileItem key={row.user + "row"} isDay={location.isDay} profile={row}/>}
+                      renderRow={row => <TouchableOpacity
+                          onPress={() => statem.logged.profileDetailsContainer({item: row.user})}><ProfileItem
+                          key={row.user + "row"} isDay={location.isDay} profile={row}/></TouchableOpacity>}
             />
         </Screen>
     }
