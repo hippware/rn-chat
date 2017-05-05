758f86321cec94928c5f416129ac40f55584084a
Eric Kirkham <eric@hippware.com>
2017-05-05 15:36:34 -0700

    Refactor and cleanup on BotCard, BotListView, and DataListView (no functional changes)

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Card from './Card';
-import CardText from './CardText';
-import BotAvatar from './BotAvatar';
 import {k} from './Global';
-import ResizedImage from './ResizedImage';
-import {Actions} from 'react-native-router-native';
-import Profile from '../model/Profile';
 import Bot from '../model/Bot';
 import {observer} from 'mobx-react/native';
 import location from '../store/locationStore';
 import BotCardInner from './BotCardInner';
 
-@observer
-export default class BotCard extends React.Component {
-    render() {
-        const isDay = location.isDay;
-        const bot: Bot = this.props.item;
-        return (
-            <Card
-                style={{paddingRight: 0, paddingLeft: 0, paddingTop: 1, paddingBottom: 5 * k}}
-                isDay={isDay}
-                onPress={() => this.props.onPress(bot)}
-                innerStyle={{
-                    paddingTop: 0 * k,
-                    paddingLeft: 0,
-                    paddingRight: 0,
-                    paddingBottom: 0 * k,
-                    height: 120 * k,
-                }}
-            >
-                <BotCardInner {...this.props} />
-            </Card>
-        );
-    }
-}
+type Props = {
+    item: Bot,
+    onPress: Function
+};
+
+const BotCard = (props: Props) => {
+    const {item, onPress} = props;
+    const isDay = location.isDay;
+    return (
+        <Card style={styles.card} isDay={isDay} onPress={() => onPress(item)} innerStyle={styles.inner}>
+            <BotCardInner {...props} />
+        </Card>
+    );
+};
+
+export default observer(BotCard);
+
+const styles = StyleSheet.create({
+    card: {
+        paddingRight: 0,
+        paddingLeft: 0,
+        paddingTop: 1,
+        paddingBottom: 5 * k,
+    },
+    inner: {
+        paddingTop: 0 * k,
+        paddingLeft: 0,
+        paddingRight: 0,
+        paddingBottom: 0 * k,
+        height: 120 * k,
+    },
+});
--- src/components/BotListView.js
+++ src/components/BotListView.js
@@ -1,39 +1,44 @@
-import React, {Component} from 'react';
-import {View, InteractionManager, StyleSheet, Image, Text, ListView} from 'react-native';
-const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+// @flow
+
+import React from 'react';
 import BotCard from './BotCard';
+import Bots from '../model/Bots';
 import {observer} from 'mobx-react/native';
-import {observable} from 'mobx';
 import statem from '../../gen/state';
 import model from '../model/model';
-import autobind from 'autobind-decorator';
-import {k, width, height} from './Global';
 import botStore from '../store/botStore';
 import DataListView from './DataListView';
+import {compose, withHandlers} from 'recompose';
 
-@autobind
-@observer
-export default class BotListView extends Component {
-    async loadMore() {
-        if (this.props.filter == 'all') {
-            await botStore.following(model.followingBots.earliestId);
-        } else {
-            await botStore.list(model.ownBots.earliestId);
-        }
-    }
+type Props = {
+    filter: string,
+    loadMore: Function
+};
 
-    render() {
-        this.bots = this.props.filter === 'all' ? model.followingBots : model.ownBots;
-        const bots = this.bots;
-        console.log('Bots render', this.props.filter, bots.finished);
-        return (
-            <DataListView
-                list={bots.list}
-                finished={bots.finished}
-                loadMore={this.loadMore}
-                footerImage={require('../../images/graphicEndBots.png')}
-                renderRow={row => <BotCard key={row.id} item={row} onPress={item => statem.botsScene.botDetails({item: item.id})} />}
-            />
-        );
-    }
-}
+const BotListView = ({filter, loadMore}:Props) => {
+    const bots: Bots = filter === 'all' ? model.followingBots : model.ownBots;
+    return (
+        <DataListView
+            list={bots.list}
+            finished={bots.finished}
+            loadMore={loadMore}
+            footerImage={require('../../images/graphicEndBots.png')}
+            renderRow={row => <BotCard key={row.id} item={row} onPress={item => statem.botsScene.botDetails({item: item.id})} />}
+        />
+    );
+};
+
+const enhance = compose(
+    observer,
+    withHandlers({
+        loadMore: ({filter}) => async () => {
+            if (filter === 'all') {
+                await botStore.following(model.followingBots.earliestId);
+            } else {
+                await botStore.list(model.ownBots.earliestId);
+            }
+        },
+    })
+);
+
+export default enhance(BotListView);
