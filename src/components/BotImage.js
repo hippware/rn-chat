42f3f1dec27f9a97b414464c697db7aa836d3633
Eric Kirkham <eric@hippware.com>
2017-05-08 10:55:39 -0700

    prefer functional component

--- src/components/BotImage.js
+++ src/components/BotImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Image, View, TouchableOpacity} from 'react-native';
+import {Image, View} from 'react-native';
 import {observer} from 'mobx-react/native';
-import {k, width, height, defaultCover} from './Global';
+import {k, defaultCover} from './Global';
 import Bot from '../model/Bot';
 import File from '../model/File';
 
@@ -10,31 +10,29 @@ type Props = {
     image?: File
 };
 
-@observer
-export default class extends React.Component {
-    props: Props;
-    render() {
-        const {bot, image} = this.props;
-        const source = (image && image.source) || (bot.image && bot.image.source) || (bot.thumbnail && bot.thumbnail.source);
-        return (
-            <View>
-                {source
-                    ? <Image
-                        resizeMode='contain'
-                        style={{
-                            width: 375 * k,
-                            height: 275 * k,
-                        }}
-                        source={source}
-                    />
-                    : <Image
-                        style={{
-                            width: 375 * k,
-                            height: 275 * k,
-                        }}
-                        source={defaultCover[bot.coverColor % 4]}
-                    />}
-            </View>
-        );
-    }
-}
+const BotImage = (props: Props) => {
+    const {bot, image} = props;
+    const source = (image && image.source) || (bot.image && bot.image.source) || (bot.thumbnail && bot.thumbnail.source);
+    return (
+        <View>
+            {source
+                ? <Image
+                    resizeMode='contain'
+                    style={{
+                        width: 375 * k,
+                        height: 275 * k,
+                    }}
+                    source={source}
+                />
+                : <Image
+                    style={{
+                        width: 375 * k,
+                        height: 275 * k,
+                    }}
+                    source={defaultCover[bot.coverColor % 4]}
+                />}
+        </View>
+    );
+};
+
+export default observer(BotImage);
