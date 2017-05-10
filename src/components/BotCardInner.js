dd6ac51dff5b8acddc1db8f41836bc9fa1642a92
Eric Kirkham <eric@hippware.com>
2017-05-10 11:58:37 -0700

    Observer on all components in BotCardInner

--- src/components/BotCardInner.js
+++ src/components/BotCardInner.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {k, defaultCover} from './Global';
@@ -9,45 +11,33 @@ import Avatar from './Avatar';
 import statem from '../../gen/state';
 import * as colors from '../constants/colors.js';
 
-const MainImage = ({item, source}: {item: Bot, source: any}) => (
-    <View style={{width: 120 * k, height: 120 * k}}>
-        <View style={{position: 'absolute'}}>
-            <Image style={{width: 120 * k, height: 120 * k}} source={source || defaultCover[item.coverColor % 4]} />
-            <View style={styles.innerWrapper}>
-                {item.image &&
-                    item.image.loaded &&
-                    <LinearGradient colors={['rgba(255,255,255,0)', 'rgba(0,0,0,0.75)']} style={{height: 50 * k, top: 0}} pointerEvents='none' />}
-                {item.imagesCount > 0 &&
-                    <View style={styles.image}>
-                        <Image source={require('../../images/iconPhotoSmall.png')} />
-                        <View style={{bottom: 2 * k, left: 2 * k}}>
-                            <Text style={styles.imagesCount}>
-                                {item.imagesCount}
-                            </Text>
-                        </View>
-                    </View>}
+const MainImage = observer(({item}: {item: Bot}) => {
+    const source = item.thumbnail && item.thumbnail.source;
+    return (
+        <View style={{width: 120 * k, height: 120 * k}}>
+            <View style={{position: 'absolute'}}>
+                <Image style={{width: 120 * k, height: 120 * k}} source={source || defaultCover[item.coverColor % 4]} />
+                <View style={styles.innerWrapper}>
+                    {item.image &&
+                        item.image.loaded &&
+                        <LinearGradient colors={['rgba(255,255,255,0)', 'rgba(0,0,0,0.75)']} style={{height: 50 * k, top: 0}} pointerEvents='none' />}
+                    {item.imagesCount > 0 &&
+                        <View style={styles.image}>
+                            <Image source={require('../../images/iconPhotoSmall.png')} />
+                            <View style={{bottom: 2 * k, left: 2 * k}}>
+                                <Text style={styles.imagesCount}>
+                                    {item.imagesCount}
+                                </Text>
+                            </View>
+                        </View>}
+                </View>
+
             </View>
-
         </View>
-    </View>
-);
+    );
+});
 
-const TheAvatar = ({profile, isDay}: {profile: Object, isDay: boolean}) => (
-    <View style={styles.avatar}>
-        <Avatar
-            size={30}
-            profile={profile}
-            source={profile.avatar && profile.avatar.source}
-            tappable
-            title={profile.displayName}
-            isDay={isDay}
-            disableStatus
-            borderWidth={0}
-        />
-    </View>
-);
-
-const UserName = ({profile}: {profile: Object}) => (
+const UserName = observer(({profile}: {profile: Object}) => (
     <TouchableOpacity
         onPress={() =>
             statem.logged.profileDetailsContainer({
@@ -60,28 +50,35 @@ const UserName = ({profile}: {profile: Object}) => (
             @{profile && profile.handle}
         </Text>
     </TouchableOpacity>
-);
+));
 
-const BottomLine = props => (
-    <View
-        style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-        }}
-    >
-        <Image style={{width: 15 * k, height: 16 * k}} source={require('../../images/iconSubSmall.png')} />
-        <Text style={styles.followersSize}>
-            {props.item.followersSize}
-        </Text>
-        <View style={{paddingLeft: 10 * k}}>
-            <Image style={{width: 14 * k, height: 17 * k}} source={require('../../images/iconBotLocation2.png')} />
+const BottomLine = observer(({item}: {item: Bot}) => {
+    const distance = location.location
+        ? location.distanceToString(
+              location.distance(location.location.latitude, location.location.longitude, item.location.latitude, item.location.longitude)
+          )
+        : null;
+    return (
+        <View
+            style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}
+        >
+            <Image style={{width: 15 * k, height: 16 * k}} source={require('../../images/iconSubSmall.png')} />
+            <Text style={styles.followersSize}>
+                {item.followersSize}
+            </Text>
+            <View style={{paddingLeft: 10 * k}}>
+                <Image style={{width: 14 * k, height: 17 * k}} source={require('../../images/iconBotLocation2.png')} />
+            </View>
+            <Text style={styles.distance}>
+                {distance}
+            </Text>
+            <UserName profile={item.owner} />
         </View>
-        <Text style={styles.distance}>
-            {props.distance}
-        </Text>
-        <UserName profile={props.profile} />
-    </View>
-);
+    );
+});
 
 type Props = {
     style: any,
@@ -90,26 +87,31 @@ type Props = {
 
 const BotCardInner = (props: Props) => {
     const {item, style} = props;
-    const isDay = location.isDay;
     const profile = item.owner;
-    const source = item.thumbnail && item.thumbnail.source;
-    const distance = location.location
-        ? location.distanceToString(
-              location.distance(location.location.latitude, location.location.longitude, item.location.latitude, item.location.longitude)
-          )
-        : null;
+
     return (
         <View style={[styles.container, style]}>
-            <MainImage {...props} source={source} />
+            <MainImage item={item} />
             <View style={styles.rightSide}>
                 <Text numberOfLines={1} style={styles.botTitle}>
                     {item.title}
                 </Text>
                 <View style={{flexDirection: 'row', flex: 1}}>
                     <Text numberOfLines={2} style={styles.smallText}>{item.address}</Text>
-                    <TheAvatar location={location} isDay={isDay} profile={profile} />
+                    <View style={styles.avatar}>
+                        <Avatar
+                            size={30}
+                            profile={item.owner}
+                            source={profile.avatar && profile.avatar.source}
+                            tappable
+                            title={profile.displayName}
+                            isDay={location.isDay}
+                            disableStatus
+                            borderWidth={0}
+                        />
+                    </View>
                 </View>
-                <BottomLine {...props} distance={distance} profile={profile} />
+                <BottomLine item={item} />
             </View>
         </View>
     );
