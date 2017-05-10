4d996a96caf68efdbb2ab0bf417d81e0f230e7e5
Eric Kirkham <eric@hippware.com>
2017-05-09 17:43:53 -0700

    BotShareCompleted: refactor, flex positioning and layout, number size adjusts

--- src/components/BotShareCompleted.js
+++ src/components/BotShareCompleted.js
@@ -1,61 +1,76 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {k, width, height} from './Global';
 import {Actions} from 'react-native-router-native';
 import Avatar from './Avatar';
 import profileFactory from '../factory/profileFactory';
+import * as colors from '../constants/colors';
+
+type Props = {
+    number: number,
+    user: any
+};
 
 export default class extends React.Component {
+    props: Props;
+
     componentDidMount() {
-        setTimeout(Actions.pop, 3000);
+        // setTimeout(Actions.pop, 3000);
     }
 
     render() {
-        const profile = profileFactory.create(this.props.user);
+        const {number, user} = this.props;
+        const profile = profileFactory.create(user);
+        const friendString = number > 1 ? 'friends' : 'friend';
         return (
-            <View
-                style={{
-                    width,
-                    height,
-                    backgroundColor: 'transparent',
-                    paddingTop: 175 * k,
-                    paddingRight: 82 * k,
-                    paddingLeft: 82 * k,
-                }}
-            >
-                <View
-                    style={{
-                        backgroundColor: 'rgba(255,255,255,0.9)',
-                        height: 80 * k,
-                        borderRadius: 74 * k,
-                        flexDirection: 'row',
-                    }}
-                >
-                    {this.props.number > 1 &&
-                        <View style={{position: 'absolute', top: 20 * k, left: 50 * k}}>
-                            <Avatar size={40 * k} text={'+' + (this.props.number - 1)} />
+            <View style={styles.container}>
+                <View style={styles.inner}>
+                    <Avatar size={40 * k} profile={profile} />
+                    {number > 1 &&
+                        <View style={styles.numberBubble}>
+                            <Avatar size={40 * k} text={`+${number - 1}`} smallFont={number > 9} />
                         </View>}
-                    <View style={{position: 'absolute', top: 20 * k, left: 20 * k}}>
-                        <Avatar size={40 * k} profile={profile} />
-                    </View>
-                    <View style={{position: 'absolute', top: 20 * k, left: 110 * k, width: 95 * k}}>
-                        <Text
-                            style={{
-                                color: 'rgb(63,50,77)',
-                                fontSize: 14 * k,
-                                fontFamily: 'Roboto-Medium',
-                            }}
-                        >
-                            Bot Shared
-                            {' '}
+                    <View style={styles.textContainer}>
+                        <Text style={styles.botSharedText}>
+                            {'Bot Shared '}
+                        </Text>
+                        <Text style={styles.botSharedText}>
                             <Text style={{fontFamily: 'Roboto-Regular'}}>with </Text>
-                            {this.props.number + ' '}
-                            friend{this.props.number > 1 ? 's' : ''}!
+                            {`${number} ${friendString}!`}
                         </Text>
                     </View>
-
                 </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width,
+        height,
+        backgroundColor: 'transparent',
+        paddingTop: 175 * k,
+        alignItems: 'center',
+    },
+    inner: {
+        backgroundColor: colors.hexToRgba(colors.WHITE, 0.9),
+        height: 80 * k,
+        borderRadius: 74 * k,
+        flexDirection: 'row',
+        padding: 20 * k,
+        paddingRight: 30 * k,
+    },
+    numberBubble: {
+        marginLeft: -10 * k,
+        zIndex: -1,
+    },
+    textContainer: {
+        marginLeft: 7 * k,
+    },
+    botSharedText: {
+        color: colors.PURPLE,
+        fontSize: 14 * k,
+        fontFamily: 'Roboto-Medium',
+    },
+});
