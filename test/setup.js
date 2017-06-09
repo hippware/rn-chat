7dbff4be45dd908c450178bac9fa77615b7cf491
aksonov <aksonov@gmail.com>
2017-06-09 10:44:31 +0200

    2-space format, fix unit test

--- test/setup.js
+++ test/setup.js
@@ -11,54 +11,55 @@ global.fs = require('fs');
 global.fs.unlink = fs.unlinkSync;
 global.tempDir = require('os').tmpdir();
 global.downloadHttpFile = async function (urlString, fileName, headers) {
-    const URL = require('url');
-    const http = require('http');
-    const url = URL.parse(urlString);
+  const URL = require('url');
+  const http = require('http');
+  const url = URL.parse(urlString);
 
-    return new Promise((resolve, reject) => {
-        const request = http
-            .get(
-            {
-                host: url.hostname,
-                port: url.port,
-                path: url.path,
-                headers,
-            },
-                function (response) {
-                    console.log('RESPONSE:', response.statusCode);
-                    if (response.statusCode !== 200) {
-                        reject(`Invalid status code ${response.statusCode}`);
-                        return;
-                    }
-                    const file = fs.createWriteStream(fileName);
-                    response.pipe(file);
-                    file.on('finish', function () {
-                        file.close(resolve(fileName)); // close() is async, call cb after close completes.
-                    });
-                }
-            )
-            .on('error', function (err) {
-                // Handle errors
-                reject(err.message);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    const request = http
+      .get(
+      {
+        host: url.hostname,
+        port: url.port,
+        path: url.path,
+        headers,
+      },
+        function (response) {
+          console.log('RESPONSE:', response.statusCode);
+          if (response.statusCode !== 200) {
+            reject(`Invalid status code ${response.statusCode}`);
+            return;
+          }
+          const file = fs.createWriteStream(fileName);
+          response.pipe(file);
+          file.on('finish', function () {
+            console.log('FINISHED');
+            file.close(resolve(fileName)); // close() is async, call cb after close completes.
+          });
+        }
+      )
+      .on('error', function (err) {
+        // Handle errors
+        reject(err.message);
+      });
+  });
 };
 var denodeify = require('denodeify');
 global.readFile = denodeify(fs.readFile);
 global.writeFile = denodeify(fs.writeFile);
 global.mkdir = denodeify(fs.mkdir);
 global.fileExists = async function (filePath) {
-    return new Promise((resolve, reject) => {
-        fs.stat(filePath, (error, stat) => {
-            if (error) {
-                resolve(false);
-            } else {
-                console.log('CACHED:', filePath);
-                resolve(true);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    fs.stat(filePath, (error, stat) => {
+      if (error) {
+        resolve(false);
+      } else {
+        console.log('CACHED:', filePath);
+        resolve(true);
+      }
     });
+  });
 };
-global.getImageSize = uri => new Promise((resolve, reject) => {});
+global.getImageSize = uri => new Promise((resolve, reject) => resolve({}));
 
 global.__DEV__ = true;
