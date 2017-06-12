d4dc384e2e686925b4f5f4f15df7bcbcce72f62a
aksonov <aksonov@gmail.com>
2017-06-12 15:09:31 +0200

    hotfix for loading launch screen

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {View, Image, StyleSheet} from 'react-native';
-import {k} from './Global';
+import {width, height, k} from './Global';
 import * as Progress from 'react-native-progress';
 import {colors} from '../constants';
 
 export default props => (
   <View style={{flex: 1, backgroundColor: 'transparent'}}>
     <View style={styles.container}>
-      <Image style={styles.backgroundImage} source={require('../../images/Launch.gif')} resizeMode='cover' />
+      <Image style={{width, height}} source={require('../../images/Launch.gif')} />
+      <Progress.CircleSnail size={26 * k} thickness={2} style={{position: 'absolute', top: 556 * k}} color={colors.PINK} />
     </View>
-    <Progress.CircleSnail size={26 * k} thickness={2} style={{top: 556 * k}} color={colors.PINK} />
     {props.children}
   </View>
 );
