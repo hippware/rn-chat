9f2564d2d69f34727ee6568d4ab5890d48b127b4
Eric Kirkham <eric@hippware.com>
2017-06-14 14:28:10 -0700

    stateless SignUpAvatar

--- src/components/SignUpAvatar.js
+++ src/components/SignUpAvatar.js
@@ -1,51 +1,50 @@
-import React, {Component} from 'react';
-import {Image, View, TouchableOpacity, NativeModules} from 'react-native';
+import React from 'react';
+import {Image, TouchableOpacity} from 'react-native';
 import {k} from './Global';
 import profile from '../store/profileStore';
-import autobind from 'autobind-decorator';
 import showImagePicker from './ImagePicker';
 import {observer} from 'mobx-react/native';
+import {compose, withState} from 'recompose';
 
-@autobind
-@observer
-export default class SignUpAvatar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  onPhotoAdd() {
-    showImagePicker('Select Avatar', (source, response) => {
-      profile.uploadAvatar({
-        file: source,
-        width: response.width,
-        height: response.height,
-        size: response.fileSize,
-      });
-      this.setState({source});
-    });
-  }
-
-  render() {
-    const avatar = this.state.source || (this.props.avatar && this.props.avatar.source) || require('../../images/addPhoto.png');
-    return (
-      <TouchableOpacity style={{alignItems: 'center'}} onPress={this.onPhotoAdd}>
-        <Image
-            style={[
-              {
-                width: 82 * k,
-                height: 80 * k,
-                borderRadius: 40 * k,
-                borderColor: 'white',
-              },
-              this.props.style,
-            ]}
-            source={avatar}
-        />
-      </TouchableOpacity>
-    );
-  }
-}
-
-SignUpAvatar.propTypes = {
-  avatar: React.PropTypes.any,
+type Props = {
+  avatar?: Object,
+  source: Object,
+  setSource: Function,
+  style?: Object
 };
+
+const SignUpAvatar = ({source, setSource, avatar, style}: Props) => {
+  const theAvatar = source || (avatar && avatar.source) || require('../../images/addPhoto.png');
+  return (
+    <TouchableOpacity
+        style={{alignItems: 'center'}}
+        onPress={() =>
+        showImagePicker('Select Avatar', (src, response) => {
+          profile.uploadAvatar({
+            file: src,
+            width: response.width,
+            height: response.height,
+            size: response.fileSize,
+          });
+          setSource(src);
+        })}
+    >
+      <Image
+          style={[
+            {
+              width: 82 * k,
+              height: 80 * k,
+              borderRadius: 40 * k,
+            },
+            style,
+          ]}
+          source={theAvatar}
+          resizeMode='cover'
+      />
+    </TouchableOpacity>
+  );
+};
+
+const enhance = compose(observer, withState('source', 'setSource', null));
+
+export default enhance(SignUpAvatar);
