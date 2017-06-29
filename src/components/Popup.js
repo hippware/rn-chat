a19acc3ee0f081182262bb39a44167a79485c20a
Eric Kirkham <eric@hippware.com>
2017-06-28 17:00:54 -0700

    refactor and cleanup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,57 +1,60 @@
+// @flow
+
 import React from 'react';
-import {ScrollView, Image, View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
+import {ScrollView, Image, View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Actions} from 'react-native-router-native';
 import {k, width, height} from './Global';
 
-export default class extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View
-            style={{
-              position: 'absolute',
-              right: 15 * k,
-              left: 15 * k,
-              top: 40 * k,
-              bottom: 40 * k,
-            }}
-        >
-          <View
+type Props = {
+  title: string,
+  children?: any
+};
+
+export default ({title, children}: Props) => (
+  <View style={styles.container}>
+    <View
+        style={{
+          position: 'absolute',
+          right: 15 * k,
+          left: 15 * k,
+          top: 40 * k,
+          bottom: 40 * k,
+        }}
+    >
+      <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            borderRadius: 2 * k,
+            opacity: 0.90,
+            backgroundColor: 'white',
+          }}
+      >
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.title}>{title}</Text>
+          <TouchableOpacity
               style={{
-                position: 'absolute',
-                top: 0,
-                bottom: 0,
-                right: 0,
-                left: 0,
-                borderRadius: 2 * k,
-                opacity: 0.90,
-                backgroundColor: 'white',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: 40 * k,
+                height: 50 * k,
               }}
+              onPress={() => Actions.pop()}
           >
-            <View style={{flexDirection: 'row'}}>
-              <Text style={styles.title}>{this.props.title}</Text>
-              <TouchableOpacity
-                  style={{
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: 40 * k,
-                    height: 50 * k,
-                  }}
-                  onPress={() => Actions.pop()}
-              >
-                <Image style={{}} source={require('../../images/iconClose.png')} />
-              </TouchableOpacity>
-            </View>
-            <View style={{height: 1 * k, backgroundColor: 'rgba(155,155,155,0.15)'}} />
-            <ScrollView style={{paddingLeft: 21 * k, paddingRight: 21 * k}}>
-              {this.props.children}
-            </ScrollView>
-          </View>
+            <Image style={{}} source={require('../../images/iconClose.png')} />
+          </TouchableOpacity>
         </View>
+        <View style={{height: 1 * k, backgroundColor: 'rgba(155,155,155,0.15)'}} />
+        <ScrollView style={{paddingLeft: 21 * k, paddingRight: 21 * k}}>
+          {children}
+        </ScrollView>
       </View>
-    );
-  }
-}
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
