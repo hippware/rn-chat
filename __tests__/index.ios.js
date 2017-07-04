7dc1030c073d38791847bcdca159bfe243fd2052
Eric Kirkham <eric@hippware.com>
2017-07-03 17:40:18 -0700

    working my way up to testing BotInfo

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,15 +1,18 @@
 // @flow
 
-import {NativeModules} from 'react-native';
+// import {NativeModules} from 'react-native';
+import 'react-native';
 import React from 'react';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 // @TODO: mocks
-NativeModules.RNCarrierInfo = {isoCountryCode: jest.fn()};
+// NativeModules.RNCarrierInfo = {isoCountryCode: jest.fn()};
 
 import AddBot from '../src/components/BotDetails/AddBot';
+// import BotInfo from '../src/components/BotInfo';
+import Card from '../src/components/Card';
 
 describe('Testing jest', () => {
   // test('renders correctly', () => {
@@ -27,7 +30,58 @@ describe('Testing jest', () => {
 
   test('AddBot', () => {
     const tree = renderer.create(<AddBot />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Card', () => {
+    const tree = renderer.create(<Card />).toJSON();
     // const tree = renderer.create(<SideMenu />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  // jest.mock('Linking', () => {
+  //   return {
+  //     addEventListener: jest.fn(),
+  //     removeEventListener: jest.fn(),
+  //     openURL: jest.fn(),
+  //     canOpenURL: jest.fn(),
+  //     getInitialURL: jest.fn(),
+  //   };
+  // });
+  //
+  // jest.mock('NetInfo', () => {
+  //   return {
+  //     isConnected: {
+  //       fetch: () => {
+  //         return new Promise((accept, resolve) => {
+  //           accept(true);
+  //         });
+  //       },
+  //     },
+  //   };
+  // });
+  //
+  // jest.mock('PushNotificationIOS', () => {
+  //   return {
+  //     getInitialNotification: () => {
+  //       return new Promise((accept, resolve) => {
+  //         accept(true);
+  //       });
+  //     },
+  //   };
+  // });
+  // //
+  // jest.mock(
+  //   'NativeEventEmitter',
+  //   () =>
+  //     class NEE {
+  //       constructor() {}
+  //       addListener = jest.fn();
+  //     }
+  // );
+  //
+  // test('BotInfo', () => {
+  //   const tree = renderer.create(<BotInfo />).toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
 });
