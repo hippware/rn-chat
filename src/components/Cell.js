c9500a054195225a802028e9fc0295cd75fc7c10
Eric Kirkham <eric@hippware.com>
2017-08-07 16:15:04 -0700

    wip on #933. needs style tweaks

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,69 +1,79 @@
+// @flow
+
 import React from 'react';
-import {Image, StyleSheet, View, TouchableOpacity, TouchableHighlight, Text} from 'react-native';
-import styles from './styles';
+import {Image, StyleSheet, View, TouchableOpacity, Text} from 'react-native';
 import {k, navBarTextColorDay, navBarTextColorNight} from '../globals';
 import location from '../store/locationStore';
 import {observer} from 'mobx-react/native';
 
-@observer
-export default class Cell extends React.Component {
-  render() {
-    const cell = (
-      <View style={[{flexDirection: 'row', alignItems: 'center', padding: 15 * k}, this.props.style]}>
-        {this.props.image &&
-          <View
+type Props = {
+  style: ?Object,
+  imageStyle: ?Object,
+  textStyle: ?Object,
+  image: any,
+  children: any,
+  onRemove: ?Function,
+  onPress: ?Function,
+};
+
+const Cell = ({style, imageStyle, textStyle, image, children, onRemove, onPress}) => {
+  const cell = (
+    <View style={[{flexDirection: 'row', alignItems: 'center', padding: 15 * k}, style]}>
+      {image &&
+        <View
+          style={[
+            {
+              width: 15 * k,
+              paddingLeft: 5 * k,
+              paddingRight: 15 * k,
+              alignItems: 'center',
+            },
+            imageStyle,
+          ]}
+        >
+          <Image source={image} />
+        </View>}
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {typeof children === 'string' &&
+          <Text
+            numberOfLines={1}
             style={[
               {
-                width: 15 * k,
-                paddingLeft: 5 * k,
-                paddingRight: 15 * k,
-                alignItems: 'center',
+                flex: 1,
+                fontFamily: 'Roboto-Regular',
+                fontSize: 15,
+                color: location.isDay ? navBarTextColorDay : navBarTextColorNight,
               },
-              this.props.imageStyle,
+              textStyle,
             ]}
           >
-            <Image source={this.props.image} />
-          </View>}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {typeof this.props.children === 'string' &&
-            <Text
-              numberOfLines={1}
-              style={[
-                {
-                  flex: 1,
-                  fontFamily: 'Roboto-Regular',
-                  fontSize: 15,
-                  color: location.isDay ? navBarTextColorDay : navBarTextColorNight,
-                },
-                this.props.textStyle,
-              ]}
-            >
-              {this.props.children}
-            </Text>}
-          {typeof this.props.children !== 'string' && this.props.children}
-        </View>
-        {this.props.onRemove &&
-          <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center'}} onPress={this.props.onRemove}>
-            <Image source={require('../../images/iconCloseSmall.png')} />
-          </TouchableOpacity>}
+            {children}
+          </Text>}
+        {typeof children !== 'string' && children}
       </View>
-    );
+      {onRemove &&
+        <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center'}} onPress={onRemove}>
+          <Image source={require('../../images/iconCloseSmall.png')} />
+        </TouchableOpacity>}
+    </View>
+  );
 
-    if (this.props.onPress) {
-      return (
-        <TouchableOpacity {...this.props} style={[this.props.style]}>
-          {cell}
-        </TouchableOpacity>
-      );
-    } else {
-      return cell;
-    }
+  if (onPress) {
+    return (
+      <TouchableOpacity onPress={onPress} style={style}>
+        {cell}
+      </TouchableOpacity>
+    );
+  } else {
+    return cell;
   }
-}
+};
+
+export default observer(Cell);
