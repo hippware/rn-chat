fb4ffd0d0a079e3b5be0f12c5d49de861fa100af
Eric Kirkham <eric@hippware.com>
2017-08-08 15:06:23 -0700

    extract SearchBar to separate component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+// @flow
+
+import React from 'react';
+import {Image, StyleSheet, View, TextInput} from 'react-native';
+import {k} from './Global';
+import {colors} from '../constants';
+
+type Props = {
+  onChangeText: Function,
+  value: string,
+  style: ?Object,
+  // image: ?React.ImageSource,
+  image: any,
+};
+
+const SearchBar = (props: Props) =>
+  (<View style={styles.searchBar}>
+    <Image source={props.image || require('../../images/iconFriendsSearch.png')} style={{margin: 5 * k, height: 12 * k}} resizeMode='contain' />
+    <TextInput style={[{width: 200 * k, fontFamily: 'Roboto-Light', fontSize: 14 * k, margin: 5 * k}, props.style]} {...props} />
+  </View>);
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  searchBar: {
+    marginHorizontal: 10 * k,
+    marginBottom: 5 * k,
+    backgroundColor: colors.LIGHT_GREY,
+    borderRadius: 2 * k,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+});
