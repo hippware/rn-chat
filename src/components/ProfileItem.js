2e779af7b10396a28f9ffc707fc77bf251cc47b2
Eric Kirkham <eric@hippware.com>
2017-08-08 15:06:36 -0700

    flow type ProfileItem

--- src/components/ProfileItem.js
+++ src/components/ProfileItem.js
@@ -1,58 +1,60 @@
-import React, {Component} from 'react';
-import {TouchableOpacity, Image, StyleSheet, ListView, View, Text} from 'react-native';
+// @flow
+
+import React from 'react';
+import {Image, View, Text} from 'react-native';
 import Avatar from './Avatar';
 import ProfileNameText from './ProfileNameText';
 import {k} from './Global';
 import {observer} from 'mobx-react/native';
+import Profile from '../model/Profile';
 
-@observer
-export default class ProfileItem extends Component {
-  render() {
-    const {profile, selected, isDay} = this.props;
-    const displayName = profile.displayName;
-    return (
-      <View
-        style={[
-          {
-            flex: 1,
-            flexDirection: 'row',
-            padding: 3 * k,
-            paddingRight: 0,
-            alignItems: 'center',
-          },
-          this.props.style,
-        ]}
-      >
-        <View style={{padding: 5 * k}}>
-          <Avatar size={40} profile={profile} isDay={isDay} />
-        </View>
-        <View style={{flex: 1, padding: 7 * k}}>
-          <ProfileNameText isDay={isDay}>
-            @{profile.handle}
-          </ProfileNameText>
-          <Text
-            isDay={isDay}
-            style={{
-              color: 'rgb(194,194,194)',
-              fontFamily: 'Roboto-Medium',
-              fontSize: 15 * k,
-            }}
-          >
-            {profile.displayName}
-          </Text>
-        </View>
-        {selected !== undefined &&
-          <View style={{width: 40 * k, padding: 10 * k}}>
-            <Image style={{right: 20 * k}} source={selected ? require('../../images/contactSelect.png') : require('../../images/addContactUnselectedV2.png')} />
-          </View>}
-        {this.props.children}
-      </View>
-    );
-  }
-}
-
-ProfileItem.propTypes = {
-  profile: React.PropTypes.any.isRequired,
-  isDay: React.PropTypes.bool.isRequired,
-  selected: React.PropTypes.bool,
+type Props = {
+  profile: Profile,
+  isDay: boolean,
+  selected: boolean,
+  style: ?Object,
+  children: any,
 };
+
+const ProfileItem = ({profile, selected, isDay, style, children}: Props) => {
+  return (
+    <View
+      style={[
+        {
+          flex: 1,
+          flexDirection: 'row',
+          padding: 3 * k,
+          paddingRight: 0,
+          alignItems: 'center',
+        },
+        style,
+      ]}
+    >
+      <View style={{padding: 5 * k}}>
+        <Avatar size={40} profile={profile} isDay={isDay} />
+      </View>
+      <View style={{flex: 1, padding: 7 * k}}>
+        <ProfileNameText isDay={isDay}>
+          @{profile.handle}
+        </ProfileNameText>
+        <Text
+          isDay={isDay}
+          style={{
+            color: 'rgb(194,194,194)',
+            fontFamily: 'Roboto-Medium',
+            fontSize: 15 * k,
+          }}
+        >
+          {profile.displayName}
+        </Text>
+      </View>
+      {selected !== undefined &&
+        <View style={{width: 40 * k, padding: 10 * k}}>
+          <Image style={{right: 20 * k}} source={selected ? require('../../images/contactSelect.png') : require('../../images/addContactUnselectedV2.png')} />
+        </View>}
+      {children}
+    </View>
+  );
+};
+
+export default observer(ProfileItem);
