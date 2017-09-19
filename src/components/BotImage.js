7af304061ee0fcd8446a010a68d8c43a2c0e08ad
Eric Kirkham <eric@hippware.com>
2017-09-19 16:21:02 -0700

    cleanup, refactor

--- src/components/BotImage.js
+++ src/components/BotImage.js
@@ -1,31 +1,21 @@
 // @flow
 
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Image} from 'react-native';
 import {observer} from 'mobx-react/native';
-import {k, defaultCover, width} from './Global';
+import {defaultCover, width} from './Global';
 import Bot from '../model/Bot';
 import File from '../model/File';
-import {colors} from '../constants';
 
 type Props = {
   bot: Bot,
   image?: File,
-  filter?: boolean,
 };
 
 const BotImage = (props: Props) => {
   const {bot, image} = props;
-  const source = image ? image.source : bot.image && bot.image.source;
-  if (source) {
-    return (
-      <View style={{flex: 1}}>
-        <Image style={{height: width, width}} source={source} resizeMode='contain' />
-      </View>
-    );
-  } else {
-    return <Image style={{height: width, width}} source={defaultCover[bot.coverColor % 4]} resizeMode='contain' />;
-  }
+  const source = (image ? image.source : bot.image && bot.image.source) || defaultCover[bot.coverColor % 4];
+  return <Image style={{height: width, width}} source={source} resizeMode='contain' />;
 };
 
 export default observer(BotImage);
--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -14,6 +14,7 @@ type Props = {
 @observer
 export default class EventCard extends React.Component {
   props: Props;
+  card: any;
 
   render() {
     const isDay = location.isDay;
@@ -28,7 +29,7 @@ export default class EventCard extends React.Component {
       <Card
         key={row.event.id}
         isDay={isDay}
-        onPress={() => this.refs.card.onPress && this.refs.card.onPress()}
+        onPress={() => this.card.onPress && this.card.onPress()}
         style={{
           paddingTop: 10 * k,
           paddingLeft: 0,
@@ -36,7 +37,7 @@ export default class EventCard extends React.Component {
           paddingBottom: 0,
         }}
       >
-        <CardClass ref='card' item={event} />
+        <CardClass ref={r => (this.card = r)} item={event} />
       </Card>
     );
   }
