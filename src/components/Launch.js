7a3e581d401bfcc965e7d4cc5e2c06e716800a31
aksonov <aksonov@gmail.com>
2017-10-10 16:03:13 +0200

    hot-fix: remove progress bar that adds a lot of CPU usage

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {View, Image, StyleSheet} from 'react-native';
 import {width, height, k} from './Global';
-import * as Progress from 'react-native-progress';
 import {colors} from '../constants';
 
 export default props =>
   (<View style={[styles.container, {flex: 1, backgroundColor: 'transparent'}]}>
     <View style={styles.container}>
       <Image style={{width, height}} source={require('../../images/Launch.gif')} />
-      <Progress.CircleSnail size={26 * k} thickness={2} style={{position: 'absolute', top: 556 * k}} color={colors.PINK} />
     </View>
     {props.children}
   </View>);
