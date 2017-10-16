3f4e3e8806ce89da9de9df4e285eda789fa26d4b
Eric Kirkham <eric@hippware.com>
2017-10-16 13:20:23 -0700

    account for no 'onPress' in ProfileHandle.

--- src/components/common/ProfileHandle.js
+++ src/components/common/ProfileHandle.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import {Image, TouchableOpacity} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import {k} from '../Global';
 import * as colors from '../../constants/colors';
 import {observer} from 'mobx-react/native';
@@ -19,14 +19,17 @@ type Props = {
 const ProfileHandle = (props: Props) => {
   const {onPress, profile, style, size, textStyle} = props;
   const theSize = size || 13;
-  return (
-    <TouchableOpacity onPress={onPress} style={[{flexDirection: 'row', alignItems: 'center'}, style]}>
+
+  const inner = (
+    <View style={[{flexDirection: 'row', alignItems: 'center'}, style]}>
       <RText color={colors.DARK_PURPLE} weight='Medium' size={theSize} style={textStyle}>
         @{profile.handle}
       </RText>
       {profile.isVerified && <Image source={require('../../../images/verifiedIcon.png')} style={{marginLeft: 2 * k}} resizeMode='contain' />}
-    </TouchableOpacity>
+    </View>
   );
+
+  return onPress ? <TouchableOpacity onPress={onPress}>{inner}</TouchableOpacity> : inner;
 };
 
 export default observer(ProfileHandle);
