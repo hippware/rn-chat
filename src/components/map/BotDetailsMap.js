7da989fcad14ee4d2fff808c4be9366df7bfbd84
Eric Kirkham <eric@hippware.com>
2017-10-23 17:46:18 -0700

    for #1409

--- src/components/map/BotDetailsMap.js
+++ src/components/map/BotDetailsMap.js
@@ -2,10 +2,8 @@
 
 import React from 'react';
 import Map from './Map';
-import {TouchableWithoutFeedback, View} from 'react-native';
 import Bot from '../../model/Bot';
-import {Actions} from 'react-native-router-flux';
-import {k, width, height, defaultCover} from '../Global';
+import {k, defaultCover} from '../Global';
 import Bubble from './Bubble';
 import MapView from 'react-native-maps';
 
@@ -19,19 +17,22 @@ type Props = {
 export default ({bot, scale, ...props}: Props) => {
   const fullMap = scale === 0;
   const y = scale === 1 ? 0 : fullMap ? -35 : -106;
-  return (<Map
-    location={bot.location}
-    showOnlyBot
-    showUser={fullMap}
-    fullMap={fullMap}
-    scale={scale}
-    scrollEnabled={fullMap}
-    rotateEnabled={fullMap}
-    pitchEnabled={fullMap}
-    marker={<MapView.Marker centerOffset={{x: 0, y}} identifier='marker' coordinate={{latitude: bot.location.latitude, longitude: bot.location.longitude}}>
-      <Bubble {...props} text={bot.address} scale={scale} image={bot.image && bot.image.source ? bot.image.source : defaultCover[bot.coverColor % 4]} />
-    </MapView.Marker>}
-    {...props}
-  />);
+  return (
+    <Map
+      location={bot.location}
+      showOnlyBot
+      showUser={fullMap}
+      fullMap={fullMap}
+      scale={scale}
+      scrollEnabled={fullMap}
+      rotateEnabled={fullMap}
+      pitchEnabled={fullMap}
+      marker={
+        <MapView.Marker centerOffset={{x: 0, y}} identifier='marker' coordinate={{latitude: bot.location.latitude, longitude: bot.location.longitude}}>
+          <Bubble {...props} text={bot.addressData.locationShort} scale={scale} image={bot.image && bot.image.source ? bot.image.source : defaultCover[bot.coverColor % 4]} />
+        </MapView.Marker>
+      }
+      {...props}
+    />
+  );
 };
-
