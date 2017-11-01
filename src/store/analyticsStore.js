cd135dd68d0b8ff543570fbada4e3f985f6a50a1
Eric Kirkham <eric@hippware.com>
2017-11-01 12:37:21 -0700

    analyticsStore

---
+++ src/store/analyticsStore.js
@@ -0,0 +1,66 @@
+// @flow
+
+// mixpanel JS API docs: https://mixpanel.com/help/reference/javascript
+
+import {settings} from '../globals';
+import {when} from 'mobx';
+import model from '../model/model';
+import * as log from '../utils/log';
+
+let Mixpanel;
+try {
+  Mixpanel = require('react-native-mixpanel');
+} catch (e) {
+  log.log(`No mixpanel ${e}`);
+}
+
+class AnalyticsStore {
+  inSession: boolean = false;
+
+  start = (): void => {
+    Mixpanel.sharedInstanceWithToken(settings.isStaging ? '5ee41c4ec134d9c7d769d9ddf41ed8eb' : '3f62ffcf7a8fc0100157f877af5668a6');
+
+    // TODO: modify to handle logout/login of different users in the same session?
+    when(
+      () => model.profile,
+      () => {
+        Mixpanel.identify(model.profile.user);
+        Mixpanel.set({
+          $email: model.profile.email,
+          $first_name: model.profile.firstName,
+          $last_name: model.profile.lastName,
+          phone: model.profile.phoneNumber,
+          username: model.profile.handle,
+        });
+        this.sessionStart();
+      },
+    );
+  };
+
+  track = (name: string, properties: ?Object): void => {
+    try {
+      if (!properties) {
+        log.log('& track', name);
+        Mixpanel.track(name);
+      } else {
+        log.log('& track with props', name, properties);
+        Mixpanel.trackWithProperties(name, properties);
+      }
+    } catch (err) {
+      log.log('Mixpanel tracking error', err);
+    }
+  };
+
+  sessionStart = () => {
+    if (this.inSession) return;
+    this.inSession = true;
+    Mixpanel.timeEvent('session');
+  };
+
+  sessionEnd = () => {
+    Mixpanel.track('session');
+    this.inSession = false;
+  };
+}
+
+export default new AnalyticsStore();
