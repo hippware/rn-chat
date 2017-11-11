e2426ae3bbf19bf898b4246cecbef0e7ac5924c9
Eric Kirkham <eric@hippware.com>
2017-11-10 17:36:56 -0800

    wip RN 0.50.0 upgrade

--- android/app/src/main/java/com/tinyrobot/MainApplication.java
+++ android/app/src/main/java/com/tinyrobot/MainApplication.java
@@ -43,6 +43,11 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactNativeMapboxGLPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
