87e9f33fa8f76065e64d66e44d9137fa7c438e4e
Eric Kirkham <eric@hippware.com>
2017-11-27 14:45:40 -0800

    prevent bugsnags in dev. Add user info when possible.

--- src/utils/errorReporting.js
+++ src/utils/errorReporting.js
@@ -3,15 +3,20 @@
 
 import NativeEnv from 'react-native-native-env';
 import profileStore from '../store/profileStore';
-import {Client} from 'bugsnag-react-native';
+import model from '../model/model';
+import {Client, Configuration} from 'bugsnag-react-native';
 
-const bsClient = new Client();
+const config = new Configuration();
+config.notifyReleaseStages = ['testflight', 'production'];
+const bsClient = new Client(config);
 
 if (!NativeEnv.get('DEBUG')) {
   // if there is an uncaught js error, logout and rethrow shortly thereafter
   // there is technically a race condition to see if the network request can complete in time to crash
   const originalGlobalErrorHandler = ErrorUtils.getGlobalHandler();
   ErrorUtils.setGlobalHandler(async (error) => {
+    const {profile} = model;
+    bsClient.setUser(model.user, profile && profile.displayName, profile && profile.email);
     bsClient.notify(error);
     // TODO: figure out a more elegant way of "restarting" from scratch
     await profileStore.logout();
