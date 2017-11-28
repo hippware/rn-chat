d123852881d0bef974bcb17d6253bd5428703659
aksonov <aksonov@gmail.com>
2017-11-28 17:19:24 +0100

    Hot-fixes: Android Firebase integration

--- android/app/src/main/java/com/tinyrobot/MainApplication.java
+++ android/app/src/main/java/com/tinyrobot/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import rnxmpp.RNXMPPPackage;
@@ -35,6 +38,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAnalyticsPackage(),
+            new RNFirebaseAuthPackage(),
             new MapsPackage(),
             new ReactNativePushNotificationPackage(),
             new RNXMPPPackage(),
