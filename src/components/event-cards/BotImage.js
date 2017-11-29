22812363a5b2c5e12a7161948f8a0962a1d79a3a
Eric Kirkham <eric@hippware.com>
2017-11-28 16:29:39 -0800

    gray BotImage loader

--- src/components/event-cards/BotImage.js
+++ src/components/event-cards/BotImage.js
@@ -1,21 +1,27 @@
 // @flow
 
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, View, StyleSheet} from 'react-native';
 import {observer} from 'mobx-react/native';
 import {defaultCover, width} from '../Global';
 import Bot from '../../model/Bot';
 import File from '../../model/File';
+import {colors} from '../../constants';
 
 type Props = {
   bot: Bot,
   image?: File,
 };
 
-const BotImage = (props: Props) => {
+const BotImage = observer((props: Props) => {
   const {bot, image} = props;
-  const source = (image ? image.source : bot.image && bot.image.source) || defaultCover[bot.coverColor % 4];
-  return <Image style={{height: width, width}} source={source} resizeMode='contain' />;
-};
+  const img = image || bot.image;
+  const source = img ? img.source : defaultCover[bot.coverColor % 4];
+  return img && !img.loaded ? <View style={[styles.image, {backgroundColor: colors.GREY}]} /> : <Image style={styles.image} source={source} resizeMode='contain' />;
+});
 
-export default observer(BotImage);
+export default BotImage;
+
+const styles = StyleSheet.create({
+  image: {height: width, width},
+});
