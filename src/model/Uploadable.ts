cda5ac43aff6e0871e0516a8624d87b8542e22fd
aksonov <aksonov@gmail.com>
2018-01-31 12:28:02 +0100

    simplify Uploadable

--- src/model/Uploadable.ts
+++ src/model/Uploadable.ts
@@ -2,14 +2,11 @@
 import {types, getEnv, flow, getParent, IModelType, ISnapshottable} from 'mobx-state-tree'
 // tslint:disable-next-line:no_unused-variable
 import {IObservableArray} from 'mobx'
-import {File} from './File'
 import {Base} from './Base'
 
 export function createUploadable(property: string, access: string | Function) {
-  let props: any = {}
-  props[property] = types.maybe(types.reference(File))
   return types
-    .compose(Base, types.model('Uploadable', props))
+    .compose(Base, types.model('Uploadable', {}))
     .volatile(self => ({
       uploading: false,
       uploaded: false,
