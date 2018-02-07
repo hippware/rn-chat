11866a5a882f03be2ccb2b06704f1edb1d21c45a
Eric Kirkham <eric@southerneer.com>
2018-02-06 16:24:26 -0800

    blockreport

--- src/components/ProfileDetail/BlockReport.js
+++ src/components/ProfileDetail/BlockReport.js
@@ -1,20 +1,19 @@
 // @flow
 
-import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity, Alert, Image} from 'react-native';
+import React from 'react';
+import {TouchableOpacity, Alert, Image} from 'react-native';
 import {observer} from 'mobx-react/native';
 import ActionSheet from 'react-native-actionsheet';
 import {Actions} from 'react-native-router-flux';
 import {Profile} from 'wocky-client';
-import {k} from '../Global';
-import {colors} from '../../constants';
+
+type Props = {
+  profile: Profile,
+};
 
 @observer
-export default class BlockReport extends Component {
+class BlockReport extends React.Component<Props> {
   actionSheet: any;
-  props: {
-    profile: Profile,
-  };
 
   onTap = (index: number) => {
     if (index === 0) {
@@ -29,8 +28,9 @@ export default class BlockReport extends Component {
           {
             text: 'Block',
             style: 'destructive',
-            onPress: () => {
-              // TODO: friendStore.block(this.props.profile);
+            onPress: async () => {
+              // TODO: spinner
+              await this.props.profile.block();
               Actions.reset('root');
             },
           },
@@ -41,7 +41,15 @@ export default class BlockReport extends Component {
 
   render() {
     return (
-      <TouchableOpacity onPress={() => this.actionSheet.show()} style={styles.rightButton}>
+      <TouchableOpacity
+        onPress={() => this.actionSheet.show()}
+        style={{
+          marginLeft: 15,
+          width: 24,
+          height: 24,
+          justifyContent: 'center',
+        }}
+      >
         <Image source={require('../../../images/ellipsis.png')} />
         <ActionSheet ref={o => (this.actionSheet = o)} options={['Report', 'Block', 'Cancel']} cancelButtonIndex={2} destructiveButtonIndex={1} onPress={this.onTap} />
       </TouchableOpacity>
@@ -49,50 +57,4 @@ export default class BlockReport extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  header: {
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-  },
-  displayName: {
-    paddingTop: 10 * k,
-    color: colors.navBarTextColorDay,
-    textAlign: 'center',
-  },
-  tagline: {
-    paddingBottom: 23 * k,
-    color: colors.navBarTextColorDay,
-    textAlign: 'center',
-  },
-  metabar: {
-    flexDirection: 'row',
-    paddingBottom: 30 * k,
-  },
-  number: {
-    color: colors.navBarTextColorDay,
-    textAlign: 'center',
-  },
-  followContainer: {
-    alignItems: 'center',
-    height: 15 * k,
-  },
-  followButton: {
-    marginTop: -30 * k,
-  },
-  word: {
-    color: colors.DARK_GREY,
-    textAlign: 'center',
-  },
-  rightContainer: {
-    marginRight: 10 * k,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  rightButton: {
-    marginLeft: 15,
-    width: 24,
-    height: 24,
-    justifyContent: 'center',
-  },
-});
+export default BlockReport;
