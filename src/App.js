9aea43260eb40fe0bb6df5e193a833225c2c93aa
Eric Kirkham <eric@southerneer.com>
2018-02-09 14:51:04 -0800

    deactivate reactotron for build

--- src/App.js
+++ src/App.js
@@ -5,27 +5,31 @@ import {View} from 'react-native';
 import {Provider} from 'mobx-react/native';
 import TinyRobotRouter from './components/Router';
 import analytics from './utils/analytics';
-import store, {notificationStore, codePushStore} from './store';
+import store, {notificationStore, codePushStore, reportStore} from './store';
 import NotificationBanner from './components/NotificationBanner';
 // import TinyRobotRouter from './components/RouterTest';
-import Reactotron, {trackGlobalErrors, openInEditor, overlay, asyncStorage, networking} from 'reactotron-react-native';
-import {mst} from 'reactotron-mst';
 
-Reactotron.configure({
-  name: 'Tinyrobot',
-})
-  .use(trackGlobalErrors())
-  .use(openInEditor())
-  .use(overlay())
-  .use(asyncStorage())
-  .use(networking())
-  .use(mst())
-  .connect();
+// if (__DEV__) {
+//   const Reactotron = require('reactotron-react-native').default;
+//   const {trackGlobalErrors, openInEditor, overlay, asyncStorage, networking} = require('reactotron-react-native');
+//   const {mst} = require('reactotron-mst');
 
-Reactotron.trackMstNode(store);
+//   Reactotron.configure({
+//     name: 'Tinyrobot',
+//   })
+//     .use(trackGlobalErrors())
+//     .use(openInEditor())
+//     .use(overlay())
+//     .use(asyncStorage())
+//     .use(networking())
+//     .use(mst())
+//     .connect();
+
+//   Reactotron.trackMstNode(store);
+// }
 
 const App = () => (
-  <Provider store={store} {...store} analytics={analytics} codePushStore={codePushStore} notificationStore={notificationStore}>
+  <Provider store={store} {...store} analytics={analytics} codePushStore={codePushStore} notificationStore={notificationStore} reportStore={reportStore}>
     <View style={{flex: 1}}>
       <TinyRobotRouter />
       <NotificationBanner />
