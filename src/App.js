c00986615122d02378765a6ae5e951e04aa31257
Eric Kirkham <eric@southerneer.com>
2018-02-12 11:59:52 -0800

    move reactotron to separate file

--- src/App.js
+++ src/App.js
@@ -8,25 +8,7 @@ import analytics from './utils/analytics';
 import store, {notificationStore, codePushStore, reportStore} from './store';
 import NotificationBanner from './components/NotificationBanner';
 // import TinyRobotRouter from './components/RouterTest';
-
-// if (__DEV__) {
-//   const Reactotron = require('reactotron-react-native').default;
-//   const {trackGlobalErrors, openInEditor, overlay, asyncStorage, networking} = require('reactotron-react-native');
-//   const {mst} = require('reactotron-mst');
-
-//   Reactotron.configure({
-//     name: 'Tinyrobot',
-//   })
-//     .use(trackGlobalErrors())
-//     .use(openInEditor())
-//     .use(overlay())
-//     .use(asyncStorage())
-//     .use(networking())
-//     .use(mst())
-//     .connect();
-
-//   Reactotron.trackMstNode(store);
-// }
+// require('./utils/reactotron');
 
 const App = () => (
   <Provider store={store} {...store} analytics={analytics} codePushStore={codePushStore} notificationStore={notificationStore} reportStore={reportStore}>
---
+++ src/utils/reactotron.js
@@ -0,0 +1,20 @@
+// @flow
+
+import store from '../store';
+// import TinyRobotRouter from './components/RouterTest';
+
+import Reactotron, {trackGlobalErrors, openInEditor, overlay, asyncStorage, networking} from 'reactotron-react-native';
+import {mst} from 'reactotron-mst';
+
+Reactotron.configure({
+  name: 'Tinyrobot',
+})
+  .use(trackGlobalErrors())
+  .use(openInEditor())
+  .use(overlay())
+  .use(asyncStorage())
+  .use(networking())
+  .use(mst())
+  .connect();
+
+Reactotron.trackMstNode(store);
