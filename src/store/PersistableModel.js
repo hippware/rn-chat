8dd46f6182499ff55886033fa84919f88b7fa330
Eric Kirkham <eric@southerneer.com>
2018-02-12 15:45:07 -0800

    minimal store hydration

--- src/store/PersistableModel.js
+++ src/store/PersistableModel.js
@@ -1,25 +1,34 @@
 // @flow
 
-import {types, getType, flow, applySnapshot, getSnapshot, getEnv} from 'mobx-state-tree';
+import {types, getType, flow, applySnapshot, getEnv} from 'mobx-state-tree';
 import {reaction} from 'mobx';
 import {Base} from 'wocky-client';
+import _ from 'lodash';
 
 export default types.compose(Base, types.model({id: 'Persistable'})).actions((self) => {
   const {logger, storage} = getEnv(self);
-  const prevSnapshot = {};
 
   async function loadFromStorage() {
     const modelName = getType(self).name;
     return new Promise((resolve, reject) => {
       storage.getItem(modelName, (error: Object, data: string) => {
         if (data) {
-          // console.log('loadFromStorage', modelName, data);
+          let parsed;
           try {
+            parsed = JSON.parse(data);
+            // throw new Error('Hydrate minimally');
             // TODO: custom "migrations" of 'Store' snapshots based on version
             applySnapshot(self, JSON.parse(data));
           } catch (err) {
-            logger.warn(`${modelName} hydration error`, data, err);
-            // reject(err)
+            logger.log('hydration error', modelName, parsed, err);
+            if (modelName === 'MainStore' && parsed.wocky) {
+              try {
+                applySnapshot(self.wocky, _.pick(parsed.wocky, ['username', 'password', 'host', 'resource', 'profile']));
+              } catch (err) {
+                logger.warn('Minimal hydration error', err);
+                reject(err);
+              }
+            }
           }
         } else if (error) {
           logger.warn(`${modelName} hydration error`, error);
@@ -32,7 +41,6 @@ export default types.compose(Base, types.model({id: 'Persistable'})).actions((se
   return {
     hydrate: flow(function* hydrate() {
       if (storage) {
-        // console.log('trying to load from storage');
         yield loadFromStorage();
         reaction(
           () => self.snapshot,
