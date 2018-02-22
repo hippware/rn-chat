6c29043aaef1cb7b24b84093d22641ea4eccb413
Eric Kirkham <eric@southerneer.com>
2018-02-21 17:57:48 -0800

    cleanup. mock virtualized list

--- __tests__/BotDetails.test.js
+++ __tests__/BotDetails.test.js
@@ -6,14 +6,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import BotDetails from '../src/components/BotDetails';
 import {Provider} from 'mobx-react/native';
-import {EventList, EventBotNote, EventBotPost} from 'wocky-client';
+// import {} from 'wocky-client';
 import injects from './utils/inject-mocks';
 
-describe('Home', () => {
+describe('BotDetails', () => {
   test('renders with no data', () => {
     const wocky = {
-      updates: [],
-      events: EventList.create({}),
+      bots: [],
+    };
+    const toRender = (
+      <Provider wocky={wocky} {...injects}>
+        <BotDetails />
+      </Provider>
+    );
+    const tree = renderer.create(toRender).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders with test bot', () => {
+    const wocky = {
       bots: [],
     };
     const toRender = (
