c339266b1d19c4aeed51664bfcac9b65c104ae10
Eric Kirkham <eric@southerneer.com>
2018-02-23 16:20:47 -0800

    simplify PeristableModel

--- src/store/PersistableModel.js
+++ src/store/PersistableModel.js
@@ -5,43 +5,59 @@ import {reaction} from 'mobx';
 import {Base} from 'wocky-client';
 import _ from 'lodash';
 
+// const migrations = {
+//   '2.0.0': {
+//     getState: (storage) => {
+//       storage.getItem(modelName, (error: Object, data: string) => {
+//       return storage.getItem('rnchat:model')
+//     },
+//     migrate: (previousState) => {
+//       return _.pick(previousState, ['user', 'server', 'password', 'resource'])
+//     }
+//   }
+// }
+
+function loadFromStorage(storage, key: string) {
+  return new Promise((resolve, reject) => {
+    storage.getItem(key, (error: Object, data: string) => {
+      if (error) reject(error);
+      else resolve(data);
+    });
+  });
+}
+
 export default types.compose(Base, types.model({id: 'Persistable'})).actions((self) => {
   const {logger, storage} = getEnv(self);
 
-  async function loadFromStorage() {
+  function loadMinimal(parsed: Object, modelName: string) {
+    logger.log('loadMinimal');
+    try {
+      applySnapshot(self.wocky, _.pick(parsed.wocky, ['username', 'password', 'host', 'resource', 'profile']));
+    } catch (err) {
+      logger.warn('Minimal hydration error', err);
+      // TODO: mixpanel
+      throw err;
+    }
+  }
+
+  async function load() {
     const modelName = getType(self).name;
-    return new Promise((resolve, reject) => {
-      storage.getItem(modelName, (error: Object, data: string) => {
-        if (data) {
-          let parsed;
-          try {
-            parsed = JSON.parse(data);
-            // throw new Error('Hydrate minimally');
-            // TODO: custom "migrations" of 'Store' snapshots based on version
-            applySnapshot(self, JSON.parse(data));
-          } catch (err) {
-            logger.log('hydration error', modelName, parsed, err);
-            if (modelName === 'MainStore' && parsed.wocky) {
-              try {
-                applySnapshot(self.wocky, _.pick(parsed.wocky, ['username', 'password', 'host', 'resource', 'profile']));
-              } catch (err) {
-                logger.warn('Minimal hydration error', err);
-                reject(err);
-              }
-            }
-          }
-        } else if (error) {
-          logger.warn(`${modelName} hydration error`, error);
-        }
-        resolve();
-      });
-    });
+    let parsed;
+    try {
+      const data = await loadFromStorage(storage, modelName);
+      parsed = JSON.parse(data);
+      // throw new Error('Hydrate minimally');
+      applySnapshot(self, parsed);
+    } catch (err) {
+      logger.log('hydration error', modelName, parsed, err);
+      modelName === 'MainStore' && parsed.wocky && loadMinimal(parsed, modelName);
+    }
   }
 
   return {
     hydrate: flow(function* hydrate() {
       if (storage) {
-        yield loadFromStorage();
+        yield load();
         reaction(
           () => self.snapshot,
           (json) => {
@@ -52,6 +68,7 @@ export default types.compose(Base, types.model({id: 'Persistable'})).actions((se
         );
         return true;
       }
+      return false;
     }),
   };
 });
