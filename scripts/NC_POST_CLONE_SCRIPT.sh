aa63ccc7430dd5b678d4e51a4aac85eebac9a53d
Eric Kirkham <eric@southerneer.com>
2018-03-05 11:20:31 -0800

    get nevercode scripts into source control

---
+++ scripts/NC_POST_CLONE_SCRIPT.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $GREENHOUSE_BUILD_DIR
+npm install react-native-cli yarn -g
+yarn
+date
+yarn test
+npm run bugsnag
+
+#cd ../..
+#react-native bundle --minify --dev false --entry-file index.ios.js --platform ios --assets-dest ios.bundle --bundle-output ios.bundle/main.jsbundle
+#zip -r os.bundle.zip ios.bundle
+#npm run sign os.bundle.zip
+#zip main.zip ios.bundle.zip  signature.txt
+#find node_modules -type f -name '.babelrc' | grep -v 'node_modules/react-native/packager/react-packager/.babelrc' | xargs rm
---
+++ scripts/NC_PRE_BUILD_SCRIPT.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $GREENHOUSE_BUILD_DIR
+set -e	# exit on first failed command
+set -x  # print all executed commands to the terminal
+
+# $NEVERCODE_BUILD_DIR points to the cloned repository root
+gem uninstall -a -x cocoapods
+gem install cocoapods -v 1.3.1
+pod repo update
+date
+
+#cd ../..
+#react-native bundle --minify --dev false --entry-file index.ios.js --platform ios --assets-dest ios.bundle --bundle-output ios.bundle/main.jsbundle
+#zip -r os.bundle.zip ios.bundle
+#npm run sign os.bundle.zip
+#zip main.zip ios.bundle.zip  signature.txt
+#find node_modules -type f -name '.babelrc' | grep -v 'node_modules/react-native/packager/react-packager/.babelrc' | xargs rm
