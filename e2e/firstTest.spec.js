173d7e0375435d452d3afccff5126c3736438c23
Eric Kirkham <eric@southerneer.com>
2018-03-09 18:08:51 -0800

    WIP test script

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -12,12 +12,21 @@ describe('Example', () => {
     await element(by.id('bypassRegisterButton')).tap();
 
     // TODO: what is the next "visible" element/screen before we eventually make it to the home screen?
-    // using `waitFor` is kind of a hack until we figure this out: https://github.com/wix/detox/blob/master/docs/Troubleshooting.RunningTests.md#test-tries-to-find-my-component-before-its-created
-
+    // using `waitFor` is kind of a hack until we figure this out
+    // https://github.com/wix/detox/blob/master/docs/APIRef.waitFor.md
     await waitFor(element(by.id('screenHome')))
       .toBeVisible()
       .withTimeout(2000);
-
     // await expect(element(by.id('screenHome'))).toBeVisible();
+
+    await waitFor(element(by.label('Always Allow').and(by.type('_UIAlertControllerActionView'))))
+      .toBeVisible()
+      .withTimeout(2000);
+
+    // TODO: something like this to dismiss the location and notification permissions
+    await element(by.label('Always Allow').and(by.type('_UIAlertControllerActionView'))).tap();
+
+    // TODO: wocky-client mock with repackager
+    // https://github.com/wix/detox/blob/master/docs/Guide.Mocking.md
   });
 });
