ed5611b1cf1c0e5e6a12c4fc2e2e558face757c3
Eric Kirkham <eric@southerneer.com>
2018-04-02 13:17:24 -0700

    tsx RText

---
+++ src/components/common/RText.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+type Props = {
+  size?: number
+  color?: string
+  weight?: 'Regular' | 'Bold' | 'Light' | 'Medium' | 'Italic'
+  style?: any
+  children?: any
+}
+
+const RText = (props: Props) => {
+  const {children, color, size, weight, style, ...rest} = props
+  const fontFamily = weight ? `Roboto-${weight}` : 'Roboto-Regular'
+  const fontSize = size || 12
+  return (
+    <Text style={[{color, fontSize, fontFamily}, style]} {...rest}>
+      {children}
+    </Text>
+  )
+}
+
+export default RText
