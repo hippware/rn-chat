c55a4834e569b579715ba382a6245cff42a8e5e3
Eric Kirkham <eric@southerneer.com>
2018-04-17 17:06:40 -0700

    BotShareSelectFriends: wrap actions + prettify

--- src/components/people-lists/BotShareSelectFriends.js
+++ src/components/people-lists/BotShareSelectFriends.js
@@ -1,73 +1,76 @@
 // @flow
 
-import React from 'react';
-import {Alert, TouchableOpacity, Text, View, Keyboard, StyleSheet} from 'react-native';
-import {observer, inject} from 'mobx-react/native';
-import {observable} from 'mobx';
-import {Actions} from 'react-native-router-flux';
-import {k} from '../Global';
-import {AutoExpandingTextInput} from '../common';
-import Screen from '../Screen';
-import {colors} from '../../constants';
-import SelectableProfileList from '../../store/SelectableProfileList';
-import FriendMultiSelect from './FriendMultiSelect';
+import React from 'react'
+import {Alert, TouchableOpacity, Text, View, Keyboard, StyleSheet} from 'react-native'
+import {observer, inject} from 'mobx-react/native'
+import {observable, action} from 'mobx'
+import {Actions} from 'react-native-router-flux'
+import {k} from '../Global'
+import {AutoExpandingTextInput} from '../common'
+import Screen from '../Screen'
+import {colors} from '../../constants'
+import SelectableProfileList from '../../store/SelectableProfileList'
+import FriendMultiSelect from './FriendMultiSelect'
 
 type Props = {
   botId: string,
-};
+}
 
 type State = {
   height: number,
   message: string,
-};
+}
 
 @inject('wocky', 'notificationStore')
 @observer
 class BotShareSelectFriends extends React.Component<Props, State> {
-  @observable selection: SelectableProfileList = SelectableProfileList.create({});
-  @observable bot: Bot;
-  mounted: boolean = false;
+  @observable selection: SelectableProfileList = SelectableProfileList.create({})
+  @observable bot: Bot
+  mounted: boolean = false
 
   constructor(props: Props) {
-    super(props);
-    this.state = {height: 0, message: ''};
+    super(props)
+    this.state = {height: 0, message: ''}
   }
 
+  @action
   componentDidMount() {
-    const {friends, getBot} = this.props.wocky;
-    this.bot = getBot({id: this.props.botId});
-    this.selection.setList(friends.map(f => ({profile: f})));
-    Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
-    Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
-    this.mounted = true;
+    const {friends, getBot} = this.props.wocky
+    this.bot = getBot({id: this.props.botId})
+    this.selection.setList(friends.map(f => ({profile: f})))
+    Keyboard.addListener('keyboardWillShow', this.keyboardWillShow)
+    Keyboard.addListener('keyboardWillHide', this.keyboardWillHide)
+    this.mounted = true
   }
 
   componentWillUnmount() {
-    this.mounted = false;
-    Keyboard.removeListener('keyboardWillShow');
-    Keyboard.removeListener('keyboardWillHide');
+    this.mounted = false
+    Keyboard.removeListener('keyboardWillShow')
+    Keyboard.removeListener('keyboardWillHide')
   }
 
   share = () => {
-    const shareSelect = this.selection.selected.map(sp => sp.id);
+    const shareSelect = this.selection.selected.map(sp => sp.id)
     try {
-      this.bot.share(shareSelect, this.state.message);
-      const num = shareSelect.length;
-      this.props.notificationStore.flash(`Bot shared with ${num} ${num > 1 ? 'friends' : 'friend'} 🎉`);
-      Actions.pop({animated: false});
+      this.bot.share(shareSelect, this.state.message)
+      const num = shareSelect.length
+      this.props.notificationStore.flash(
+        `Bot shared with ${num} ${num > 1 ? 'friends' : 'friend'} 🎉`
+      )
+      Actions.pop({animated: false})
     } catch (e) {
-      Alert.alert('There was a problem sharing the bot.');
-      console.warn(e);
+      Alert.alert('There was a problem sharing the bot.')
+      console.warn(e)
     }
-  };
+  }
 
-  keyboardWillShow = (e) => {
-    if (this.mounted) this.setState({height: e.endCoordinates.height});
-  };
+  keyboardWillShow = e => {
+    if (this.mounted) this.setState({height: e.endCoordinates.height})
+  }
 
-  keyboardWillHide = (e) => {
-    if (this.mounted) this.setState({height: 0});
-  };
+  keyboardWillHide = e => {
+    if (this.mounted) this.setState({height: 0})
+  }
 
   render() {
     return (
@@ -82,7 +85,7 @@ class BotShareSelectFriends extends React.Component<Props, State> {
                 maxLength={140}
                 value={this.state.message}
                 onChangeText={text => this.setState({message: text})}
-                placeholder='Write an optional message...'
+                placeholder="Write an optional message..."
               />
             </View>
             <TouchableOpacity style={styles.shareButton} onPress={this.share}>
@@ -92,11 +95,11 @@ class BotShareSelectFriends extends React.Component<Props, State> {
         )}
         <View style={{height: this.state.height}} />
       </Screen>
-    );
+    )
   }
 }
 
-export default BotShareSelectFriends;
+export default BotShareSelectFriends
 
 const styles = StyleSheet.create({
   container: {
@@ -126,4 +129,4 @@ const styles = StyleSheet.create({
     fontSize: 15 * k,
     color: colors.PURPLE,
   },
-});
+})
