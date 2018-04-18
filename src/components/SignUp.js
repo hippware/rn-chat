70922ba4902106b44a5aab496c72ee837026020c
Eric Kirkham <eric@southerneer.com>
2018-04-17 17:00:13 -0700

    SignUp: wrap action + prettify

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,78 +1,95 @@
 // @flow
 
-import React from 'react';
-import {View, Image, StyleSheet, Text, Linking} from 'react-native';
-import {observable, when} from 'mobx';
-import {observer, inject} from 'mobx-react/native';
-import {Actions} from 'react-native-router-flux';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {k} from './Global';
-import FormTextInput from './FormTextInput';
-import SignUpAvatar from './SignUpAvatar';
+import React from 'react'
+import {View, Image, StyleSheet, Text, Linking} from 'react-native'
+import {observable, when, action} from 'mobx'
+import {observer, inject} from 'mobx-react/native'
+import {Actions} from 'react-native-router-flux'
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import {k} from './Global'
+import FormTextInput from './FormTextInput'
+import SignUpAvatar from './SignUpAvatar'
 // import * as log from '../utils/log';
-import {colors} from '../constants';
-import Button from 'apsl-react-native-button';
-import {RText, Spinner} from './common';
-import {ValidatableProfile} from '../utils/formValidation';
+import {colors} from '../constants'
+import Button from 'apsl-react-native-button'
+import {RText, Spinner} from './common'
+import {ValidatableProfile} from '../utils/formValidation'
 
 @inject('wocky', 'analytics')
 @observer
 class SignUp extends React.Component<{}> {
-  @observable vProfile: ValidatableProfile;
-  handle: any;
-  firstName: any;
-  lastName: any;
-  email: any;
-  when: any;
+  @observable vProfile: ValidatableProfile
+  handle: any
+  firstName: any
+  lastName: any
+  email: any
+  when: any
 
+  @action
   componentDidMount() {
-    this.vProfile = this.props.wocky.profile && new ValidatableProfile(this.props.wocky.profile);
+    this.vProfile = this.props.wocky.profile && new ValidatableProfile(this.props.wocky.profile)
   }
 
+  @action
   componentWillReceiveProps(nextProps) {
     if (nextProps.routeName === 'signUp') {
-      this.vProfile = this.props.wocky.profile && new ValidatableProfile(this.props.wocky.profile);
+      this.vProfile = this.props.wocky.profile && new ValidatableProfile(this.props.wocky.profile)
     }
   }
 
   done = () => {
-    const {profile} = this.props.wocky;
+    const {profile} = this.props.wocky
     try {
-      profile.update(this.vProfile.asObject);
+      profile.update(this.vProfile.asObject)
       this.when = when(
         () => !profile.updating && !profile.updateError,
         () => {
-          this.props.analytics.track('createprofile_complete', {profile: this.props.wocky.profile.toJSON()});
-          Actions.logged();
-        },
-      );
+          this.props.analytics.track('createprofile_complete', {
+            profile: this.props.wocky.profile.toJSON(),
+          })
+          Actions.logged()
+        }
+      )
     } catch (err) {
-      this.props.analytics.track('createprofile_fail', {profile: this.props.wocky.profile.toJSON(), error: err});
+      this.props.analytics.track('createprofile_fail', {
+        profile: this.props.wocky.profile.toJSON(),
+        error: err,
+      })
     }
-  };
+  }
 
   render() {
-    const {profile} = this.props.wocky;
+    const {profile} = this.props.wocky
     if (!profile) {
       return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Text>Profile is not loaded</Text>
         </View>
-      );
+      )
     }
-    const {updating, updateError} = profile;
+    const {updating, updateError} = profile
     // TODO: handle update errors with notificationStore. Watch for updateError in componentDidMount and flash error
     if (updateError !== '') {
-      console.warn('update error?', updateError);
+      console.warn('update error?', updateError)
     }
 
-    const buttonDisabled = (this.vProfile && !this.vProfile.isValid) || updating;
+    const buttonDisabled = (this.vProfile && !this.vProfile.isValid) || updating
     return (
       <KeyboardAwareScrollView style={{flex: 1}}>
-        <View style={{marginLeft: 70 * k, marginRight: 70 * k, marginTop: 47.5 * k, flexDirection: 'row'}}>
-          <Image style={{width: 60 * k, height: 69 * k}} source={require('../../images/pink.png')} />
+        <View
+          style={{
+            marginLeft: 70 * k,
+            marginRight: 70 * k,
+            marginTop: 47.5 * k,
+            flexDirection: 'row',
+          }}
+        >
+          <Image
+            style={{width: 60 * k, height: 69 * k}}
+            source={require('../../images/pink.png')}
+          />
           <View style={{paddingLeft: 20 * k}}>
-            <RText weight='Light' size={30} color={colors.PINK} style={{lineHeight: 35 * k}}>
+            <RText weight="Light" size={30} color={colors.PINK} style={{lineHeight: 35 * k}}>
               Let's create your profile!
             </RText>
           </View>
@@ -84,23 +101,23 @@ class SignUp extends React.Component<{}> {
           <FormTextInput
             icon={require('../../images/iconUsernameNew.png')}
             ref={r => (this.handle = r)}
-            name='handle'
-            label='Username'
-            autoCapitalize='none'
+            name="handle"
+            label="Username"
+            autoCapitalize="none"
             onSubmitEditing={() => this.firstName.focus()}
             store={this.vProfile && this.vProfile.handle}
           />
           <FormTextInput
             icon={require('../../images/iconSubsNew.png')}
-            name='firstName'
-            label='First Name'
+            name="firstName"
+            label="First Name"
             ref={r => (this.firstName = r)}
             onSubmitEditing={() => this.lastName.focus()}
             store={this.vProfile && this.vProfile.firstName}
           />
           <FormTextInput
-            name='lastName'
-            label='Last Name'
+            name="lastName"
+            label="Last Name"
             ref={r => (this.lastName = r)}
             onSubmitEditing={() => this.email.focus()}
             store={this.vProfile && this.vProfile.lastName}
@@ -108,35 +125,46 @@ class SignUp extends React.Component<{}> {
           <FormTextInput
             onSubmit={this.done}
             icon={require('../../images/iconEmailNew.png')}
-            name='email'
-            label='Email'
-            autoCapitalize='none'
-            keyboardType='email-address'
-            returnKeyType='done'
+            name="email"
+            label="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            returnKeyType="done"
             ref={r => (this.email = r)}
             store={this.vProfile && this.vProfile.email}
           />
         </View>
         <RText size={12.5} color={colors.DARK_GREY} style={styles.agreeNote}>
           {'By signing up you agree to our '}
-          <RText weight='Bold' onPress={() => Linking.openURL('https://tinyrobot.com/privacy-policy/')}>
+          <RText
+            weight="Bold"
+            onPress={() => Linking.openURL('https://tinyrobot.com/privacy-policy/')}
+          >
             Privacy Policy
           </RText>
           {',\r\n '}
-          <RText weight='Bold' onPress={() => Linking.openURL('https://tinyrobot.com/terms-of-service/')}>
+          <RText
+            weight="Bold"
+            onPress={() => Linking.openURL('https://tinyrobot.com/terms-of-service/')}
+          >
             Terms of Service
           </RText>
           <RText>{', and for us to contact you via email\r\nfor updates and information.'}</RText>
         </RText>
-        <Button isDisabled={buttonDisabled || !this.props.wocky.connected} onPress={this.done} style={styles.submitButton} textStyle={styles.text}>
-          {updating ? <Spinner color='white' size={22} /> : 'Done'}
+        <Button
+          isDisabled={buttonDisabled || !this.props.wocky.connected}
+          onPress={this.done}
+          style={styles.submitButton}
+          textStyle={styles.text}
+        >
+          {updating ? <Spinner color="white" size={22} /> : 'Done'}
         </Button>
       </KeyboardAwareScrollView>
-    );
+    )
   }
 }
 
-export default SignUp;
+export default SignUp
 
 const styles = StyleSheet.create({
   text: {fontSize: 17.5 * k, letterSpacing: 0.8, fontFamily: 'Roboto-Regular', color: 'white'},
@@ -155,4 +183,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   paginationStyle: {bottom: 170 * k},
-});
+})
