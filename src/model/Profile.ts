a244b7e9b892e9021fd45bc03d55bbd41fa14d11
aksonov <aksonov@gmail.com>
2018-04-19 14:46:49 +0200

    sorter order of bots

--- src/model/Profile.ts
+++ src/model/Profile.ts
@@ -111,7 +111,14 @@ export const Profile = types
           return geofenceBots
         },
         get activeBots(): Array<IBot> {
-          return geofenceBots.list.filter((bot: IBot) => bot.visitorsSize)
+          const arr = geofenceBots.list.filter((bot: IBot) => bot.visitorsSize).map((data, index) => ({data, index}))
+          return arr
+            .sort((a, b) => {
+              if (a.data.visitor && !b.data.visitor) return -1
+              if (!a.data.visitor && b.data.visitor) return 1
+              return a.index - b.index
+            })
+            .map(rec => rec.data)
         },
         get displayName(): string {
           if (self.firstName && self.lastName) {
