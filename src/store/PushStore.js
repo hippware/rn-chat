45745569f169758cf3cf0bde542ebb32669e4a43
aksonov <aksonov@gmail.com>
2018-04-23 16:44:26 +0200

    Reset counter only for non-zero count #2155

--- src/store/PushStore.js
+++ src/store/PushStore.js
@@ -1,43 +1,43 @@
 // @flow
 
-import {when, observable} from 'mobx';
-import * as log from '../utils/log';
-import PushNotification from 'react-native-push-notification';
-import {Linking} from 'react-native';
+import {when, observable} from 'mobx'
+import * as log from '../utils/log'
+import PushNotification from 'react-native-push-notification'
+import {Linking} from 'react-native'
 
 class PushStore {
-  requestedPermissions = false;
-  wocky: any;
-  analytics: any;
-  @observable pushNotificationToken: string;
+  requestedPermissions = false
+  wocky: any
+  analytics: any
+  @observable pushNotificationToken: string
 
   constructor(wocky, analytics) {
-    this.wocky = wocky;
-    this.analytics = analytics;
+    this.wocky = wocky
+    this.analytics = analytics
     PushNotification.configure({
       onRegister({token}) {
-        this.pushNotificationToken = token;
+        this.pushNotificationToken = token
         when(
           () => wocky.connected,
           () => {
             try {
-              wocky.enablePush(this.pushNotificationToken);
+              wocky.enablePush(this.pushNotificationToken)
             } catch (err) {
-              log.warn('Push Notification enable error', err);
+              log.warn('Push Notification enable error', err)
             }
-          },
-        );
+          }
+        )
       },
       onNotification(notification) {
-        log.log('Push Notification:', notification);
-        analytics.track('push_notification_received', {notification});
+        log.log('Push Notification:', notification)
+        analytics.track('push_notification_received', {notification})
         if (!notification.foreground && notification.data && notification.data.uri) {
           try {
-            analytics.track('push_notification_try', {notification});
-            Linking.openURL(notification.data.uri);
-            analytics.track('push_notification_success', {notification});
+            analytics.track('push_notification_try', {notification})
+            Linking.openURL(notification.data.uri)
+            analytics.track('push_notification_success', {notification})
           } catch (err) {
-            analytics.track('push_notification_fail', {notification, error: err});
+            analytics.track('push_notification_fail', {notification, error: err})
           }
         }
       },
@@ -49,17 +49,21 @@ class PushStore {
       },
       popInitialNotification: true,
       requestPermissions: false,
-    });
-    PushNotification.setApplicationIconBadgeNumber(0);
-    when(() => wocky.connected, this.requestPushPermissions);
+    })
+    PushNotification.getApplicationIconBadgeNumber((badgeCount: number) => {
+      if (badgeCount > 0) {
+        PushNotification.setApplicationIconBadgeNumber(0)
+      }
+    })
+    when(() => wocky.connected, this.requestPushPermissions)
   }
 
   requestPushPermissions = async () => {
     if (!this.requestedPermissions && !PushNotification.isPermissionsRequestPending) {
-      const res = await PushNotification.requestPermissions();
-      this.requestedPermissions = true;
+      const res = await PushNotification.requestPermissions()
+      this.requestedPermissions = true
     }
-  };
+  }
 }
 
-export default PushStore;
+export default PushStore
