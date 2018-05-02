a44fd117100c3acbcf659253cf0f267c8f2db500
Eric Kirkham <eric@southerneer.com>
2018-05-01 17:37:17 -0700

    UI ready

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,8 +1,15 @@
 // @flow
 
-import LocationGeofenceWarning from './LocationGeofenceWarning';
-import LocationPrimer from './LocationPrimer';
-import LocationWarning from './LocationWarning';
-import SharePresencePrimer from './SharePresencePrimer';
+import LocationGeofenceWarning from './LocationGeofenceWarning'
+import LocationPrimer from './LocationPrimer'
+import LocationWarning from './LocationWarning'
+import SharePresencePrimer from './SharePresencePrimer'
+import FirstTimeGuestPrimer from './FirstTimeGuestPrimer'
 
-export {LocationGeofenceWarning, LocationPrimer, LocationWarning, SharePresencePrimer};
+export {
+  LocationGeofenceWarning,
+  LocationPrimer,
+  LocationWarning,
+  SharePresencePrimer,
+  FirstTimeGuestPrimer,
+}
