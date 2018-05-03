1f34ba7134d9ee1bcf63fb9608b3585bbc4e2ef9
Eric Kirkham <eric@southerneer.com>
2018-05-03 13:31:44 -0700

    faster nevercode builds?

---
+++ scripts/NC_POST_BUILD_SCRIPT.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd $NEVERCODE_BUILD_DIR
+set -e	# exit on first failed command
+set -x  # print all executed commands to the terminal
+
+# DETOX
+brew tap wix/brew
+brew install applesimutils
+# we don't need to pod install as long as we're keeping pods checked into git
+# - run: pod install --project-directory=./ios
+detox build --configuration ios.sim.release
+# detox clean-framework-cache && detox build-framework-cache
+detox test --configuration ios.sim.release --cleanup
+
+# gem uninstall -a -x cocoapods
+# gem install cocoapods -v 1.3.1
+# pod repo update
+date
