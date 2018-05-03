2e61e98c49b74b07f2fa9ca44070d1891e51cab2
aksonov <aksonov@gmail.com>
2018-05-03 15:05:04 +0200

    better marker key

--- src/components/map/BotMarker.tsx
+++ src/components/map/BotMarker.tsx
@@ -32,18 +32,21 @@ export default class BotMarker extends React.Component<Props> {
     if (!bot || !isAlive(bot) || !bot.location) {
       return null
     }
+    const key =
+      (id || bot.id) +
+      (bot && bot.image && bot.image.loaded) +
+      (bot && bot.image && bot.image.loading) +
+      (bot && bot.image && bot.image.id) +
+      (bot && bot.image && bot.image.thumbnail && bot.image.thumbnail.uri) +
+      (bot && bot.address)
+    // DIRTY workaround to re-render googlemaps marker without tracksViewChanges (otherwise we will have 100% CPU usage)
+
     return (
       <MapView.Marker.Animated
         anchor={{x: 0.5, y}}
         tracksViewChanges={this.tracking}
         style={[{top: -2000}, style]} // DIRTY workaround to catch all onPress events for the marker.
-        key={
-          (id || bot.id) +
-          (bot && bot.image && bot.image.loaded) +
-          (bot && bot.image && bot.image.loading) +
-          (bot && bot.image && bot.image.thumbnail && bot.image.thumbnail.uri) +
-          (bot && bot.address)
-        } // DIRTY workaround to re-render googlemaps marker without tracksViewChanges (otherwise we will have 100% CPU usage)
+        key={key}
         identifier={bot.id}
         coordinate={{latitude: bot.location.latitude, longitude: bot.location.longitude}}
         onPress={props.onImagePress}
