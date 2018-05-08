bf9363d09dd07e1c9fcfa986b45947738d45ffdb
aksonov <aksonov@gmail.com>
2018-05-08 16:22:56 +0200

    hot-fix: use correct force param

--- src/store/Wocky.ts
+++ src/store/Wocky.ts
@@ -576,7 +576,7 @@ export const Wocky = types
           () => self.profile && self.connected,
           async (connected: boolean) => {
             if (connected) {
-              self.geofenceBots.load({refresh: true})
+              self.geofenceBots.load({force: true})
               await self.loadChats()
               self.requestRoster()
               if (!self.version) {
