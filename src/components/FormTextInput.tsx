96af1c0dc80fd935016a7ca140eebb8e80eb58a3
Eric Kirkham <eric@southerneer.com>
2018-05-09 18:38:04 -0700

    FormTextInput -> tsx

--- src/components/FormTextInput.tsx
+++ src/components/FormTextInput.tsx
@@ -1,36 +1,36 @@
 // @flow
 
-import React from 'react';
-import {Image, View, TextInput} from 'react-native';
-import autobind from 'autobind-decorator';
-import {k} from './Global';
-import {colors} from '../constants';
-import {observer} from 'mobx-react/native';
-import {ValidateItem} from '../utils/formValidation';
-import {RText} from './common';
-import Cell from './Cell';
+import React from 'react'
+import {Image, View, TextInput} from 'react-native'
+import autobind from 'autobind-decorator'
+import {k} from './Global'
+import {colors} from '../constants'
+import {observer} from 'mobx-react/native'
+import {ValidateItem} from '../utils/formValidation'
+import {RText} from './common'
+import Cell from './Cell'
 
 type Props = {
-  icon?: any,
-  label: string,
-  store?: ValidateItem,
-};
+  icon?: any
+  label: string
+  store?: ValidateItem
+}
 
 @autobind
 @observer
 class FormTextInput extends React.Component<Props> {
-  input: any;
+  input: any
 
   focus() {
-    this.input.focus();
+    this.input.focus()
   }
 
   blur() {
-    this.input.blur();
+    this.input.blur()
   }
 
   render() {
-    const {icon, label, store} = this.props;
+    const {icon, label, store} = this.props
 
     return (
       <View>
@@ -42,18 +42,27 @@ class FormTextInput extends React.Component<Props> {
           <View style={{height: 11 * k}} />
         )}
 
-        <Cell image={icon} style={{justifyContent: 'center', paddingTop: 2 * k}} imageStyle={{height: 20 * k, width: 20 * k, marginHorizontal: 5 * k}}>
+        <Cell
+          image={icon}
+          style={{justifyContent: 'center', paddingTop: 2 * k}}
+          imageStyle={{height: 20 * k, width: 20 * k, marginHorizontal: 5 * k}}
+        >
           {icon ? null : <View style={{width: 30 * k}} />}
           <TextInput
-            style={{flex: 1, color: colors.DARK_PURPLE, fontFamily: 'Roboto-Regular', fontSize: 18 * k}}
+            style={{
+              flex: 1,
+              color: colors.DARK_PURPLE,
+              fontFamily: 'Roboto-Regular',
+              fontSize: 18 * k,
+            }}
             placeholder={label}
-            clearButtonMode='while-editing'
-            underlineColorAndroid='transparent'
+            clearButtonMode="while-editing"
+            underlineColorAndroid="transparent"
             ref={r => (this.input = r)}
             placeholderTextColor={colors.GREY}
-            returnKeyType='next'
-            onChangeText={(text) => {
-              if (store) store.value = text;
+            returnKeyType="next"
+            onChangeText={text => {
+              if (store) store.value = text
             }}
             value={store && store.value}
             autoCorrect={false}
@@ -72,8 +81,8 @@ class FormTextInput extends React.Component<Props> {
         </Cell>
         <View style={{height: 1, backgroundColor: colors.DARK_PURPLE, opacity: 0.2}} />
       </View>
-    );
+    )
   }
 }
 
-export default FormTextInput;
+export default FormTextInput
