e9abedc40271b2696c53a16669014108feafc985
Eric Kirkham <eric@southerneer.com>
2018-05-11 14:18:06 -0700

    test cleanup

--- __tests__/utils/mockMap.js
+++ __tests__/utils/mockMap.js
@@ -1,39 +1,39 @@
 // from https://github.com/react-community/react-native-maps/issues/889#issuecomment-300637902
 
 jest.mock('react-native-maps', () => {
-  const React = require.requireActual('react');
-  const MapView = require.requireActual('react-native-maps');
+  const React = require.requireActual('react')
+  const MapView = require.requireActual('react-native-maps')
 
   class MockCallout extends React.Component {
     render() {
-      return React.createElement('Callout', this.props, this.props.children);
+      return React.createElement('Callout', this.props, this.props.children)
     }
   }
 
   class MockMarker extends React.Component {
     render() {
-      return React.createElement('Marker', this.props, this.props.children);
+      return React.createElement('Marker', this.props, this.props.children)
     }
   }
 
   class MockCircle extends React.Component {
     render() {
-      return React.createElement('Circle', this.props, this.props.children);
+      return React.createElement('Circle', this.props, this.props.children)
     }
   }
 
   class MockMapView extends React.Component {
     render() {
-      return React.createElement('MapView', this.props, this.props.children);
+      return React.createElement('MapView', this.props, this.props.children)
     }
   }
 
-  MockCallout.propTypes = MapView.Callout.propTypes;
-  MockMarker.propTypes = MapView.Marker.propTypes;
-  MockMapView.propTypes = MapView.propTypes;
-  MockMapView.Marker = MockMarker;
-  MockMapView.Marker.Animated = MockMarker;
-  MockMapView.Callout = MockCallout;
-  MockMapView.Circle = MockCircle;
-  return MockMapView;
-});
+  MockCallout.propTypes = MapView.Callout.propTypes
+  MockMarker.propTypes = MapView.Marker.propTypes
+  MockMapView.propTypes = MapView.propTypes
+  MockMapView.Marker = MockMarker
+  MockMapView.MarkerAnimated = MockMarker
+  MockMapView.Callout = MockCallout
+  MockMapView.Circle = MockCircle
+  return MockMapView
+})
--- src/components/BotCompose/BotComposeMap.tsx
+++ src/components/BotCompose/BotComposeMap.tsx
@@ -62,7 +62,6 @@ class BotComposeMap extends React.Component<Props> {
               bot={bot}
             />
           }
-          style={{}}
         />
       </View>
     )
