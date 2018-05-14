28bce81aa440fc6d006038b9ffe49f99d63227ac
Eric Kirkham <eric@southerneer.com>
2018-05-14 12:42:25 -0700

    DRY: map marker hacks in  one file

--- src/components/map/ClusterMarker.tsx
+++ src/components/map/ClusterMarker.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import {Marker} from 'react-native-maps'
-import {observer} from 'mobx-react/native'
 import {k} from '../Global'
 import {View} from 'react-native'
 import {colors} from '../../constants'
 import {RText} from '../common'
+import HackMarker from './HackMarker'
 
 type Props = {
   coordinate: any
@@ -12,54 +11,25 @@ type Props = {
   pointCount: number
 }
 
-@observer
-export default class BotMarker extends React.Component<Props> {
-  mounted = false
-  state = {tracking: true}
+const ClusterMarker = ({pointCount, ...props}: Props) => (
+  <HackMarker {...props}>
+    <View
+      style={{
+        height: 40 * k,
+        width: 40 * k,
+        borderRadius: 8 * k,
+        borderColor: colors.WHITE,
+        backgroundColor: colors.PINK,
+        borderWidth: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <RText size={20} color={colors.WHITE}>
+        {pointCount}
+      </RText>
+    </View>
+  </HackMarker>
+)
 
-  componentDidMount() {
-    this.mounted = true
-    setTimeout(() => this.mounted && this.setState({tracking: false}), 1000)
-  }
-
-  componentWillUnmount() {
-    this.mounted = false
-  }
-
-  componentDidUpdate(_0, previousState) {
-    if (previousState.tracking && !this.state.tracking) {
-      return
-    }
-    if (!previousState.tracking && this.state.tracking) {
-      return
-    }
-    if (this.mounted && !this.state.tracking) {
-      this.setState({tracking: true})
-      setTimeout(() => this.mounted && this.setState({tracking: false}), 500)
-    }
-  }
-
-  render() {
-    const {coordinate, onPress, pointCount} = this.props
-    return (
-      <Marker coordinate={coordinate} onPress={onPress} tracksViewChanges={this.state.tracking}>
-        <View
-          style={{
-            height: 40 * k,
-            width: 40 * k,
-            borderRadius: 8 * k,
-            borderColor: colors.WHITE,
-            backgroundColor: colors.PINK,
-            borderWidth: 2,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <RText size={20} color={colors.WHITE}>
-            {pointCount}
-          </RText>
-        </View>
-      </Marker>
-    )
-  }
-}
+export default ClusterMarker
