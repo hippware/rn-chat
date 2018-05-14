4d1b71666c443ff9a0dfaa60a54af308133d9fe2
Eric Kirkham <eric@southerneer.com>
2018-05-14 12:42:25 -0700

    DRY: map marker hacks in  one file

--- src/components/map/BotMarker.tsx
+++ src/components/map/BotMarker.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import BotBubble from './BotBubble'
-import {MarkerAnimated} from 'react-native-maps'
 import {observer} from 'mobx-react/native'
 import {isAlive} from 'mobx-state-tree'
 import {IBot} from 'wocky-client'
+import HackMarker from './HackMarker'
 
 type Props = {
   bot: IBot
-  id?: string
   scale: number
   onImagePress: any
   image?: any
@@ -15,50 +14,22 @@ type Props = {
   showLoader?: boolean
 }
 
-@observer
-export default class BotMarker extends React.Component<Props> {
-  mounted = false
-  state = {tracking: true}
-
-  componentDidMount() {
-    this.mounted = true
-    setTimeout(() => this.mounted && this.setState({tracking: false}), 1000)
+const BotMarker = observer(({bot, scale, ...props}: Props) => {
+  const y = scale === 1 ? 0.5 : 1 // fullMap ? -35 : -106
+  if (!bot || !isAlive(bot) || !bot.location) {
+    return null
   }
+  return (
+    <HackMarker
+      identifier={bot.id}
+      coordinate={{latitude: bot.location.latitude, longitude: bot.location.longitude}}
+      onPress={props.onImagePress}
+      scale={scale}
+      {...props}
+    >
+      <BotBubble bot={bot} scale={scale} {...props} />
+    </HackMarker>
+  )
+})
 
-  componentWillUnmount() {
-    this.mounted = false
-  }
-
-  componentDidUpdate(_0, previousState) {
-    if (previousState.tracking && !this.state.tracking) {
-      return
-    }
-    if (!previousState.tracking && this.state.tracking) {
-      return
-    }
-    if (this.mounted && !this.state.tracking) {
-      this.setState({tracking: true})
-      setTimeout(() => this.mounted && this.setState({tracking: false}), 500)
-    }
-  }
-
-  render() {
-    const {id, bot, scale, style, ...props} = this.props
-    const y = scale === 1 ? 0.5 : 1 // fullMap ? -35 : -106
-    if (!bot || !isAlive(bot) || !bot.location) {
-      return null
-    }
-    return (
-      <MarkerAnimated
-        anchor={{x: 0.5, y}}
-        tracksViewChanges={this.state.tracking}
-        style={[{top: -2000}, style]} // DIRTY workaround to catch all onPress events for the marker.
-        identifier={bot.id}
-        coordinate={{latitude: bot.location.latitude, longitude: bot.location.longitude}}
-        onPress={props.onImagePress}
-      >
-        <BotBubble bot={bot} scale={scale} {...props} />
-      </MarkerAnimated>
-    )
-  }
-}
+export default BotMarker
