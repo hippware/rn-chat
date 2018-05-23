dc53c4349948ab16ff925ab61049028303efdc60
Eric Kirkham <eric@southerneer.com>
2018-05-23 13:16:02 -0700

    name intermediate screenshots

--- e2e/detox.spec.ts
+++ e2e/detox.spec.ts
@@ -17,15 +17,16 @@ describe('Detox', () => {
 
   it('should successfully bypass register new user and get to home screen', async () => {
     await expect(element(by.id('onboarding'))).toBeVisible()
+    takeScreenshot('onboarding-visible')
     await element(by.id('bypassButton')).tap()
     await element(by.id('bypassPhoneInput')).typeText('444')
     await element(by.id('bypassRegisterButton')).tap()
-    takeScreenshot()
+    takeScreenshot('register-tap')
 
     await waitFor(element(by.id('signUpTopRow')))
       .toBeVisible()
       .withTimeout(3000)
-    takeScreenshot()
+    takeScreenshot('signup-visible')
 
     // fill out the form and hit return on the last entry
     await element(by.id('signUpUsername')).tap()
@@ -36,6 +37,7 @@ describe('Detox', () => {
     await element(by.id('signUpLastName')).typeText('four')
     await element(by.id('signUpEmail')).tap()
     await element(by.id('signUpEmail')).typeText('email@email.com\n')
+    takeScreenshot('signup-form-filled')
 
     // TODO: what is the next "visible" element/screen before we eventually make it to the home screen?
     // using `waitFor` is kind of a hack until we figure this out
@@ -44,10 +46,12 @@ describe('Detox', () => {
     await waitFor(element(by.id('screenHome')))
       .toBeVisible()
       .withTimeout(4000)
+    takeScreenshot('home-screen')
   })
 
   it('should open sidemenu', async () => {
     await element(by.id('wrapper')).tapAtPoint(navLeftButtonCoords)
+    takeScreenshot('sidemenu-opening')
     await expect(element(by.id('myAccountMenuItem'))).toBeVisible()
   })
 
@@ -57,25 +61,30 @@ describe('Detox', () => {
     await waitFor(element(by.id('profileDetail')))
       .toBeVisible()
       .withTimeout(1000)
+    takeScreenshot('profile-detail')
   })
 
   it('should navigate to MyAccount screen', async () => {
     await element(by.id('myAccountEdit')).tap()
     await expect(element(by.id('profileInfo'))).toBeVisible()
+    takeScreenshot('my-account')
   })
 
   it('should logout and delete profile', async () => {
     await element(by.id('myAccountScrollView')).scrollTo('bottom')
     await element(by.id('deleteProfile')).tap()
+    takeScreenshot('delete-tap')
 
     // NOTE: apparently 2 dialogs are created (?) so we must use `atIndex` to match just one
     await element(by.text('Delete Profile'))
       .atIndex(1)
       .tap()
+    takeScreenshot('delete-profile-confirm')
 
     await waitFor(element(by.id('onboarding')))
       .toBeVisible()
       .withTimeout(2000)
+    takeScreenshot('onboarding-after-delete')
   })
 })
 
--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -10,10 +10,10 @@ const SCREENSHOT_OPTIONS = {
 
 let screenshotIndex = 0
 
-const takeScreenshot = () => {
+const takeScreenshot = (name?: string) => {
   try {
     if (!existsSync(SCREENSHOT_DIR)) mkdirSync(SCREENSHOT_DIR)
-    const screenshotFilename = `${SCREENSHOT_DIR}/screenshot-${screenshotIndex++}.png`
+    const screenshotFilename = `${SCREENSHOT_DIR}/${name || `screenshot-${screenshotIndex++}`}.png`
     execSync(`xcrun simctl io booted screenshot ${screenshotFilename}`, SCREENSHOT_OPTIONS)
   } catch (err) {
     // TODO: get screenshots working on Nevercode?
