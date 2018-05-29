300999d869a2bd57b73176978d7b5bdc6dc94cf7
aksonov <aksonov@gmail.com>
2018-05-29 15:51:23 +0200

    remove console

--- __tests__/utils/mockTransport.ts
+++ __tests__/utils/mockTransport.ts
@@ -5,7 +5,6 @@ export default class MockTransport extends XmppTransport {
   loginCount = 0
 
   async login(user?: string, password?: string, host?: string) {
-    console.log('LOGIN', this.loginCount)
     this.loginCount += 1
     if (this.loginCount !== 2) {
       await super.login(user, password, host)
