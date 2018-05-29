bdb19af00e354bb6608a4e3b5304ae1aec5b4e8f
Eric Kirkham <eric@southerneer.com>
2018-05-29 16:07:49 -0700

    ignore (safely) "isMounted" warnings

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,8 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
+import {AppRegistry, YellowBox} from 'react-native'
+import App from './src/App'
 
-AppRegistry.registerComponent('App', () => App);
+// remove this after upgrade to RN 0.56+
+// https://github.com/facebook/react-native/issues/18868#issuecomment-382671739
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
+
+AppRegistry.registerComponent('App', () => App)
