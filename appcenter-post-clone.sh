61d669fcbef08c53f2fcb4194e030cd8112f54e7
Eric Kirkham <eric@southerneer.com>
2018-06-06 16:11:37 -0700

    wip

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+echo "testing equality"
+echo "branch =" $APPCENTER_BRANCH
+echo "token =" $CODEPUSH_TOKEN_ERIC
+if [$APPCENTER_BRANCH -eq "deploy-stage"]
+then
+  echo "Codepush Staging rollback bundle"
+  ./node_modules/.bin/appcenter codepush release-react -a hippware/tinyrobot-2 -d StagingRollback --description "rollback to TestFlight version" --token $CODEPUSH_TOKEN_ERIC
+fi
+
 brew uninstall node@6
 NODE_VERSION="8.9.4"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
