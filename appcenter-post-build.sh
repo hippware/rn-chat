f2b2bdea558639325fcd6a9412caf76e0f03476b
Eric Kirkham <eric@southerneer.com>
2018-06-06 17:17:41 -0700

    post-clone script for deploy-stage only

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,6 +2,6 @@
 
 if [$APPCENTER_BRANCH -eq "deploy-stage"]
 then
-  echo "Codepush Staging rollback bundle"
+  echo "Codepush StagingRollback bundle"
   ./node_modules/.bin/appcenter codepush release-react -a hippware/tinyrobot-2 -d StagingRollback --description "rollback to TestFlight version" --token $CODEPUSH_TOKEN_ERIC
 fi
--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -7,25 +7,22 @@ sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 echo "Node version..."
 node --version
 
-yarn
-
-# echo "Codepush Staging rollback bundle"
-./node_modules/.bin/appcenter codepush release-react -a hippware/tinyrobot-2 -d StagingRollback --description "rollback to TestFlight version" --token $CODEPUSH_TOKEN_ERIC
-
+# these tests run on CircleCI
+# yarn
 # yarn test
 
-# echo "Installing applesimutils"
-# mkdir simutils
-# cd simutils
-# curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
-# tar xzvf applesimutils.tar.gz
-# sh buildForBrew.sh 
-# cd ..
-# export PATH=$PATH:./simutils/build/Build/Products/Release
+echo "Installing applesimutils"
+mkdir simutils
+cd simutils
+curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
+tar xzvf applesimutils.tar.gz
+sh buildForBrew.sh 
+cd ..
+export PATH=$PATH:./simutils/build/Build/Products/Release
 
-# echo "Detecting applesimutils"
-# which applesimutils
-# npm install detox-cli -g
-# echo 'Detox install'
-# set -ex
-# yarn detox
+echo "Detecting applesimutils"
+which applesimutils
+npm install detox-cli -g
+echo 'Detox install'
+set -ex
+yarn detox
