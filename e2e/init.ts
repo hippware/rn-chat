e8310582e3da4fe1bbf077890fd77b2d3dd37de4
Eric Kirkham <eric@southerneer.com>
2018-06-12 22:49:36 -0700

    disable invalid detox tests

--- e2e/init.ts
+++ e2e/init.ts
@@ -4,10 +4,10 @@ require('../third-party/wocky-client/test/support/setup.js')
 import {createXmpp} from '../third-party/wocky-client/test/support/testuser'
 
 before(async () => {
+  await clearTestUser()
   await detox1.init(config)
   // set permissions before running any tests: https://github.com/wix/detox/blob/master/docs/APIRef.DeviceObjectAPI.md#devicelaunchappparams
   await device.launchApp({permissions: {location: 'always', notifications: 'YES'}})
-  await clearTestUser()
 })
 
 after(async () => {
