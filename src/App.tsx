3f233ac3782db37a2fddda4b9628fa0e436d6d9f
Eric Kirkham <eric@southerneer.com>
2018-07-10 12:43:35 -0700

    move ErrorHandler inside Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import TinyRobotRouter from './components/Router'
 import analytics from './utils/analytics'
 import store, {notificationStore, reportStore, pushStore} from './store'
 import NotificationBanner from './components/NotificationBanner'
-import ErrorHandler from './components/common/ErrorHandler'
 import Connectivity from './components/Connectivity'
 import * as logger from './utils/log'
 // import TinyRobotRouter from './components/RouterTest';
@@ -21,13 +20,11 @@ const App = () => (
     pushStore={pushStore}
     {...logger}
   >
-    <ErrorHandler>
-      <View style={{flex: 1}} testID="wrapper">
-        <TinyRobotRouter />
-        <NotificationBanner />
-        <Connectivity />
-      </View>
-    </ErrorHandler>
+    <View style={{flex: 1}} testID="wrapper">
+      <TinyRobotRouter />
+      <NotificationBanner />
+      <Connectivity />
+    </View>
   </Provider>
 )
 
