d7c5a958e03823f6a536712f0dd66bb6622dec7a
Pavlo Aksonov <aksonov+github@gmail.com>
2018-07-11 17:35:23 +0200

    fix tsc error

--- third-party/wocky-client/src/store/Wocky.ts
+++ third-party/wocky-client/src/store/Wocky.ts
@@ -306,7 +306,7 @@ export const Wocky = types
       yield waitFor(() => self.connected)
       yield self.transport.unblock(username)
     }),
-    _hideUser: flow(function*(value: boolean, expire: Date = null) {
+    _hideUser: flow(function*(value: boolean, expire: Date | null = null) {
       yield waitFor(() => self.connected)
       yield self.transport.hideUser(value, expire)
     }),
