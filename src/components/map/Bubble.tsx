51ce43f2eee09fbd7e7d4024bde412c7a800904a
Eric Kirkham <eric@southerneer.com>
2018-07-11 15:42:53 -0700

    Generalize Bubble, delete BubbleIcon

--- src/components/map/Bubble.tsx
+++ src/components/map/Bubble.tsx
@@ -1,56 +1,63 @@
 import React from 'react'
-import {View, Image} from 'react-native'
+import {View, Image, StyleSheet, ViewStyle, ImageStyle} from 'react-native'
 import Triangle from './Triangle'
 import {RText} from '../common'
 import {colors} from '../../constants'
 import {observer} from 'mobx-react/native'
 
-// scale here - 1 is full image, 0.5 is bot details UI (half-screen), 0 is full map mode
 type Props = {
-  text: string
+  text?: string
   image: any
-  showLoader: boolean
+  showLoader?: boolean
   children?: any
+  style?: ViewStyle
+  imageStyle?: ImageStyle
+  size?: number
 }
+
+const defaultSize = 58
 
 @observer
 export default class Bubble extends React.Component<Props> {
   render() {
-    const {image, text, showLoader, children} = this.props
-    const width = 58
-    const height = 58
-    const borderRadius = 9.6
+    const {image, text, showLoader, children, style, imageStyle, size} = this.props
+    const theSize = size || defaultSize
 
     return (
       <View style={{alignItems: 'center'}}>
         <View
-          style={{
-            backgroundColor: colors.PINK,
-            borderRadius,
-            width,
-            height,
-            overflow: 'hidden',
-            borderWidth: 1.2,
-            borderColor: colors.PINK,
-          }}
+          style={[
+            styles.bubble,
+            {
+              width: theSize,
+              height: theSize,
+            },
+            style,
+          ]}
         >
           {showLoader ? (
-            <View style={{width, height: width, backgroundColor: colors.GREY}} />
+            <View style={{width: theSize, height: theSize, backgroundColor: colors.GREY}} />
           ) : (
-            <Image style={{width, height: width}} resizeMode="contain" source={image} />
+            <Image
+              style={[{width: theSize, height: theSize}, imageStyle]}
+              resizeMode="contain"
+              source={image}
+            />
           )}
 
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <RText
-              color={colors.WHITE}
-              size={13}
-              style={{padding: 2}}
-              numberOfLines={1}
-              ellipsizeMode="middle"
-            >
-              {text}
-            </RText>
-          </View>
+          {text && (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+              <RText
+                color={colors.WHITE}
+                size={13}
+                style={{padding: 2}}
+                numberOfLines={1}
+                ellipsizeMode="middle"
+              >
+                {text}
+              </RText>
+            </View>
+          )}
           {children}
         </View>
         <Triangle width={14} height={8} color={colors.PINK} direction="down" />
@@ -58,3 +65,15 @@ export default class Bubble extends React.Component<Props> {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  bubble: {
+    backgroundColor: colors.PINK,
+    overflow: 'hidden',
+    borderWidth: 1.2,
+    borderColor: colors.PINK,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 9.6,
+  },
+})
