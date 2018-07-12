713faafe854415aa5a62b49f4563f784e9575ca7
Eric Kirkham <eric@southerneer.com>
2018-07-12 16:49:44 -0700

    whoops

--- src/components/Home/RightPanel.tsx
+++ src/components/Home/RightPanel.tsx
@@ -21,7 +21,7 @@ const RightPanel = inject('homeStore')(
   observer(({homeStore: {listMode, toggleListMode}}: Props) => (
     <View style={styles.container} pointerEvents="box-none">
       <TouchableOpacity
-        onPress={() => Actions.geoHeaderPrimer()}
+        onPress={() => Actions.bottomMenu()}
         // TODO: remove this when the settings menu is done
         onLongPress={() => Actions.codePush()}
         style={styles.button}
