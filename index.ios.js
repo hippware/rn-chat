807ad992e554c02387bf5d6bf5a7c167d08f1cfb
Eric Kirkham <eric@southerneer.com>
2018-07-13 12:44:54 -0700

    generalize ignoreWarning

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,7 @@ YellowBox.ignoreWarnings([
   'Module RCTImageLoader requires main', // TODO: fixed in RN56. https://github.com/facebook/react-native/issues/17504
   'Module LRDRCTNativeEnv requires main', // react-native-native-env...but no new releases and appears unsupported
   'Module RNXMPP requires main', // not a big deal since we're moving away from XMPP gradually anyway
-  'RCTBridge required dispatch_sync to load CodePush', // https://github.com/Microsoft/react-native-code-push/issues/632#issuecomment-265962068
+  'RCTBridge required dispatch_sync to load', // https://github.com/Microsoft/react-native-code-push/issues/632#issuecomment-265962068
   'Required dispatch_sync to load constants', // https://github.com/Microsoft/react-native-code-push/issues/632#issuecomment-265962068
   'Class RCTCxxModule was not exported.', // https://github.com/facebook/react-native/issues/18201#issuecomment-384166348
 ])
