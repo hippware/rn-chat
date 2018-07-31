e352f2f535a946f6554464695fa9a9ffeea9a034
Pavel Aksonov <aksonov@gmail.com>
2018-07-31 17:21:51 +0200

    WIP: first draft (#2505)

--- src/components/BotCompose/IconSelector.tsx
+++ src/components/BotCompose/IconSelector.tsx
@@ -3,13 +3,21 @@ import {View, StyleSheet, Text, ViewStyle} from 'react-native'
 import {colors} from '../../constants'
 import {width} from '../Global'
 import Carousel from 'react-native-snap-carousel'
-import {Observer, inject} from 'mobx-react/native'
+import {Observer, observer, inject} from 'mobx-react/native'
 import {observable, action} from 'mobx'
 import {IHomeStore} from '../../store/HomeStore'
 import {IBot} from 'wocky-client'
+import EmojiSelector from 'react-native-emoji-selector'
 
-const {drinks, trees, heart, silverware, store, plane} = require('../../constants/botIcons.json')
-const icons = [undefined, silverware, drinks, trees, plane, store, heart]
+const {
+  drinks,
+  trees,
+  heart,
+  silverware,
+  store,
+  plane,
+  emoji,
+} = require('../../constants/botIcons.json')
 
 const itemSize = 53
 
@@ -20,29 +28,47 @@ type Props = {
 }
 
 @inject('homeStore')
+@observer
 class IconSelector extends React.Component<Props> {
+  @observable icons = [undefined, silverware, drinks, trees, plane, store, heart, emoji]
   selector?: any
   @observable index?: number = 0
+  @observable emojiSelected = false
+
+  @action
+  setIcon = icon => {
+    this.props.homeStore.setEditIcon(icon)
+    this.props.bot.load({icon})
+  }
 
   @action
   onSnap = (index: number) => {
     this.index = index
-    this.props.homeStore.setEditIcon(icons[index])
-    this.props.bot.load({icon: icons[index]})
+    this.emojiSelected = index === this.icons.length - 1
+    this.setIcon(this.icons[this.index])
   }
 
+  @action
+  onEmojiSelected = emoji => {
+    this.icons[this.index] = emoji
+    this.setIcon(this.icons[this.index])
+    this.emojiSelected = false
+  }
   render() {
     return (
       <View style={this.props.style}>
         <Carousel
           ref={r => (this.selector = r)}
-          data={icons}
+          data={this.icons.slice()}
           renderItem={this.renderIcon}
           sliderWidth={width}
           itemWidth={itemSize}
           onBeforeSnapToItem={this.onSnap}
           inactiveSlideOpacity={1}
         />
+        {this.emojiSelected && (
+          <EmojiSelector onEmojiSelected={this.onEmojiSelected} showSearchBar={false} columns={8} />
+        )}
       </View>
     )
   }
