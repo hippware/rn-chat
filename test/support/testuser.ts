828671f73137e37af2b2833ca543851871d1a9e1
Pavel Aksonov <aksonov@gmail.com>
2018-08-03 08:58:33 +0200

    improve bot tests

--- test/support/testuser.ts
+++ test/support/testuser.ts
@@ -3,7 +3,7 @@ import {Wocky, XmppTransport, IWocky, HybridTransport, GraphQLTransport} from '.
 import fileService from './fileService'
 import {simpleActionLogger} from 'mst-middlewares'
 import {addMiddleware} from 'mobx-state-tree'
-import {when} from 'mobx'
+import {when, reaction} from 'mobx'
 const fs = require('fs')
 
 export function testFile() {
@@ -11,7 +11,7 @@ export function testFile() {
   const file = {
     name: fileName.substring(fileName.lastIndexOf('/') + 1),
     body: fs.readFileSync(fileName),
-    type: 'image/jpeg',
+    type: 'image/jpeg'
   }
   const data = {height: 300, width: 300, size: 3801, file}
   return data
@@ -28,6 +28,7 @@ export async function createXmpp(num?: number, phoneNum?: string): Promise<IWock
     const xmppTransport = new XmppTransport(provider, 'testing')
     const gql = new GraphQLTransport('testing')
     const transport = new HybridTransport(xmppTransport, gql)
+
     // const provider = new XmppStropheV2(console.log)
     const phoneNumber =
       phoneNum ||
@@ -42,7 +43,7 @@ export async function createXmpp(num?: number, phoneNum?: string): Promise<IWock
       {
         transport,
         fileService,
-        logger: {log: (msg: string, ...params: any[]) => console.log(msg, ...params)},
+        logger: {log: (msg: string, ...params: any[]) => console.log(msg, ...params)}
       }
     )
     addMiddleware(service, simpleActionLogger)
@@ -55,12 +56,20 @@ export async function createXmpp(num?: number, phoneNum?: string): Promise<IWock
         emailAddressIsVerified: false,
         'X-Auth-Service-Provider': 'http://localhost:9999',
         emailAddress: '',
-        'X-Verify-Credentials-Authorization': '',
+        'X-Verify-Credentials-Authorization': ''
       },
       'digits'
     )
     console.log('credentials', service.username, service.password) // need it for debug with GraphiQL
     await service.login()
+    reaction(
+      () => service.transport.notification,
+      async data => {
+        if (service.connected) {
+          console.log('ONNOTIFICATION FOR USER:', service.username, JSON.stringify(data))
+        }
+      }
+    )
     return service
   } catch (e) {
     console.error(e)
@@ -114,7 +123,7 @@ export const homestreamTestData = {
                 {
                   var: 'jid',
                   type: 'jid',
-                  value: 'testing.dev.tinyrobot.com/bot/f9002b8a-0a52-11e8-b066-0a580a0206c7',
+                  value: 'testing.dev.tinyrobot.com/bot/f9002b8a-0a52-11e8-b066-0a580a0206c7'
                 },
                 {var: 'address', type: 'string'},
                 {var: 'address_data', type: 'string', value: 'null'},
@@ -124,8 +133,7 @@ export const homestreamTestData = {
                 {
                   var: 'image',
                   type: 'string',
-                  value:
-                    'tros:f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com/file/fab30420-0a52-11e8-ba6a-0a580a0206c7',
+                  value: 'tros:f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com/file/fab30420-0a52-11e8-ba6a-0a580a0206c7'
                 },
                 {
                   var: 'location',
@@ -133,8 +141,8 @@ export const homestreamTestData = {
                   geoloc: {
                     xmlns: 'http://jabber.org/protocol/geoloc',
                     lat: '1.300000',
-                    lon: '2.300000',
-                  },
+                    lon: '2.300000'
+                  }
                 },
                 {var: 'visibility', type: 'int', value: '100'},
                 {var: 'radius', type: 'float', value: '3.00000000000000000000e+01'},
@@ -144,7 +152,7 @@ export const homestreamTestData = {
                 {
                   var: 'subscribers+hash',
                   type: 'string',
-                  value: '28b6abe8d4038edbf742297f13af0786',
+                  value: '28b6abe8d4038edbf742297f13af0786'
                 },
                 {var: 'title', type: 'string', value: 'Test bot!'},
                 {var: 'type', type: 'string'},
@@ -152,8 +160,8 @@ export const homestreamTestData = {
                 {
                   var: 'owner',
                   type: 'jid',
-                  value: 'f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com',
-                },
+                  value: 'f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com'
+                }
               ],
               tags: [{var: 'tags', type: 'tags'}, null],
               subscribed: 'true',
@@ -169,15 +177,14 @@ export const homestreamTestData = {
                   'https://s3-us-west-2.amazonaws.com/wocky-kube-tros-testing/testing.dev.tinyrobot.com-6774/fab30420-0a52-11e8-ba6a-0a580a0206c7-thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJOOG6KJT7K4EHNCA%2F20180205%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180205T090019Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=b554b0a4cfe6bba954d6a91d04ead0017221e0542878be5479c4a9b1eef61446',
                 full_url:
                   'https://s3-us-west-2.amazonaws.com/wocky-kube-tros-testing/testing.dev.tinyrobot.com-6774/fab30420-0a52-11e8-ba6a-0a580a0206c7?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJOOG6KJT7K4EHNCA%2F20180205%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180205T090019Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=0b19a49e6893c31a7c5ccb071cd2c88dd17951c2256a7af1745c7f56628d376e',
-                '#text':
-                  'tros:f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com/file/fab30420-0a52-11e8-ba6a-0a580a0206c7',
+                '#text': 'tros:f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com/file/fab30420-0a52-11e8-ba6a-0a580a0206c7'
               },
               location: {
                 geoloc: {
                   xmlns: 'http://jabber.org/protocol/geoloc',
                   lat: '1.300000',
-                  lon: '2.300000',
-                },
+                  lon: '2.300000'
+                }
               },
               visibility: '100',
               radius: '3.00000000000000000000e+01',
@@ -185,10 +192,10 @@ export const homestreamTestData = {
               subscribers: {size: '1', hash: '28b6abe8d4038edbf742297f13af0786'},
               title: 'Test bot!',
               updated: '2018-02-05T09:00:19.649618Z',
-              owner: 'f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com',
-            },
-          },
-        },
+              owner: 'f57c6690-0a52-11e8-bc08-0a580a02057d@testing.dev.tinyrobot.com'
+            }
+          }
+        }
       },
       {
         id: 'testing.dev.tinyrobot.com/bot/45f3c018-09b4-11e8-a764-0a580a02057d',
@@ -201,9 +208,9 @@ export const homestreamTestData = {
             jid: 'testing.dev.tinyrobot.com/bot/45f3c018-09b4-11e8-a764-0a580a02057d',
             id: '45f3c018-09b4-11e8-a764-0a580a02057d',
             server: 'testing.dev.tinyrobot.com',
-            action: 'show',
-          },
-        },
+            action: 'show'
+          }
+        }
       },
       {
         id: 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d',
@@ -219,19 +226,19 @@ export const homestreamTestData = {
             jid: 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d',
             id: '3d577dbe-09b4-11e8-b7e5-0a580a02057d',
             server: 'testing.dev.tinyrobot.com',
-            action: 'share',
-          },
-        },
-      },
+            action: 'share'
+          }
+        }
+      }
     ],
     set: {
       xmlns: 'http://jabber.org/protocol/rsm',
       first: {
         index: '0',
-        '#text': 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d',
+        '#text': 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d'
       },
       last: 'testing.dev.tinyrobot.com/bot/45f3c018-09b4-11e8-a764-0a580a02057d',
-      count: '2',
+      count: '2'
     },
     'extra-data': {
       bot: [
@@ -244,7 +251,7 @@ export const homestreamTestData = {
             {
               var: 'jid',
               type: 'jid',
-              value: 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d',
+              value: 'testing.dev.tinyrobot.com/bot/3d577dbe-09b4-11e8-b7e5-0a580a02057d'
             },
             {var: 'address', type: 'string'},
             {var: 'address_data', type: 'string', value: 'null'},
@@ -254,8 +261,7 @@ export const homestreamTestData = {
             {
               var: 'image',
               type: 'string',
-              value:
-                'tros:39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com/file/3efde978-09b4-11e8-98a0-0a580a02057d',
+              value: 'tros:39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com/file/3efde978-09b4-11e8-98a0-0a580a02057d'
             },
             {
               var: 'location',
@@ -263,8 +269,8 @@ export const homestreamTestData = {
               geoloc: {
                 xmlns: 'http://jabber.org/protocol/geoloc',
                 lat: '1.300000',
-                lon: '2.300000',
-              },
+                lon: '2.300000'
+              }
             },
             {var: 'visibility', type: 'int', value: '100'},
             {var: 'radius', type: 'float', value: '3.00000000000000000000e+01'},
@@ -278,8 +284,8 @@ export const homestreamTestData = {
             {
               var: 'owner',
               type: 'jid',
-              value: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com',
-            },
+              value: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com'
+            }
           ],
           tags: [{var: 'tags', type: 'tags'}, null],
           subscribed: 'false',
@@ -295,11 +301,10 @@ export const homestreamTestData = {
               'https://s3-us-west-2.amazonaws.com/wocky-kube-tros-testing/testing.dev.tinyrobot.com-26b1/3efde978-09b4-11e8-98a0-0a580a02057d-thumbnail?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJOOG6KJT7K4EHNCA%2F20180204%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180204T140415Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=8712360347540deaf778ce626b3af91c5713f7a047ab8d39a5020c4104dc2619',
             full_url:
               'https://s3-us-west-2.amazonaws.com/wocky-kube-tros-testing/testing.dev.tinyrobot.com-26b1/3efde978-09b4-11e8-98a0-0a580a02057d?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJOOG6KJT7K4EHNCA%2F20180204%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180204T140415Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=74c8bc9f6404c17a51a32daee57303d59da4e072c9fa24311e788cc0d35cf9f3',
-            '#text':
-              'tros:39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com/file/3efde978-09b4-11e8-98a0-0a580a02057d',
+            '#text': 'tros:39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com/file/3efde978-09b4-11e8-98a0-0a580a02057d'
           },
           location: {
-            geoloc: {xmlns: 'http://jabber.org/protocol/geoloc', lat: '1.300000', lon: '2.300000'},
+            geoloc: {xmlns: 'http://jabber.org/protocol/geoloc', lat: '1.300000', lon: '2.300000'}
           },
           visibility: '100',
           radius: '3.00000000000000000000e+01',
@@ -307,7 +312,7 @@ export const homestreamTestData = {
           subscribers: {size: '0', hash: 'd41d8cd98f00b204e9800998ecf8427e'},
           title: 'Test bot!',
           updated: '2018-02-04T14:03:59.953149Z',
-          owner: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com',
+          owner: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com'
         },
         {
           xmlns: 'hippware.com/hxep/bot',
@@ -318,7 +323,7 @@ export const homestreamTestData = {
             {
               var: 'jid',
               type: 'jid',
-              value: 'testing.dev.tinyrobot.com/bot/45f3c018-09b4-11e8-a764-0a580a02057d',
+              value: 'testing.dev.tinyrobot.com/bot/45f3c018-09b4-11e8-a764-0a580a02057d'
             },
             {var: 'address', type: 'string'},
             {var: 'address_data', type: 'string', value: 'null'},
@@ -332,8 +337,8 @@ export const homestreamTestData = {
               geoloc: {
                 xmlns: 'http://jabber.org/protocol/geoloc',
                 lat: '1.200000',
-                lon: '2.200000',
-              },
+                lon: '2.200000'
+              }
             },
             {var: 'visibility', type: 'int', value: '100'},
             {var: 'radius', type: 'float', value: '3.00000000000000000000e+01'},
@@ -347,8 +352,8 @@ export const homestreamTestData = {
             {
               var: 'owner',
               type: 'jid',
-              value: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com',
-            },
+              value: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com'
+            }
           ],
           tags: [{var: 'tags', type: 'tags'}, null],
           subscribed: 'false',
@@ -359,7 +364,7 @@ export const homestreamTestData = {
           alerts: '0',
           id: '45f3c018-09b4-11e8-a764-0a580a02057d',
           location: {
-            geoloc: {xmlns: 'http://jabber.org/protocol/geoloc', lat: '1.200000', lon: '2.200000'},
+            geoloc: {xmlns: 'http://jabber.org/protocol/geoloc', lat: '1.200000', lon: '2.200000'}
           },
           visibility: '100',
           radius: '3.00000000000000000000e+01',
@@ -367,9 +372,9 @@ export const homestreamTestData = {
           subscribers: {size: '0', hash: 'd41d8cd98f00b204e9800998ecf8427e'},
           title: 'Test bot2',
           updated: '2018-02-04T14:04:10.911004Z',
-          owner: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com',
-        },
-      ],
-    },
-  },
+          owner: '39e09af8-09b4-11e8-80d4-0a580a02057d@testing.dev.tinyrobot.com'
+        }
+      ]
+    }
+  }
 }
