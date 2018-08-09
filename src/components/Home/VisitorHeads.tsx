abe0bd926418132722533bbc9fb50320459f7d9f
Eric Kirkham <eric@southerneer.com>
2018-08-09 11:38:19 -0700

    make VisitorHeads functional component

--- src/components/Home/VisitorHeads.tsx
+++ src/components/Home/VisitorHeads.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {View} from 'react-native'
 import {observer} from 'mobx-react/native'
-import {IBot, IWocky} from 'wocky-client'
+import {IBot} from 'wocky-client'
 
 import {k} from '../Global'
 import {Avatar, RText} from '../common'
@@ -9,49 +9,45 @@ import {colors} from '../../constants'
 
 type Props = {
   bot: IBot
-  wocky?: IWocky
 }
 
-@observer
-class VisitorHeads extends React.Component<Props> {
-  render() {
-    const {list} = this.props.bot.visitors!
-    const profile = list!.length ? list![0] : null
-    const rest = this.props.bot.visitorsSize - 1
-    const size = 30
-    return profile ? (
-      <View style={{position: 'absolute', top: -15, right: -8}}>
-        {rest > 0 && (
-          <View
-            style={{
-              height: size * k,
-              width: size * k,
-              borderRadius: size * k / 2,
-              backgroundColor: colors.PINK,
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              borderWidth: 2 * k,
-              borderColor: colors.WHITE,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <RText size={12} color={colors.WHITE} weight="Medium">
-              {`+${rest}`}
-            </RText>
-          </View>
-        )}
-        <Avatar
-          profile={profile}
-          tappable={false}
-          size={size}
-          hideDot
-          style={{position: 'absolute', right: rest > 0 ? 22 * k : 0, top: 0}}
-        />
-      </View>
-    ) : null
-  }
-}
+const VisitorHeads = observer(({bot}: Props) => {
+  const {visitors: {list}, visitorsSize} = bot
+  const profile = list!.length ? list![0] : null
+  const rest = visitorsSize - 1
+  const size = 30
+  return profile ? (
+    <View style={{position: 'absolute', top: -15, right: -8}}>
+      {rest > 0 && (
+        <View
+          style={{
+            height: size * k,
+            width: size * k,
+            borderRadius: size * k / 2,
+            backgroundColor: colors.PINK,
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            borderWidth: 2 * k,
+            borderColor: colors.WHITE,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <RText size={12} color={colors.WHITE} weight="Medium">
+            {`+${rest}`}
+          </RText>
+        </View>
+      )}
+      <Avatar
+        profile={profile}
+        tappable={false}
+        size={size}
+        hideDot
+        style={{position: 'absolute', right: rest > 0 ? 22 * k : 0, top: 0}}
+      />
+    </View>
+  ) : null
+})
 
 export default VisitorHeads
