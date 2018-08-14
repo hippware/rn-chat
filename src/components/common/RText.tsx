642f8ce1ca9e2e2a66b0dd402f6ff9acaeb1aa83
Eric Kirkham <eric@southerneer.com>
2018-08-14 15:22:24 -0700

    for #2564. Add Invite CTA

--- src/components/common/RText.tsx
+++ src/components/common/RText.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Text, TextProperties} from 'react-native'
+import {colors} from '../../constants'
 
 interface IProps extends TextProperties {
   size?: number
   color?: string
   weight?: 'Regular' | 'Bold' | 'Light' | 'Medium' | 'Italic'
   children?: any
+  pink?: boolean
 }
 
 const RText = (props: IProps) => {
-  const {children, color, size, weight, style, ...rest} = props
+  const {children, color, size, weight, style, pink, ...rest} = props
   const fontFamily = weight ? `Roboto-${weight}` : 'Roboto-Regular'
   const fontSize = size || 12
   return (
-    <Text style={[{color, fontSize, fontFamily}, style]} {...rest}>
+    <Text style={[{color: pink ? colors.PINK : color, fontSize, fontFamily}, style]} {...rest}>
       {children}
     </Text>
   )
