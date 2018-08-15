0566be9bbf2d28c365a4f21e614ffc53841514d7
Eric Kirkham <eric@southerneer.com>
2018-08-15 16:48:34 -0700

    friendlist search-ability

--- src/components/people-lists/FriendList.tsx
+++ src/components/people-lists/FriendList.tsx
@@ -13,73 +13,90 @@ import {observable} from 'mobx'
 
 type Props = {
   wocky?: IWocky
+  searchStore: any
 }
 
 const KeyboardAwareDraggablePopupList = withKeyboardHOC(DraggablePopupList)
 
 const searchIcon = require('../../../images/search.png')
 
-@inject('wocky')
+@inject('wocky', 'searchStore')
 @observer
 class FriendList extends React.Component<Props> {
-  @observable searchMode: boolean = true
+  @observable searchMode: boolean = false
+  input: any
+  list: any
 
   renderItem = ({item}) => <FriendCard profile={item} />
 
   render() {
-    const {friends} = this.props.wocky
+    const {searchStore: {localResult}} = this.props
     return (
       <KeyboardAwareDraggablePopupList
-        headerInner={
-          <View style={{width: 300 * k, alignSelf: 'center'}}>
-            {!this.searchMode ? (
-              <View>
-                <InviteFriendsRow />
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    marginTop: 20,
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <FriendCount count={friends.length} />
-                  <SearchButton
-                    onPress={() => (this.searchMode = !this.searchMode)}
-                    style={{alignSelf: 'flex-end'}}
-                  />
-                </View>
-              </View>
-            ) : (
-              <View
-                style={{
-                  borderColor: colors.LIGHT_GREY,
-                  borderBottomWidth: 1,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  paddingBottom: 10,
-                }}
-              >
-                <SearchButton
-                  onPress={() => (this.searchMode = !this.searchMode)}
-                  style={{marginRight: 10}}
-                />
-                <TextInput
-                  style={{
-                    flex: 1,
-                    fontSize: 16 * k,
-                    fontFamily: 'Roboto-Regular',
-                    color: colors.PURPLE,
-                  }}
-                />
-              </View>
-            )}
-          </View>
-        }
+        headerInner={this.renderHeader()}
         renderItem={this.renderItem}
         keyExtractor={item => item.id}
-        data={this.props.wocky.friends}
-        initialNumToRender={20}
+        data={localResult.slice(0, 20)}
+        keyboardDismissMode="none"
       />
+    )
+  }
+
+  renderHeader = () => {
+    const {searchStore, wocky} = this.props
+    return (
+      <View style={{width: 300 * k, alignSelf: 'center'}}>
+        {!this.searchMode ? (
+          <View>
+            <InviteFriendsRow />
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 20,
+                justifyContent: 'space-between',
+              }}
+            >
+              <FriendCount count={wocky.friends.length} />
+              <SearchButton
+                onPress={() => {
+                  this.searchMode = true
+                  setTimeout(() => this.input.focus(), 16)
+                }}
+                style={{alignSelf: 'flex-end'}}
+              />
+            </View>
+          </View>
+        ) : (
+          <View
+            style={{
+              borderColor: colors.LIGHT_GREY,
+              borderBottomWidth: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              paddingBottom: 10,
+            }}
+          >
+            <SearchButton
+              onPress={() => {
+                searchStore.setLocal('')
+                this.searchMode = false
+              }}
+              style={{marginRight: 10}}
+            />
+            <TextInput
+              style={{
+                flex: 1,
+                fontSize: 16 * k,
+                fontFamily: 'Roboto-Regular',
+                color: colors.PURPLE,
+              }}
+              ref={r => (this.input = r)}
+              onChangeText={searchStore.setLocal}
+              value={searchStore.local}
+            />
+          </View>
+        )}
+      </View>
     )
   }
 }
