8e85538f933d4b95a91d7cd9261e644825d6d7b9
Eric Kirkham <eric@southerneer.com>
2018-08-15 15:37:31 -0700

    initial styling

--- src/components/people-lists/FriendCard.tsx
+++ src/components/people-lists/FriendCard.tsx
@@ -5,6 +5,7 @@ import assert from 'assert'
 import ProfileItem from './ProfileItem'
 import {Actions} from 'react-native-router-flux'
 import {observer} from 'mobx-react/native'
+import {k} from '../Global'
 
 type Props = {
   profile: any
@@ -15,7 +16,10 @@ const FriendCard = observer(({profile, children}: Props) => {
   assert(profile, 'Profile is not defined')
   const backgroundColor = backgroundColorCardDay
   return (
-    <TouchableOpacity onPress={() => Actions.profileDetails({item: profile.id})}>
+    <TouchableOpacity
+      onPress={() => Actions.profileDetails({item: profile.id})}
+      style={{width: 300 * k, alignSelf: 'center'}}
+    >
       <ProfileItem profile={profile} style={{backgroundColor}}>
         {children}
       </ProfileItem>
--- src/components/people-lists/FriendList.tsx
+++ src/components/people-lists/FriendList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, View, TextInput, TouchableOpacity, Image} from 'react-native'
 import {observer, inject} from 'mobx-react/native'
 
 import {k} from '../Global'
@@ -7,65 +7,101 @@ import FriendCard from './FriendCard'
 import {colors} from '../../constants'
 import {RText, DraggablePopupList} from '../common'
 import InviteFriendsRow from './InviteFriendsRow'
+import withKeyboardHOC from '../common/withKeyboardHOC'
+import {IWocky} from 'wocky-client'
+import {observable} from 'mobx'
 
 type Props = {
-  wocky: any
+  wocky?: IWocky
 }
+
+const KeyboardAwareDraggablePopupList = withKeyboardHOC(DraggablePopupList)
+
+const searchIcon = require('../../../images/search.png')
 
 @inject('wocky')
 @observer
-class FriendListScene extends React.Component<Props> {
-  searchText: any
+class FriendList extends React.Component<Props> {
+  @observable searchMode: boolean = true
 
   renderItem = ({item}) => <FriendCard profile={item} />
 
   render() {
     const {friends} = this.props.wocky
     return (
-      <DraggablePopupList
+      <KeyboardAwareDraggablePopupList
         headerInner={
-          <View>
-            <InviteFriendsRow />
-            <FriendCount
-              count={friends.length}
-              suffix={friends.length === 1 ? 'Friend' : 'Friends'}
-            />
+          <View style={{width: 300 * k, alignSelf: 'center'}}>
+            {!this.searchMode ? (
+              <View>
+                <InviteFriendsRow />
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginTop: 20,
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <FriendCount count={friends.length} />
+                  <SearchButton
+                    onPress={() => (this.searchMode = !this.searchMode)}
+                    style={{alignSelf: 'flex-end'}}
+                  />
+                </View>
+              </View>
+            ) : (
+              <View
+                style={{
+                  borderColor: colors.LIGHT_GREY,
+                  borderBottomWidth: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  paddingBottom: 10,
+                }}
+              >
+                <SearchButton
+                  onPress={() => (this.searchMode = !this.searchMode)}
+                  style={{marginRight: 10}}
+                />
+                <TextInput
+                  style={{
+                    flex: 1,
+                    fontSize: 16 * k,
+                    fontFamily: 'Roboto-Regular',
+                    color: colors.PURPLE,
+                  }}
+                />
+              </View>
+            )}
           </View>
         }
         renderItem={this.renderItem}
         keyExtractor={item => item.id}
         data={this.props.wocky.friends}
+        initialNumToRender={20}
       />
-      // </PeopleSearchWrapper>
     )
   }
 }
 
-const FriendCount = ({count, suffix}) =>
+const SearchButton = ({onPress, style}) => (
+  <TouchableOpacity style={style} onPress={onPress}>
+    <Image source={searchIcon} />
+  </TouchableOpacity>
+)
+
+const FriendCount = ({count}) =>
   count > 0 ? (
     <View style={styles.friendCount}>
-      <RText size={13}>
-        <RText size={16} weight="Bold">
-          {count}
-        </RText>
-        {` ${suffix}`}
+      <RText size={16} weight="Bold">
+        {count}
+        {` ${count === 1 ? 'Friend' : 'Friends'}`}
       </RText>
     </View>
   ) : null
 
-export default FriendListScene
+export default FriendList
 
 const styles = StyleSheet.create({
-  friendCount: {
-    backgroundColor: '#F1F2F3',
-    paddingHorizontal: 15 * k,
-    paddingBottom: 10 * k,
-    paddingTop: 10 * k,
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: colors.DARK_GREY,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: colors.DARK_GREY,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
+  friendCount: {},
 })
