4f4e4c11a5efc51e8616d2f19603854a8ec5728e
Eric Kirkham <eric@southerneer.com>
2018-08-30 15:25:55 -0700

    Bot Comment (post) card

--- src/components/event-cards/EventBotPostCard.tsx
+++ src/components/event-cards/EventBotPostCard.tsx
@@ -1,56 +1,32 @@
 import React from 'react'
-import {View, Image} from 'react-native'
 import {observer} from 'mobx-react/native'
 import {Actions} from 'react-native-router-flux'
-import EventBotTitle from './EventBotTitle'
-import {RText} from '../common'
-import {width} from '../Global'
-import EventBotMetabar from './EventBotMetabar'
 import {IEventBotPost} from 'wocky-client'
+import EventCardTemplate from './EventCardTemplate'
 
 type Props = {
   item: IEventBotPost
 }
 
-const imageWidth = 2 * width / 3
+const geoIcon = require('../../../images/notificationGeo.png')
 
 @observer
-class EventBotPostCard extends React.Component<Props> {
+export default class EventBotInviteCard extends React.Component<Props> {
   onPress() {
     Actions.botDetails({item: this.props.item.bot.id})
   }
 
   render() {
-    const {item} = this.props
+    const {bot, relativeDateAsString, post} = this.props.item
+
     return (
-      item.bot &&
-      item.post && (
-        <View>
-          <EventBotTitle
-            bot={item.bot}
-            action="added a post to"
-            timestamp={item.relativeDateAsString}
-            profile={item.post.profile!}
-          />
-          <View style={{marginHorizontal: 15}}>
-            {!!item.post.content && (
-              <RText size={15} weight="Light" style={{marginTop: 15}} numberOfLines={4}>
-                {item.post.content}
-              </RText>
-            )}
-            {item.post.image && (
-              <Image
-                source={item.post.image.source}
-                resizeMode="contain"
-                style={{height: imageWidth, width: imageWidth, marginTop: 15}}
-              />
-            )}
-          </View>
-          <EventBotMetabar bot={item.bot} />
-        </View>
-      )
+      <EventCardTemplate
+        profile={post.profile}
+        icon={geoIcon}
+        timestamp={relativeDateAsString}
+        action="commentedOn"
+        line2={bot.title}
+      />
     )
   }
 }
-
-export default EventBotPostCard
