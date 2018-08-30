645243bb7447342a017d13091940eb458b2f0fec
Eric Kirkham <eric@southerneer.com>
2018-08-30 14:29:10 -0700

    GradientButton

---
+++ src/components/common/GradientButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {StyleSheet, TouchableOpacity, TouchableOpacityProps, ViewStyle} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import {colors} from '../../constants'
+
+const pink = ['rgb(242,68,191)', 'rgb(254,110,98)', 'rgb(254,92,108)']
+const grey = [colors.DARK_GREY, colors.DARK_GREY]
+
+interface IProps extends TouchableOpacityProps {
+  isPink: boolean
+  offColor?: string
+  children: any
+  innerStyle?: ViewStyle
+}
+
+const GradientButton = ({isPink, offColor, children, style, innerStyle, ...rest}: IProps) => (
+  <TouchableOpacity style={[{overflow: 'hidden'}, style]} {...rest}>
+    <LinearGradient
+      start={{x: 0, y: 0.5}}
+      end={{x: 1, y: 0.5}}
+      colors={isPink ? pink : offColor ? [offColor, offColor] : grey}
+      style={[styles.gradient, innerStyle]}
+    >
+      {children}
+    </LinearGradient>
+  </TouchableOpacity>
+)
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default GradientButton
