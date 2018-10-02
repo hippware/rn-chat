bc8dd8f0f9d946a6eca822d66f185d2456b22b79
Eric Kirkham <eric@southerneer.com>
2018-10-01 17:02:23 -0700

    for #2564 - load guests

--- src/components/BotDetails/BotDetailsHeader.tsx
+++ src/components/BotDetails/BotDetailsHeader.tsx
@@ -175,15 +175,14 @@ class BotDetailsHeader extends React.Component<Props, State> {
   }
 }
 
-const VisitorsArea = ({bot}: {bot: IBot}) => {
-  let list: IProfile[], size: number, text: string
+const VisitorsArea = observer(({bot}: {bot: IBot}) => {
+  let list: IProfile[], text: string, onPress
   if (bot.visitors.list.length > 0) {
     list = bot.visitors.list
-    size = bot.visitorsSize
     text = 'are currently here!'
-  } else if (bot.guests.list.length > 0) {
-    list = bot.guests.list
-    size = bot.guestsSize
+    onPress = () => Actions.visitors({botId: bot.id})
+  } else if (bot.guests.list.length > 1) {
+    list = bot.guests.list.filter((g: IProfile) => !g.isOwn)
     text = 'accepted the invite!'
   }
   let inner = null
@@ -193,7 +192,7 @@ const VisitorsArea = ({bot}: {bot: IBot}) => {
       <Separator style={{marginHorizontal: 5, width: '100%', marginBottom: 30}} key="1" />,
       <TouchableOpacity
         key="2"
-        onPress={() => Actions.visitors({botId: bot.id})}
+        onPress={onPress}
         style={{
           alignItems: 'center',
           justifyContent: 'center',
@@ -204,7 +203,7 @@ const VisitorsArea = ({bot}: {bot: IBot}) => {
       >
         <ProfileStack
           firstProfile={list[0]}
-          stackSize={size}
+          stackSize={list.length}
           circleSize={50}
           textSize={16.5}
           style={{marginBottom: 10 * k}}
@@ -231,7 +230,7 @@ const VisitorsArea = ({bot}: {bot: IBot}) => {
       {!((bot.owner && bot.owner.isOwn) || list) && <View style={{height: 17}} />}
     </VisitorsWrapper>
   )
-}
+})
 
 const VisitorsWrapper = ({children}) => (
   <View style={{alignItems: 'center'}}>
