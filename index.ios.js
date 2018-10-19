76f906535aa1c35d0314b8a55c4c36620c42e311
Eric Kirkham <eric@southerneer.com>
2018-10-18 22:34:15 -0700

    DO NOT MERGE: codepush to prod

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,4 @@
-import {AppRegistry, YellowBox} from 'react-native'
-import App from './src/App'
+import {AppRegistry} from 'react-native'
+import UpdateBrick from './src/components/UpdateBrick'
 
-YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated', // TODO: fixed in RN56. https://github.com/facebook/react-native/issues/18868#issuecomment-382671739
-  'Module RCTImageLoader requires main', // TODO: fixed in RN56. https://github.com/facebook/react-native/issues/17504
-  'Module LRDRCTNativeEnv requires main', // react-native-native-env...but no new releases and appears unsupported
-  'Module RNXMPP requires main', // not a big deal since we're moving away from XMPP gradually anyway
-  'RCTBridge required dispatch_sync to load', // https://github.com/Microsoft/react-native-code-push/issues/632#issuecomment-265962068
-  'Required dispatch_sync to load constants', // https://github.com/Microsoft/react-native-code-push/issues/632#issuecomment-265962068
-  'Class RCTCxxModule was not exported.', // https://github.com/facebook/react-native/issues/18201#issuecomment-384166348
-])
-
-AppRegistry.registerComponent('App', () => App)
+AppRegistry.registerComponent('App', () => UpdateBrick)
