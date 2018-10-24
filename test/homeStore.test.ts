b2dd64bd6bca8051dcfde513ef634e75adb15f3a
aksonov <aksonov+github@gmail.com>
2018-10-24 17:07:38 +0200

    fix homeStore test

--- test/homeStore.test.ts
+++ test/homeStore.test.ts
@@ -1,19 +1,42 @@
 import {expect} from 'chai'
 import HomeStore from '../src/store/HomeStore'
 import {Bot} from 'wocky-client'
-import {destroy} from 'mobx-state-tree'
+import {types} from 'mobx-state-tree'
 
-const homeStore = HomeStore.create()
 describe('homeStore test', () => {
   it('add/select/remove bot', async done => {
     try {
-      const bot = Bot.create({id: '123'})
-      const bot2 = Bot.create({id: '1234'})
-      homeStore.selectBot(bot)
-      homeStore.selectBot(bot2)
-      expect(homeStore.index).to.be.equal(3)
-      homeStore.removeBot(bot2)
-      expect(homeStore.index).to.be.equal(2)
+      const model = types
+        .model({
+          homeStore: HomeStore,
+          bot: Bot,
+          bot2: Bot,
+        })
+        .create(
+          {bot: {id: '123'}, homeStore: {}, bot2: {id: '1234'}},
+          {
+            wocky: {
+              _loadBotSubscribers: () => {
+                //
+              },
+              _loadBotGuests: () => {
+                //
+              },
+              _loadBotVisitors: () => {
+                //
+              },
+              _loadBotPosts: () => {
+                //
+              },
+            },
+          }
+        )
+
+      model.homeStore.selectBot(model.bot)
+      model.homeStore.selectBot(model.bot2)
+      expect(model.homeStore.index).to.be.equal(3)
+      model.homeStore.removeBot(model.bot2)
+      expect(model.homeStore.index).to.be.equal(2)
       done()
     } catch (e) {
       done(e)
