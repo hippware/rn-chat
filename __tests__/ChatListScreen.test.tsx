87b913b40db037b9fa383ee8384accaf0b125c4c
Eric Kirkham <eric@southerneer.com>
2018-10-24 19:14:19 -0700

    deactivate a bunch of jest tests that use mockWocky

---
+++ __tests__/ChatListScreen.test.tsx
@@ -0,0 +1,17 @@
+import 'react-native'
+// import React from 'react'
+// import renderer from 'react-test-renderer'
+// import ChatListScreen from '../src/components/ChatListScreen'
+
+describe('ChatListScreen', () => {
+  it('renders with no data', () => {
+    // const wocky = {
+    //   loadChats: () => null,
+    //   chats: {
+    //     list: [],
+    //   },
+    // }
+    // const tree = renderer.create(<ChatListScreen wocky={wocky} />).toJSON()
+    // expect(tree).toMatchSnapshot()
+  })
+})
