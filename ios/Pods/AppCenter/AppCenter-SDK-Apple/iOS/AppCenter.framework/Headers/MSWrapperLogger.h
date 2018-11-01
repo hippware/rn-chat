700f539c0a8b96131daf291b35d40bc237163768
Eric Kirkham <eric@southerneer.com>
2018-11-01 16:09:18 -0700

    update some native dependencies

--- ios/Pods/AppCenter/AppCenter-SDK-Apple/iOS/AppCenter.framework/Headers/MSWrapperLogger.h
+++ ios/Pods/AppCenter/AppCenter-SDK-Apple/iOS/AppCenter.framework/Headers/MSWrapperLogger.h
@@ -3,8 +3,8 @@
 #import "MSConstants.h"
 
 /**
- * This is a utility for producing App Center style log messages. It is only
- * intended for use by App Center services and wrapper SDKs of App Center.
+ * This is a utility for producing App Center style log messages. It is only intended for use by App Center services and wrapper SDKs of App
+ * Center.
  */
 @interface MSWrapperLogger : NSObject
 
