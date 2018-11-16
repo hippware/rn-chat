39ec3ae216210eaf9e81c2c842ecfe8fa784781b
Beng Tan <bengtan@users.noreply.github.com>
2018-11-16 11:46:42 +0800

    Hand cherry pick #3009 for speculative testing.

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -15,14 +15,15 @@ import {ValidatableProfile} from '../utils/formValidation'
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {IWocky} from 'wocky-client'
 import Screen from './Screen'
-import {PINK} from '../constants/colors'
 import {settings} from '../globals'
-import Version from './Version'
 import {phoneFormat} from '../utils/misc'
+const {version} = require('../../package.json')
+import {ICodePushStore} from '../store/CodePushStore'
 
 type Props = {
   wocky?: IWocky
   profileValidationStore?: any
+  codePushStore?: ICodePushStore
 }
 
 @inject('wocky', 'profileValidationStore')
@@ -207,7 +208,7 @@ const LinkButton = ({
   style?: any
 }) => (
   <TouchableOpacity onPress={onPress}>
-    <RText size={15} color={PINK} style={[styles.text, style]}>
+    <RText size={15} color={colors.PINK} style={[styles.text, style]}>
       {children}
     </RText>
   </TouchableOpacity>
@@ -255,6 +256,34 @@ const Right = inject('profileValidationStore', 'wocky')(
         </RText>
       </TouchableOpacity>
     ) : null
+  })
+)
+
+const Version = inject('codePushStore')(
+  observer(({codePushStore}) => {
+    let versionString = settings.version // version from XCode
+    const packageJsonVersion = version // version from package.json
+
+    if (codePushStore!.metadata) {
+      const {deploymentKey, label} = codePushStore!.metadata
+
+      // Prod: rXt3kcwtaG9O8dzljOTZIDYvM8VUSJz03CBgQ
+      if (deploymentKey !== 'rXt3kcwtaG9O8dzljOTZIDYvM8VUSJz03CBgQ') {
+        const deploymentInfo = codePushStore.channels.filter(
+          deployment => deployment.key === deploymentKey
+        )
+        const deploymentName = deploymentInfo.length > 0 ? deploymentInfo[0].name : deploymentKey
+
+        versionString = `${versionString}-${deploymentName}`
+      }
+      versionString = `${versionString}-${label} (${packageJsonVersion})`
+    }
+
+    return (
+      <RText size={15} color={colors.DARK_GREY} style={{marginBottom: 15}}>
+        {`Version ${versionString}`}
+      </RText>
+    )
   })
 )
 
