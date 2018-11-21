c81ec5f6371f9b8a41c590326cfe476dbbfd90e4
Eric Kirkham <eric@southerneer.com>
2018-11-21 13:47:55 -0800

    use question mark as default emoji

--- src/components/Home/UberMarker.tsx
+++ src/components/Home/UberMarker.tsx
@@ -43,7 +43,7 @@ const UberMarker = inject('iconStore', 'homeStore')(
             imageStyle={{width: 20, height: 20}}
             size={48}
           >
-            <BotIcon icon={emoji!} size={20} />
+            <BotIcon icon={emoji || '❔'} size={24} />
           </Bubble>
           {isIconEditable && <Image source={tapToChange} style={styles.changeCTA as any} />}
         </Wrapper>
