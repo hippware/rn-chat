b434d4b73e48cd1b49fff609478d4a552c63bbfd
Jinyuan Qiu (Alan) <irfirl@gmail.com>
2018-11-20 16:25:11 -0800

    Added onboarding Accelerometer / Location page UI

--- src/components/Onboarding/OnboardingAccelerometer.tsx
+++ src/components/Onboarding/OnboardingAccelerometer.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {View} from 'react-native'
-import {RText} from '../common'
+import {View, StyleSheet, Image} from 'react-native'
+import {RText, GradientButton} from '../common'
+import {WHITE} from 'src/constants/colors'
+import {k} from '../Global'
 
 type Props = {
   onPress: () => void // the function that gets called when the user presses "Allow Accelerometer"
@@ -11,12 +13,52 @@ class OnboardingLocation extends React.Component<Props> {
     return (
       // TODO: @irfirl to add components and styling
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <RText size={30} style={{textAlign: 'center'}}>
-          Onboarding - Accelerometer
-        </RText>
+        <View style={{width: '80%', marginBottom: 33 * k}}>
+          <RText style={styles.onboardingH1}>Allow Accelerometer</RText>
+        </View>
+
+        <View style={{width: '70%', marginBottom: 42 * k}}>
+          <RText style={styles.onboardingSubtext}>
+            Using the accelerometer increases battery-efficiency by intelligently toggling
+            location-tracking while moving.
+          </RText>
+        </View>
+
+        <View style={{marginBottom: 82 * k}}>
+          <Image source={require('../../../images/walkingMan.png')} />
+        </View>
+
+        <GradientButton
+          isPink
+          style={{height: 50, width: '80%', borderRadius: 4}}
+          onPress={this.props.onPress}
+        >
+          <RText color={WHITE} size={17.5}>
+            Allow Accelerometer
+          </RText>
+        </GradientButton>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  almostDone: {
+    color: '#3f324d',
+    fontSize: 18,
+  },
+  onboardingH1: {
+    color: '#fe5c6c',
+    fontSize: 30,
+    fontWeight: '100',
+    textAlign: 'center',
+  },
+  onboardingSubtext: {
+    color: '#757575',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '100',
+  },
+})
 
 export default OnboardingLocation
--- src/components/Onboarding/OnboardingLocation.tsx
+++ src/components/Onboarding/OnboardingLocation.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {View} from 'react-native'
-import {RText} from '../common'
+import {View, StyleSheet, Image, Text} from 'react-native'
+import {RText, GradientButton} from '../common'
+import {WHITE} from 'src/constants/colors'
+import {k} from '../Global'
 
 type Props = {
   onPress: () => void // the function that gets called when the user presses "Always Allow Location"
@@ -11,15 +13,56 @@ class OnboardingLocation extends React.Component<Props> {
     return (
       // TODO: @irfirl to add components and styling
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <RText size={30} style={{textAlign: 'center'}}>
-          Onboarding - Location
-        </RText>
-        <RText size={20} style={{textAlign: 'center'}}>
-          @irfirl to add some sweet components and styles here
-        </RText>
+        <View style={{width: '80%', marginBottom: 32 * k}}>
+          <RText style={styles.onboardingH1}>Allow Location Access</RText>
+        </View>
+
+        <View style={{width: '70%', marginBottom: 67 * k}}>
+          <RText style={styles.onboardingSubtext}>
+            With '<Text style={{color: '#fe5c6c'}}>Always Allow</Text>' you won’t miss out on
+            relevant location notifications.
+          </RText>
+        </View>
+
+        <View style={{marginBottom: 37 * k}}>
+          <Image source={require('../../../images/001Gps.png')} />
+        </View>
+
+        <View style={{marginBottom: 37 * k}}>
+          <RText style={styles.onboardingSubtext}>You can also go invisible later 😉</RText>
+        </View>
+
+        <GradientButton
+          isPink
+          style={{height: 50, width: '80%', borderRadius: 4}}
+          onPress={this.props.onPress}
+        >
+          <RText color={WHITE} size={17.5}>
+            Always Allow Location
+          </RText>
+        </GradientButton>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  almostDone: {
+    color: '#3f324d',
+    fontSize: 18,
+  },
+  onboardingH1: {
+    color: '#fe5c6c',
+    fontSize: 30,
+    fontWeight: '100',
+    textAlign: 'center',
+  },
+  onboardingSubtext: {
+    color: '#757575',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '100',
+  },
+})
 
 export default OnboardingLocation
