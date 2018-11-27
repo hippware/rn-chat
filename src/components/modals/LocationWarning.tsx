afc1d2f4f04c139ad69420dd997cd1ea1e8d3804
Jinyuan Qiu (Alan) <irfirl@gmail.com>
2018-11-26 17:06:22 -0800

    Updated Location Permission Warning Page

--- src/components/modals/LocationWarning.tsx
+++ src/components/modals/LocationWarning.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import {View, StyleSheet, Text, Image, TouchableOpacity, Linking} from 'react-native'
+import {StyleSheet, Text, Image, Linking} from 'react-native'
 import {when} from 'mobx'
 import {Actions} from 'react-native-router-flux'
 import {colors} from '../../constants'
-import {k} from '../Global'
+import {k, s} from '../Global'
 import {observer, inject} from 'mobx-react/native'
 import {ILocationStore} from '../../store/LocationStore'
 import {BlurView} from 'react-native-blur'
 import globalStyles from '../styles'
-
-const botIcon = require('../../../images/iconBot.png')
+import {GradientButton, RText} from '../common'
+import {WHITE} from 'src/constants/colors'
 
 type Props = {
   locationStore?: ILocationStore
@@ -34,27 +34,34 @@ class LocationWarning extends React.Component<Props> {
   }
 }
 
-// TODO: @irfirl to tweak the design here
 export const LocationWarningUI = ({onPress}) => (
   <BlurView
     blurType="xlight"
     blurAmount={10}
     style={[globalStyles.absolute, {alignItems: 'center', justifyContent: 'center'}] as any}
   >
-    <Text style={[styles.title, {textAlign: 'center'}]}>{'Allow Location\r\nAccess'}</Text>
+    <Text style={styles.title}>
+      Tap “<Text style={{fontFamily: 'Roboto-Medium'}}>Always</Text>” to let tinyrobot work
+      perfectly.
+    </Text>
     <Image
-      source={botIcon}
-      style={{width: 60, height: 60, marginVertical: 15 * k}}
+      source={require('../../../images/fixLocationPermission.png')}
+      style={{width: 224, marginVertical: 50 * s}}
       resizeMode="contain"
     />
-    <Text style={[styles.muted, {textAlign: 'center'}]}>
-      {"We need your location to show you\r\nwhat's happening nearby!"}
+    <Text style={styles.subtext}>
+      With ‘Always’ access you won’t miss out on relevant location notifications.
     </Text>
-    <View style={{flexDirection: 'row', marginVertical: 20 * k}}>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.btnText}>Change Settings</Text>
-      </TouchableOpacity>
-    </View>
+
+    <GradientButton
+      isPink
+      style={{height: 50, width: '80%', borderRadius: 4, marginBottom: 26 * s, marginTop: 40 * s}}
+      onPress={onPress}
+    >
+      <RText color={WHITE} size={18.5}>
+        Open Settings
+      </RText>
+    </GradientButton>
   </BlurView>
 )
 
@@ -64,29 +71,16 @@ const styles = StyleSheet.create({
   title: {
     marginTop: 10 * k,
     color: colors.PINK,
-    fontSize: 30,
-    lineHeight: 32 * k,
+    fontSize: 28,
     fontFamily: 'Roboto-Light',
+    textAlign: 'center',
+    width: '80%',
   },
-  muted: {
+  subtext: {
     fontFamily: 'Roboto-Regular',
-    fontSize: 15,
+    fontSize: 17,
     color: colors.DARK_GREY,
-    marginTop: 5 * k,
-  },
-  button: {
-    flex: 1,
-    height: 50 * k,
-    borderWidth: 0,
-    borderRadius: 5 * k,
-    backgroundColor: colors.PINK,
-    alignItems: 'center',
-    marginHorizontal: 5 * k,
-    justifyContent: 'center',
-  },
-  btnText: {
-    fontSize: 15 * k,
-    fontFamily: 'Roboto-Regular',
-    color: colors.WHITE,
+    textAlign: 'center',
+    width: '70%',
   },
 })
