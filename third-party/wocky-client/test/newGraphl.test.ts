f6dc18de8e4f14dec9ffcfba7b9608b4e51371ce
Eric Kirkham <eric@southerneer.com>
2018-11-28 12:22:06 -0800

    no need for `done` or try/catch...errors in tests will cause test failure

--- third-party/wocky-client/test/newGraphl.test.ts
+++ third-party/wocky-client/test/newGraphl.test.ts
@@ -5,44 +5,29 @@ let user, user2: IWocky
 let user1phone: string
 
 describe('NewGraphQL tests', () => {
-  it('get user1 credential via GraphQL', async done => {
-    try {
-      timestamp()
-      user = await createGraphQL()
-      done()
-    } catch (e) {
-      done(e)
-    }
+  it('get user1 credential via GraphQL', async () => {
+    timestamp()
+    user = await createGraphQL()
   })
 
-  it('get user2 credentials via GraphQL', async done => {
-    try {
-      timestamp()
-      user2 = await createGraphQL()
-      done()
-    } catch (e) {
-      done(e)
-    }
+  it('get user2 credentials via GraphQL', async () => {
+    timestamp()
+    user2 = await createGraphQL()
   })
 
-  it('update profile', async done => {
-    try {
-      timestamp()
-      await waitFor(
-        () => user.profile !== null && user.profile.phoneNumber !== null,
-        'user1 profile to load'
-      )
-      user1phone = user.profile.phoneNumber
-      await user.profile!.update({
-        handle: 'a' + user1phone.replace('+', ''),
-        firstName: 'name1',
-        lastName: 'lname1',
-        email: 'a@aa.com',
-      })
-      done()
-    } catch (e) {
-      done(e)
-    }
+  it('update profile', async () => {
+    timestamp()
+    await waitFor(
+      () => user.profile !== null && user.profile.phoneNumber !== null,
+      'user1 profile to load'
+    )
+    user1phone = user.profile.phoneNumber
+    await user.profile!.update({
+      handle: 'a' + user1phone.replace('+', ''),
+      firstName: 'name1',
+      lastName: 'lname1',
+      email: 'a@aa.com',
+    })
   })
 
   it('cleans up users', async () => {
