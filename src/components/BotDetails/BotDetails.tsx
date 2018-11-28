be7a71a04db19be31a4f09c676005125bd1e536b
Jinyuan Qiu (Alan) <irfirl@gmail.com>
2018-11-28 15:34:08 -0800

    Added padding to bottom of bot page

--- src/components/BotDetails/BotDetails.tsx
+++ src/components/BotDetails/BotDetails.tsx
@@ -44,12 +44,11 @@ export default class BotDetails extends React.Component<Props> {
 
   _footerComponent: any = observer(() => {
     if (!this.bot) return null
-    return (
-      this.props.wocky!.connected &&
-      this.bot &&
-      isAlive(this.bot) &&
-      this.bot.posts.loading && <Loader />
-    )
+
+    if (this.props.wocky!.connected && this.bot && isAlive(this.bot) && this.bot.posts.loading)
+      return <Loader />
+
+    return <View style={{backgroundColor: 'white', height: 50 * k}} />
   })
 
   async componentDidMount() {
