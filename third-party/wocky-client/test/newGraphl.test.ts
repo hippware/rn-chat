262c237d6e980fa95570d9eb9b646adf72676ee6
Eric Kirkham <eric@southerneer.com>
2018-11-30 12:27:26 -0800

    restructure to allow independent bot tests

--- third-party/wocky-client/test/newGraphl.test.ts
+++ third-party/wocky-client/test/newGraphl.test.ts
@@ -1,4 +1,4 @@
-import {sleep, createUser, timestamp, waitFor} from './support/testuser'
+import {createUser, waitFor} from './support/testuser'
 import {IWocky} from '../src'
 import {getSnapshot} from 'mobx-state-tree'
 import {IBot} from '../src/model/Bot'
@@ -12,30 +12,13 @@ const icon = '\u00A9\uFE0F\u00A9'
 describe('NewGraphQL tests', () => {
   beforeAll(async () => {
     user = await createUser()
-    expect(user.username).toBeTruthy()
-    timestamp()
     user2 = await createUser()
-    expect(user2.username).toBeTruthy()
-    // not sure why they're the same
-    expect(user.username).not.toBe(user2.username)
+  })
 
-    // create bot
-    bot = await user.createBot()
-    expect(bot.icon).toBe('')
-    expect(bot.isNew).toBe(true)
-    bot.setUserLocation({latitude: 1, longitude: 2, accuracy: 1})
-    await bot.update({
-      icon,
-      public: false,
-      location: {latitude: 1.1, longitude: 2.1},
-      title: 'Test bot',
-      addressData: {city: 'Koper', country: 'Slovenia'},
-    })
-    expect(bot.icon).toBe(icon)
-    expect(bot.isNew).toBe(false)
-    expect(bot.title).toBe('Test bot')
-    expect(bot.location!.latitude).toBe(1.1)
-    expect(bot.location!.longitude).toBe(2.1)
+  it('checks created users', async () => {
+    expect(user.username).toBeTruthy()
+    expect(user2.username).toBeTruthy()
+    expect(user.username).not.toBe(user2.username)
   })
 
   it('update profile with invalid handle', async () => {
@@ -83,56 +66,86 @@ describe('NewGraphQL tests', () => {
     // // check profile is online
     // await waitFor(() => user2.sortedRoster[0].status === 'available', 'user2 not available in time')
   })
-  it('update bot location', async () => {
-    await bot.update({
-      location: {latitude: 1.3, longitude: 2.3},
-      title: 'Test bot!',
+
+  describe('bot stuff', () => {
+    beforeAll(async () => {
+      bot = await user.createBot()
+      // expect(bot.icon).toBe('')
+      // expect(bot.isNew).toBe(true)
+      bot.setUserLocation({latitude: 1, longitude: 2, accuracy: 1})
+      await bot.update({
+        icon,
+        public: false,
+        location: {latitude: 1.1, longitude: 2.1},
+        title: 'Test bot',
+        addressData: {city: 'Koper', country: 'Slovenia'},
+      })
     })
-    expect(bot.isNew).toBe(false)
-    expect(bot.title).toBe('Test bot!')
-    expect(bot.location!.latitude).toBe(1.3)
-    expect(bot.location!.longitude).toBe(2.3)
-  })
+    it('checks the bot', () => {
+      expect(bot.icon).toBe(icon)
+      expect(bot.isNew).toBe(false)
+      expect(bot.title).toBe('Test bot')
+      expect(bot.location!.latitude).toBe(1.1)
+      expect(bot.location!.longitude).toBe(2.1)
+    })
+    it('update bot location', async () => {
+      await bot.update({
+        location: {latitude: 1.3, longitude: 2.3},
+        title: 'Test bot!',
+      })
+      expect(bot.isNew).toBe(false)
+      expect(bot.title).toBe('Test bot!')
+      expect(bot.location!.latitude).toBe(1.3)
+      expect(bot.location!.longitude).toBe(2.3)
+    })
 
-  it('update bot description', async () => {
-    await bot.update({description: 'New description'})
-    expect(bot.description).toBe('New description')
-  })
+    it('update bot description', async () => {
+      await bot.update({description: 'New description'})
+      expect(bot.description).toBe('New description')
+    })
 
-  it('create bot posts', async () => {
-    await bot.posts.load()
-    expect(bot.posts.list.length).toBe(0)
-    for (let i = 0; i < 12; i += 1) {
-      const botPost = bot.createPost('hello' + i)
-      await botPost.publish()
-    }
-    expect(bot.posts.list.length).toBe(12)
-    // load bot posts
-    bot.posts.refresh()
-    expect(bot.posts.list.length).toBe(0)
-    // load first page (10 records)
-    await bot.posts.load()
-    expect(bot.posts.list.length).toBe(10)
-    // verify paging
-    await bot.posts.load()
-    expect(bot.posts.list.length).toBe(12)
-    // we have reversed order of posts here
-    for (let i = 0; i < 12; i += 1) {
-      expect(bot.posts.list[i].content).toBe('hello' + (11 - i))
-    }
+    it('create bot posts + load with paging', async () => {
+      await bot.posts.load()
+      expect(bot.posts.list.length).toBe(0)
+      for (let i = 0; i < 12; i += 1) {
+        const botPost = bot.createPost('hello' + i)
+        await botPost.publish()
+      }
+      expect(bot.posts.list.length).toBe(12)
+      // load bot posts
+      bot.posts.refresh()
+      expect(bot.posts.list.length).toBe(0)
+      // load first page (10 records)
+      await bot.posts.load()
+      expect(bot.posts.list.length).toBe(10)
+      // verify paging
+      await bot.posts.load()
+      expect(bot.posts.list.length).toBe(12)
+      // we have reversed order of posts here
+      for (let i = 0; i < 12; i += 1) {
+        expect(bot.posts.list[i].content).toBe('hello' + (11 - i))
+      }
+    })
+    afterAll(async () => {
+      // try {
+      //   await user.removeBot(bot.id)
+      // } catch (e) {
+      //   console.warn('error removing bot', e)
+      // }
+    })
   })
 
   afterAll(async () => {
-    await sleep(1000) // we need it now to see debug console.log statements
+    // await sleep(1000) // we need it now to see debug console.log statements
     try {
       await user.remove()
     } catch (e) {
-      expect(e).toBeUndefined()
+      console.warn('error removing user 1', e)
     }
     try {
       await user2.remove()
     } catch (e) {
-      expect(e).toBeUndefined()
+      console.warn('error removing user 2', e)
     }
   })
 })
