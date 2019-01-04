37f213bdc5a2c418fc531376e084817314e21263
Pavlo Aksonov <aksonov+github@gmail.com>
2019-01-04 16:01:46 +0100

    display thumbnail (because source is null)

--- src/components/Chats/ChatCard.tsx
+++ src/components/Chats/ChatCard.tsx
@@ -27,7 +27,7 @@ export default class ChatCard extends React.Component<Props> {
     const {otherUser} = chat
     let media: any = null
     try {
-      media = msg!.media && msg!.media!.source ? msg!.media : null
+      media = msg!.media && msg!.media!.thumbnail ? msg!.media : null
     } catch (err) {
       // console.log('TODO: Fix msg.media reference error', err)
     }
@@ -72,7 +72,7 @@ export default class ChatCard extends React.Component<Props> {
         </Text>
         {media && (
           <View style={{paddingTop: 15 * k}}>
-            <ResizedImage image={media.source} />
+            <ResizedImage image={media.thumbnail} />
           </View>
         )}
         {chat.unreadCount > 0 && (
