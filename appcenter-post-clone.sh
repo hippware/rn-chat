a98fcaaaa2507aef6c56cd178cec4bcecadcb899
Pavlo Aksonov <aksonov+github@gmail.com>
2019-01-08 11:16:03 +0100

    experiment with node version

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 brew uninstall node@6
-NODE_VERSION="8.9.4"
+NODE_VERSION="11.3.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 echo "Node version..."
