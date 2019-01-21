b78f132351eebe71de663d0a2e0b586ce015332b
Eric Kirkham <eric@southerneer.com>
2019-01-21 12:54:56 -0800

    separate auth strategies from auth store

--- src/store/AuthStore.ts
+++ src/store/AuthStore.ts
@@ -1,64 +1,50 @@
-import {types, Instance, getParent, flow} from 'mobx-state-tree'
-import {Credentials} from 'wocky-client'
+import {types, Instance, getParent} from 'mobx-state-tree'
 import analytics from 'src/utils/analytics'
+import strategies, {AuthStrategy, Strategy} from './authStrategies'
 
 const AuthStore = types
   .model('AuthStore', {
     phone: types.maybe(types.string),
-    isFirebase: types.maybe(types.boolean),
+    strategyName: types.optional(types.enumeration(['firebase', 'bypass']), 'firebase'),
   })
   .views(self => ({
     get canLogin(): boolean {
-      return !!self.phone && self.isFirebase !== undefined
+      return !!self.phone
     },
   }))
   .actions(self => {
-    const {firebaseStore, wocky, homeStore, locationStore} = getParent<any>(self)
+    const store = getParent<any>(self)
+    const {wocky, homeStore, locationStore} = store
+    let strategy: AuthStrategy | null = null
 
     return {
-      setPhone: (phone: string) => (self.phone = phone),
-
-      setIsFirebase: (isFirebase: boolean) => (self.isFirebase = isFirebase),
-
-      register: (phone: string, isFirebase: boolean) => {
+      register: (phone: string, s: Strategy) => {
         self.phone = phone
-        self.isFirebase = isFirebase
+        self.strategyName = s
       },
 
-      login: flow(function*() {
+      login(): Promise<boolean> {
         try {
-          let credentials: Credentials
-
           if (!self.canLogin) {
-            throw new Error('Phone number and provider must be set before login.')
+            throw new Error('Phone number must be set before login.')
           }
-
-          if (self.isFirebase) {
-            credentials = {
-              phone_number: self.phone,
-              ...(yield firebaseStore.getLoginCredentials()),
-            }
-          } else {
-            // bypass login
-            // Since users need to have unique `sub`s so we'll just use phoneNumber in the case of a bypass login
-            // https://hippware.slack.com/archives/C033TRJDD/p1543459452073900
-            credentials = {typ: 'bypass', sub: self.phone!, phone_number: self.phone}
-          }
-          return wocky.login(credentials)
+          strategy = strategies[self.strategyName]
+          return strategy.login(store)
         } catch (error) {
-          analytics.track('error_connection', {error})
+          analytics.track('& error_connection', {error})
         }
-      }),
+        return Promise.resolve(false)
+      },
 
       logout(): Promise<boolean> {
         homeStore.onLogout()
         locationStore.onLogout()
-        if (self.isFirebase) {
-          firebaseStore.logout()
+        if (strategy!.logout(store)) {
+          self.phone = undefined
+          self.strategyName = 'firebase'
+          return wocky.logout()
         }
-        self.phone = undefined
-        self.isFirebase = undefined
-        return wocky.logout()
+        return Promise.resolve(false)
       },
     }
   })
---
+++ src/store/authStrategies.ts
@@ -0,0 +1,37 @@
+import {Credentials} from 'wocky-client'
+
+export type AuthStrategy = {
+  login: (store) => Promise<boolean>
+  logout: (store) => Promise<boolean>
+}
+
+export type Strategy = 'firebase' | 'bypass'
+
+interface IStrategies {
+  [key: string]: AuthStrategy
+}
+
+const Strategies: IStrategies = {
+  firebase: {
+    login: async store => {
+      const {wocky, firebaseStore} = store
+      const credentials: Credentials = await firebaseStore.getLoginCredentials()
+      return wocky.login(credentials)
+    },
+    logout: async store => {
+      return store.firebaseStore.logout()
+    },
+  },
+  bypass: {
+    login: async store => {
+      const {authStore: {phone}, wocky} = store
+      // Since users need to have unique `sub`s so we'll just use phoneNumber in the case of a bypass login
+      // https://hippware.slack.com/archives/C033TRJDD/p1543459452073900
+      const credentials: Credentials = {typ: 'bypass', sub: phone, phone_number: phone}
+      return wocky.login(credentials)
+    },
+    logout: () => Promise.resolve(true),
+  },
+}
+
+export default Strategies
