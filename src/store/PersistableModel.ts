e19d008ff7bf5bc0d536b3ef6e0fc891aaa87745
Eric Kirkham <eric@southerneer.com>
2019-01-22 12:14:41 -0800

    clean up old tryMigrate code

--- src/store/PersistableModel.ts
+++ src/store/PersistableModel.ts
@@ -45,36 +45,7 @@ const PersistableModel = types
       }
     }
 
-    // try to migrate cached data from 1.x.x to 2.x.x
-    async function tryMigrate() {
-      let data
-      try {
-        analytics.track('migration_try')
-        const key = 'rnchat:model'
-        data = await loadFromStorage(key)
-        const {user, password, server} = JSON.parse(data)
-        applySnapshot(self.wocky, {...self.wocky, username: user, password, host: server} as any)
-        storage.removeItem(key)
-        analytics.track('migration_success', getSnapshot(self.wocky))
-      } catch (err) {
-        // logger.warn('Data migration error:', err)
-        if (data) {
-          // !data == fresh install
-          analytics.track('migration_fail', data)
-        }
-      }
-    }
-
     async function load() {
-      // Nice way to impersonate QA (it is Miranda's account)
-      // return loadMinimal({
-      //   wocky: {
-      //     id: 'wocky',
-      //     username: '1a175ee4-55d5-11e6-8fee-0eea5386eb69',
-      //     password: '$T$kpVJzlwJgNw3dX85f6+CVU2/gLeaigUGpjLwFvZBDDo=',
-      //     resource: 'cli-resource-ac35c6fb',
-      //   },
-      // });
       const modelName = getType(self).name
       let parsed
       try {
@@ -94,8 +65,6 @@ const PersistableModel = types
         logger.log('hydration error', modelName, err, parsed)
         if (modelName === STORE_NAME && parsed && parsed.authStore) {
           loadMinimal(parsed)
-        } else {
-          await tryMigrate()
         }
       }
     }
