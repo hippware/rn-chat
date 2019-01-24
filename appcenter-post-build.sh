89e13768860618b87d533a7779487d66448e867d
Beng Tan <bengtan@users.noreply.github.com>
2019-01-24 16:19:40 +0800

    Bugfix. Add `yarn bugsnag`.

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,8 +3,10 @@
 echo "Checking for deploy-stage..."
 echo $APPCENTER_BRANCH
 
-if [$APPCENTER_BRANCH -eq "deploy-stage"]
+if [ "$APPCENTER_BRANCH" == "deploy-stage" ]
 then
   echo "Codepush StagingRollback bundle"
   ./node_modules/.bin/appcenter codepush release-react -a hippware/tinyrobot-2 -d StagingRollback --description "rollback to TestFlight version" --token $CODEPUSH_TOKEN_ERIC
 fi
+
+yarn bugsnag
