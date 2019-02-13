91c81dea2da48dc1efe9e90ece63592a9bb48fd0
Eric Kirkham <eric@southerneer.com>
2019-02-12 17:52:21 -0800

    UI for Find Friends list

--- src/components/Onboarding/OnboardingFindFriendsList.tsx
+++ src/components/Onboarding/OnboardingFindFriendsList.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {View, Image, FlatList} from 'react-native'
-import {RText, GradientButton, Separator, Spinner} from '../common'
+import {View, Image, FlatList, StyleSheet} from 'react-native'
+import {RText, GradientButton, Separator, Spinner, Avatar} from '../common'
 import {s, minHeight, k} from '../Global'
 import {onboardingSlideStyle as styles} from '../styles'
 import {observer} from 'mobx-react/native'
 import {inject} from 'mobx-react'
 import ContactStore, {MyContact} from 'src/store/ContactStore'
+import PersonRow from '../people-lists/PersonRow'
+import {colors} from 'src/constants'
+import {IWocky} from 'wocky-client'
 
 type Props = {
   onPress: () => void
@@ -44,11 +47,21 @@ const OnboardingFindFriendsList = inject('contactStore')(
 
       <Separator style={{width: '100%', marginHorizontal: 5}} />
 
-      <View style={{flex: 1, justifyContent: 'center', paddingBottom: buttonHeight}}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingBottom: buttonHeight,
+          width: '100%',
+          padding: 10,
+        }}
+      >
         {contactStore!.loading ? (
           <Spinner size={38} />
         ) : (
           <FlatList
+            style={{flex: 1, width: '100%'}}
             data={contactStore!.contacts.slice()}
             renderItem={({item}) => <Friend contact={item} />}
             keyExtractor={item => item.contact.recordID}
@@ -69,7 +82,81 @@ const OnboardingFindFriendsList = inject('contactStore')(
   ))
 )
 
-// todo: flesh out UI
-const Friend = ({contact}: {contact: MyContact}) => <View style={{height: 20, borderWidth: 1}} />
+const Friend = observer(({contact}: {contact: MyContact}) => {
+  const {contact: {thumbnailPath, givenName}, phoneNumber, profile} = contact
+  return (
+    <PersonRow
+      imageComponent={
+        <Avatar
+          image={thumbnailPath ? {uri: thumbnailPath} : undefined}
+          displayName={givenName}
+          size={42}
+        />
+      }
+      handleComponent={
+        <RText color={colors.DARK_PURPLE} weight="Medium" size={14}>
+          {`${contact.contact.givenName} ${contact.contact.familyName}`}
+        </RText>
+      }
+      displayName={profile ? profile.displayName : phoneNumber ? phoneNumber.number : ''}
+    >
+      <ToggleButton contact={contact} />
+    </PersonRow>
+  )
+})
+
+const ToggleButton = inject('contactStore')(
+  observer(({contact, contactStore}: {contact: MyContact; contactStore?: ContactStore}) => {
+    const {relationship, smsSent} = contact
+
+    let text: string = 'INVITE'
+    let onPress = smsSent ? () => null : () => contactStore!.inviteContact(contact)
+    let isPink: boolean = false
+
+    if (relationship === 'NONE') {
+      text = smsSent ? 'SENT' : 'CONNECT'
+      isPink = smsSent
+    } else if (relationship === null) {
+      text = smsSent ? 'INVITED' : 'INVITE'
+      isPink = smsSent
+    } else if (relationship === 'FRIEND') {
+      text = 'FRIENDS'
+      onPress = () => null
+      isPink = true
+    } else if (relationship === 'INVITED') {
+      text = 'SENT'
+      onPress = () => null
+      isPink = true
+    }
+
+    return (
+      <GradientButton
+        style={[styles2.button]}
+        isPink={isPink}
+        offColor="white"
+        // innerStyle=
+        onPress={onPress}
+      >
+        <RText size={13} color={isPink ? 'white' : colors.PINK} weight={'Medium'}>
+          {text}
+        </RText>
+      </GradientButton>
+    )
+  })
+)
 
 export default OnboardingFindFriendsList
+
+const styles2 = StyleSheet.create({
+  button: {
+    height: 31,
+    width: 94,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: colors.PINK,
+    borderRadius: 4,
+    backgroundColor: colors.WHITE,
+    borderColor: colors.PINK,
+    borderWidth: 1,
+  },
+})
