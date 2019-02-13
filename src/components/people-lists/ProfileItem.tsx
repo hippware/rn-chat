8fbc27e028cc61330ad4fa6cb697f810b18bb231
Eric Kirkham <eric@southerneer.com>
2019-02-12 17:51:32 -0800

    modularize PersonRow to handle non-profile entities

--- src/components/people-lists/ProfileItem.tsx
+++ src/components/people-lists/ProfileItem.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {View, Text, Image} from 'react-native'
 import Avatar from '../common/Avatar'
 import {observer} from 'mobx-react/native'
 import {ProfileHandle} from '../common'
+import {IProfile} from 'wocky-client'
+import PersonRow from './PersonRow'
 
 type Props = {
-  profile: any
+  profile: IProfile
   style?: any
   children?: any
   selected?: boolean
@@ -14,39 +15,13 @@ type Props = {
 
 const ProfileItem = observer(({profile, style, children, selected, tappable}: Props) => {
   return profile && profile.handle ? (
-    <View
-      style={[
-        {
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          marginVertical: 10,
-        },
-        style,
-      ]}
+    <PersonRow
+      imageComponent={<Avatar size={44} profile={profile} tappable={tappable !== false} />}
+      handleComponent={<ProfileHandle size={16} profile={profile} />}
+      displayName={profile.displayName}
     >
-      <View style={{marginRight: 13}}>
-        <Avatar size={44} profile={profile} tappable={tappable !== false} />
-      </View>
-      <View style={{flex: 1}}>
-        <ProfileHandle size={16} profile={profile} />
-        <Text
-          style={{
-            color: '#757575',
-            fontFamily: 'Roboto-Regular',
-            fontSize: 16,
-          }}
-        >
-          {profile.displayName}
-        </Text>
-      </View>
-      {selected !== undefined && (
-        <View style={{width: 25}}>
-          {selected && <Image source={require('../../../images/contactSelect.png')} />}
-        </View>
-      )}
       {children}
-    </View>
+    </PersonRow>
   ) : null
 })
 
