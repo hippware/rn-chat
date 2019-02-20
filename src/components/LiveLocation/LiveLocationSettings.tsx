be7f9950485dda1bfe0cc47a028dfd44fedbb0d4
Eric Kirkham <eric@southerneer.com>
2019-02-20 13:49:20 -0800

    spacing tweaks, flatlist skeleton

--- src/components/LiveLocation/LiveLocationSettings.tsx
+++ src/components/LiveLocation/LiveLocationSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, TouchableOpacity, Alert, Image} from 'react-native'
+import {View, TouchableOpacity, Alert, Image, FlatList} from 'react-native'
 import {RText, Separator, Switch} from '../common'
 import {minHeight} from '../Global'
 import {Actions} from 'react-native-router-flux'
@@ -12,8 +12,9 @@ type Props = {
 }
 @inject('wocky')
 @observer
-export default class LiveLocationCompose extends React.Component<Props> {
+export default class LiveLocationSettings extends React.Component<Props> {
   render() {
+    const {profile} = this.props.wocky!
     return (
       <View
         style={{
@@ -28,68 +29,77 @@ export default class LiveLocationCompose extends React.Component<Props> {
           },
           shadowRadius: 5,
           shadowOpacity: 1,
+          height: 400 * minHeight,
         }}
       >
-        <View style={{height: 400 * minHeight}}>
-          <View
-            style={{alignItems: 'center', justifyContent: 'center', marginVertical: 23 * minHeight}}
+        <View
+          style={{
+            alignItems: 'center',
+            marginVertical: 23 * minHeight,
+          }}
+        >
+          <Image source={require('../../../images/LiveLocationIcon.png')} />
+          <RText
+            style={{
+              width: 255,
+              fontSize: 17,
+              fontWeight: '500',
+              textAlign: 'center',
+              marginTop: 11,
+              marginBottom: 11,
+            }}
           >
-            <Image source={require('../../../images/LiveLocationIcon.png')} />
-            <RText
-              style={{
-                width: 255,
-                fontSize: 17,
-                fontWeight: '500',
-                textAlign: 'center',
-                marginTop: 11,
-                marginBottom: 11,
-              }}
-            >
-              Sharing your live location
-            </RText>
-            <Switch
-              isOn={this.props.wocky!.profile!.isLocationShared}
-              onColor="#FE5C6C"
-              offColor="#D3D3D3"
-              size="regular"
-              onToggle={isOn => {
-                if (!isOn) {
-                  Alert.alert('', 'Are you sure you want to turn off live location sharing?', [
-                    {text: 'Cancel', style: 'cancel'},
-                    {
-                      text: 'Yes',
-                      style: 'destructive',
-                      onPress: () => this.props.wocky!.profile!.cancelAllLocationShares(),
-                    },
-                  ])
-                }
-              }}
-            />
-          </View>
-          <Separator />
-          <View style={{marginBottom: 50}} />
-          <TouchableOpacity
-            style={{width: '100%', height: 50 * minHeight, bottom: 0, position: 'absolute'}}
-            onPress={() => Actions.popTo('home')}
-          >
-            <LinearGradient
-              start={{x: 0, y: 0.5}}
-              end={{x: 1, y: 0.5}}
-              colors={['rgb(242,68,191)', 'rgb(254,110,98)', 'rgb(254,92,108)']}
-              style={{
-                flex: 1,
-                width: '100%',
-                height: '100%',
-                paddingVertical: 15 * minHeight,
-                alignItems: 'center',
-              }}
-            >
-              <RText color="white" size={15}>
-                Select Friends
-              </RText>
-            </LinearGradient>
-          </TouchableOpacity>
+            Sharing your live location
+          </RText>
+          <Switch
+            isOn={profile!.isLocationShared}
+            onColor="#FE5C6C"
+            offColor="#D3D3D3"
+            size="regular"
+            onToggle={isOn => {
+              if (!isOn) {
+                Alert.alert('', 'Are you sure you want to turn off live location sharing?', [
+                  {text: 'Cancel', style: 'cancel'},
+                  {
+                    text: 'Yes',
+                    style: 'destructive',
+                    onPress: () => this.props.wocky!.profile!.cancelAllLocationShares(),
+                  },
+                ])
+              }
+            }}
+          />
         </View>
+
+        <Separator style={{width: '100%', backgroundColor: 'rgb(224,224,224)'}} />
+
+        <FlatList
+          data={profile!.locationShares.list.slice()}
+          renderItem={({item}) => <View style={{borderWidth: 1, height: 50, width: '100%'}} />}
+          keyExtractor={item => item.id}
+        />
+
+        <TouchableOpacity
+          style={{width: '100%', height: 50 * minHeight, bottom: 0, position: 'absolute'}}
+          onPress={() => Actions.popTo('home')}
+        >
+          <LinearGradient
+            start={{x: 0, y: 0.5}}
+            end={{x: 1, y: 0.5}}
+            colors={['rgb(242,68,191)', 'rgb(254,110,98)', 'rgb(254,92,108)']}
+            style={{
+              flex: 1,
+              width: '100%',
+              height: '100%',
+              paddingVertical: 15 * minHeight,
+              alignItems: 'center',
+            }}
+          >
+            <RText color="white" size={15}>
+              Select Friends
+            </RText>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
     )
   }
