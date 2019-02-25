7ed51bb2c454cdb6c00a1a3358618f497f2c9ae3
Eric Kirkham <eric@southerneer.com>
2019-02-25 12:27:56 -0800

    handle homestream  banner LL tap

--- src/components/Home/ActiveLocationSharer.tsx
+++ src/components/Home/ActiveLocationSharer.tsx
@@ -6,37 +6,48 @@ import {minHeight} from '../Global'
 import {colors} from '../../constants'
 import {Actions} from 'react-native-router-flux'
 import {IActiveBannerItem} from './ActiveGeoBotBanner'
+import {inject} from 'mobx-react/native'
+import {IHomeStore} from 'src/store/HomeStore'
 
 interface IProps extends IActiveBannerItem {
   sharer: ILocationShare
+  homeStore?: IHomeStore
 }
 
 const liveImg = require('../../../images/live.png')
 
-const ActiveLocationSharer = ({sharer, outerStyle, innerStyle}: IProps) => (
-  <View style={outerStyle}>
-    <View style={innerStyle}>
-      {/* todo: need custom avatar with the extra circle around the outside...will need to wait until Alan comes back to get that asset */}
-
-      <Avatar profile={sharer.sharedWith} size={64} />
-      <Image source={liveImg} style={{position: 'absolute', right: 0, top: 0}} />
+const ActiveLocationSharer = inject('homeStore')(
+  ({sharer, outerStyle, innerStyle, homeStore}: IProps) => (
+    <View style={outerStyle}>
       <TouchableOpacity
-        onPress={() => Actions.profileDetails({item: sharer.sharedWith.id})}
-        style={{marginTop: 4}}
+        style={innerStyle}
+        onPress={() => {
+          homeStore!.setFocusedLocation(sharer.sharedWith.location)
+          homeStore!.selectProfile(sharer.sharedWith)
+        }}
       >
-        <RText
-          size={13}
-          style={{textAlign: 'center', marginTop: 2 * minHeight}}
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          color={colors.DARK_GREY}
-          weight={'Medium'}
+        {/* todo: need custom avatar with the extra circle around the outside...will need to wait until Alan comes back to get that asset */}
+
+        <Avatar profile={sharer.sharedWith} size={64} tappable={false} />
+        <Image source={liveImg} style={{position: 'absolute', right: 0, top: 0}} />
+        <TouchableOpacity
+          onPress={() => Actions.profileDetails({item: sharer.sharedWith.id})}
+          style={{marginTop: 4}}
         >
-          {`@${sharer.sharedWith.handle}`}
-        </RText>
+          <RText
+            size={13}
+            style={{textAlign: 'center', marginTop: 2 * minHeight}}
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            color={colors.DARK_GREY}
+            weight={'Medium'}
+          >
+            {`@${sharer.sharedWith.handle}`}
+          </RText>
+        </TouchableOpacity>
       </TouchableOpacity>
     </View>
-  </View>
+  )
 )
 
 export default ActiveLocationSharer
