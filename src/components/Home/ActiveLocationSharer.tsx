196e06395b27e63fbcaf88159f5fb6f050c5fc9b
Jinyuan Qiu (Alan) <irfirl@gmail.com>
2019-02-25 12:32:12 -0800

    Updated Live Location Avatar Variation

--- src/components/Home/ActiveLocationSharer.tsx
+++ src/components/Home/ActiveLocationSharer.tsx
@@ -6,6 +6,7 @@ import {minHeight} from '../Global'
 import {colors} from '../../constants'
 import {Actions} from 'react-native-router-flux'
 import {IActiveBannerItem} from './ActiveGeoBotBanner'
+import Triangle from '../map/Triangle'
 
 interface IProps extends IActiveBannerItem {
   sharer: ILocationShare
@@ -18,8 +19,26 @@ const ActiveLocationSharer = ({sharer, outerStyle, innerStyle}: IProps) => (
     <View style={innerStyle}>
       {/* todo: need custom avatar with the extra circle around the outside...will need to wait until Alan comes back to get that asset */}
 
-      <Avatar profile={sharer.sharedWith} size={64} />
-      <Image source={liveImg} style={{position: 'absolute', right: 0, top: 0}} />
+      <View
+        style={{
+          alignItems: 'center',
+          borderColor: colors.PINK,
+          borderWidth: 1,
+          borderRadius: 50,
+          paddingTop: 3.3,
+          paddingHorizontal: 3.3,
+          height: 63.6,
+          width: 63.6,
+        }}
+      >
+        <Avatar noScale size={54} profile={sharer.sharedWith} hideDot borderColor={colors.PINK} />
+        <Triangle width={8} height={8} color={colors.PINK} direction="down" />
+      </View>
+
+      <Image
+        source={liveImg}
+        style={{position: 'absolute', right: -2, top: -4, width: 39, height: 21}}
+      />
       <TouchableOpacity
         onPress={() => Actions.profileDetails({item: sharer.sharedWith.id})}
         style={{marginTop: 4}}
