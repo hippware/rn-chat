6a4be3d5bfde0271a20607ef3680fddacf2b1b97
Eric Kirkham <eric@southerneer.com>
2019-02-25 16:35:17 -0800

    remove more unused modules

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,4 @@
 declare module 'react-native-linear-gradient'
-declare module 'react-native-swipeable'
 declare module 'react-native-cube-transition'
 declare module 'react-native-permissions'
 declare module 'react-native-router-flux'
