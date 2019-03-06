70eadbf8856603117c2a4b147e66366a7d6f1b33
Pavlo Aksonov <aksonov+github@gmail.com>
2019-03-06 19:28:13 +0100

    more native modules to android

--- android/app/src/main/java/com/tinyrobot/tinyrobotStaging/MainApplication.java
+++ android/app/src/main/java/com/tinyrobot/tinyrobotStaging/MainApplication.java
@@ -3,10 +3,16 @@ package com.tinyrobot.tinyrobotStaging;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.bugsnag.BugsnagReactNative;
 import com.xgfe.reactnativeenv.RCTNativeEnvPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.rnfs.RNFSPackage;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.kevinejohn.RNMixpanel.RNMixpanel;
 import com.rome2rio.android.reactnativetouchthroughview.TouchThroughViewPackage;
@@ -21,6 +27,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -30,10 +42,16 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
+            new CodePush("aKbSrov0EFd-mvtC31iYca_W0EtFS1jJ7ITUE", getApplicationContext(), BuildConfig.DEBUG),
+            new RNDeviceInfo(),
+            BugsnagReactNative.getPackage(),
             new RCTNativeEnvPackage(BuildConfig.class),
             new LinearGradientPackage(),
             new RNFSPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseMessagingPackage(),
             new MapsPackage(),
             new RNMixpanel(),
             new TouchThroughViewPackage()
