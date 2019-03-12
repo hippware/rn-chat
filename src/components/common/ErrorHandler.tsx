ea1e0449831e832ffadbe0232b863d34a3ad5bc3
Eric Kirkham <eric@southerneer.com>
2019-03-12 01:33:37 -0700

    for #3452 (#3454)
    * for #3452
    
    * clarify error message setting

--- src/components/common/ErrorHandler.tsx
+++ src/components/common/ErrorHandler.tsx
@@ -61,7 +61,7 @@ class ErrorHandler extends React.Component<Props> {
 }
 
 const ErrorView = ({error, errorInfo, reload}) => {
-  if (settings.friendlyErrorMessage) {
+  if (settings.showFullErrorMessage) {
     return (
       <View style={{flex: 1}}>
         <ScrollView
