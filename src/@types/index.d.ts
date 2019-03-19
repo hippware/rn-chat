8b2f089f5ee58fc9443aa241070471e9dfed6353
Eric Kirkham <eric@southerneer.com>
2019-03-19 15:11:18 -0700

    unmask rnrf typings

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,7 +1,6 @@
 declare module 'react-native-linear-gradient'
 declare module 'react-native-cube-transition'
 declare module 'react-native-permissions'
-declare module 'react-native-router-flux'
 declare module 'react-native-background-geolocation'
 declare const process
 
