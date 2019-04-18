d9afc6e58f01d03900ead7d4f83e6ddb30aed5dd
Eric Kirkham <eric@southerneer.com>
2019-04-17 22:50:26 -0700

    prevent error message on logout

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -5,7 +5,6 @@ import {observable} from 'mobx'
 import {minHeight} from './Global'
 import SignUpAvatar from './SignUpAvatar'
 import {Actions} from 'react-native-router-flux'
-import {error} from '../utils/logger'
 import Cell from './Cell'
 import FormTextInput from './FormTextInput'
 import {colors} from '../constants'
@@ -57,7 +56,7 @@ class MyAccount extends React.Component<Props> {
     const {wocky} = this.props
     const {profile} = wocky!
     if (!profile || !this.vProfile) {
-      error('NULL PROFILE')
+      // error('NULL PROFILE')
       return <View style={{flex: 1, backgroundColor: 'white'}} />
     }
     return (
