4a2599bc69a256a6174579833dcbecfc332ba4cd
Eric Kirkham <eric@southerneer.com>
2019-05-01 18:42:18 -0700

    improve VS Code + Typescript interaction on console.log

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -4,6 +4,15 @@ declare module 'react-native-permissions'
 declare module 'react-native-background-geolocation'
 declare const process
 
+// prevent ts errors in storybook
+declare var module: any
+
+// prevent ts errors in testuser.ts
+declare var __dirname: any
+
+// prevent ts errors in testuser.ts
+declare var global: any
+
 // tslint:disable:interface-name
 
 // use module augmentation to fix reactotron types: https://www.typescriptlang.org/docs/handbook/declaration-merging.html
