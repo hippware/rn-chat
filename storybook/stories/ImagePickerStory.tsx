8b7c570ab61c74aa567e41d52d8273a327ea2e63
Eric Kirkham <eric@southerneer.com>
2019-05-17 14:21:41 -0700

    clean up story

---
+++ storybook/stories/ImagePickerStory.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Camera from '../../src/components/Camera'
+import {Router, Scene, Stack} from 'react-native-router-flux'
+import {TouchableOpacity, View, Alert, Text} from 'react-native'
+import {showImagePicker} from 'src/components/ImagePicker'
+import fileService from 'src/store/fileService'
+
+export default () => (
+  <Router>
+    <Stack hideNavBar>
+      <Scene key="screen" component={Screen} />
+      <Stack hideNavBar key="camera">
+        <Scene key="cameraScreen" component={CameraScreen} />
+      </Stack>
+    </Stack>
+  </Router>
+)
+
+const Screen = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <TouchableOpacity
+      onPress={async () => {
+        const image = await showImagePicker()
+        Alert.alert('we have an image!' + JSON.stringify(image))
+      }}
+      style={{padding: 20, borderWidth: 2}}
+    >
+      <Text>Show Image Picker</Text>
+    </TouchableOpacity>
+  </View>
+)
+
+const CameraScreen = (props: any) => (
+  <Camera store={{getImageSize: fileService.getImageSize}} {...props} />
+)
