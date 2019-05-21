c71f595af265220392d7777b60f4ec8fbf910f16
Eric Kirkham <eric@southerneer.com>
2019-05-21 13:45:00 -0700

    `loadWithData` on paginable lists for correct count + cursor properties

--- third-party/wocky-client/src/transport/types.ts
+++ third-party/wocky-client/src/transport/types.ts
@@ -4,6 +4,26 @@ export interface IPagingList<T> {
   list: T[]
   cursor?: string
   count: number
+  loading: boolean
+  finished: boolean
+  remove: (id: string) => void
+  add: (item) => void
+  addToTop: (item) => void
+  loadWithData: (params: LoadWithDataParams) => void
+  length: number
+  first: T | null
+  last: T | null
+  exists: (id: string) => boolean
+  refresh: () => void
+  load: (params: {force?: boolean}) => Promise<T[]>
+}
+
+export type LoadWithDataParams = {
+  list: any[]
+  count: number
+  cursor?: string
+  force?: boolean
+  addMiddleware?: (p: any) => any
 }
 
 export type MediaUploadParams = {
