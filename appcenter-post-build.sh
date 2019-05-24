36331603dd89f879b581b9835183b40cce8dd4ed
Eric Kirkham <eric@southerneer.com>
2019-05-23 18:48:41 -0700

    only run detox on iOS builds for now

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Installing applesimutils"
-brew tap wix/brew
-brew install applesimutils
+if [ -z ${APPCENTER_XCODE_SCHEME+x} ]
+then
+  echo "Android build"
+else 
+  echo "iOS build! $APPCENTER_XCODE_SCHEME"
 
-echo "Detecting applesimutils"
-which applesimutils
-# npm install -g detox-cli
-echo 'Running detox'
-yarn detox
+  echo "Installing applesimutils"
+  brew tap wix/brew
+  brew install applesimutils
 
+  echo "Detecting applesimutils"
+  which applesimutils
+
+  echo 'Running detox'
+  yarn detox
+fi
 
 if [ "$APPCENTER_BRANCH" == "deploy-stage" ] || [ "$APPCENTER_BRANCH" == "production" ]
 then
