62437de1594d1d87dc6e901aefd1493feac24b7f
Eric Kirkham <eric@southerneer.com>
2019-06-27 14:19:13 -0700

    react hooks for UpdateBrick

--- src/components/UpdateBrick.tsx
+++ src/components/UpdateBrick.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {View, Image, TouchableOpacity, Linking} from 'react-native'
 import codePush from 'react-native-code-push'
 import RText from './common/RText'
@@ -7,61 +7,61 @@ import {PINK, DARK_GREY, WHITE} from '../constants/colors'
 const background = require('../../images/codepushBackground.png')
 const icon = require('../../images/codePushIcon.png')
 
-export default class UpdateBrick extends React.Component<{}> {
-  componentWillMount() {
+const UpdateBrick = () => {
+  useEffect(() => {
     codePush.notifyAppReady()
-  }
+  }, [])
 
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Image
-          source={background}
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          }}
-          resizeMode="cover"
-        />
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Image
+        source={background}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+        resizeMode="cover"
+      />
 
-        <Image source={icon} />
+      <Image source={icon} />
 
-        <RText
-          style={{marginVertical: 30, textAlign: 'center'}}
-          color={PINK}
-          size={30}
-          weight="Light"
-        >{`New Update\r\nAvailable!`}</RText>
+      <RText
+        style={{marginVertical: 30, textAlign: 'center'}}
+        color={PINK}
+        size={30}
+        weight="Light"
+      >{`New Update\r\nAvailable!`}</RText>
 
-        <RText
-          color={DARK_GREY}
-          size={18}
-          weight="Light"
-          style={{textAlign: 'center'}}
-        >{`Please visit the app store to\r\nupdate the app.`}</RText>
+      <RText
+        color={DARK_GREY}
+        size={18}
+        weight="Light"
+        style={{textAlign: 'center'}}
+      >{`Please visit the app store to\r\nupdate the app.`}</RText>
 
-        <TouchableOpacity
-          style={{
-            height: 46,
-            width: '90%',
-            backgroundColor: PINK,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 30,
-            borderRadius: 4,
-          }}
-          onPress={() =>
-            Linking.openURL(
-              'itms-apps://itunes.apple.com/us/app/id1295678402?mt=8&action=write-review'
-            )
-          }
-        >
-          <RText size={18} color={WHITE}>
-            Update
-          </RText>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+      <TouchableOpacity
+        style={{
+          height: 46,
+          width: '90%',
+          backgroundColor: PINK,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 30,
+          borderRadius: 4,
+        }}
+        onPress={() =>
+          Linking.openURL(
+            'itms-apps://itunes.apple.com/us/app/id1295678402?mt=8&action=write-review'
+          )
+        }
+      >
+        <RText size={18} color={WHITE}>
+          Update
+        </RText>
+      </TouchableOpacity>
+    </View>
+  )
 }
+
+export default UpdateBrick
