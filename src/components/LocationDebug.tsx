5205438d3b0b46ed57dd7aa9c501a614de950ad2
Beng Tan <bengtan@users.noreply.github.com>
2019-07-01 14:23:11 +0800

    Remove 'latest locations' and getLocationsVisited API.

--- src/components/LocationDebug.tsx
+++ src/components/LocationDebug.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, TouchableOpacity} from 'react-native'
+import {TouchableOpacity} from 'react-native'
 import t from 'tcomb-form-native'
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 import {inject, observer} from 'mobx-react/native'
@@ -14,7 +14,6 @@ import {
 } from '../store/LocationStore'
 import Screen from './Screen'
 import _ from 'lodash'
-import {observable, when} from 'mobx'
 import {IWocky} from 'wocky-client'
 import {RText} from './common'
 import {colors} from '../constants'
@@ -74,25 +73,6 @@ type Props = {
 @inject('locationStore', 'wocky')
 @observer
 export default class LocationDebug extends React.Component<Props> {
-  // @observable locations?: ObservableArray<any> = new ObservableArray<any>([])
-  readonly locations = observable.array<object>([])
-  @observable syncing: boolean = false
-
-  componentWillMount() {
-    when(() => this.props.wocky!.connected, this.getLocations)
-  }
-
-  getLocations = async (): Promise<void> => {
-    if (this.syncing) return
-    this.syncing = true
-    try {
-      const locations: object[] = await this.props.wocky!.getLocationsVisited()
-      this.locations.replace(locations)
-    } finally {
-      this.syncing = false
-    }
-  }
-
   renderLocation = (l, index) => (
     <RText size={13} key={l.createdAt}>
       {`${index + 1}. ${moment(l.createdAt).calendar()}: ${l.lat}, ${l.lon}`}
@@ -104,7 +84,6 @@ export default class LocationDebug extends React.Component<Props> {
     if (!backgroundOptions) return null
     let value = _.pick(backgroundOptions, BG_STATE_PROPS)
     value = _.assign(value, {debugSounds})
-    const syncing = this.syncing || !this.props.wocky!.connected
 
     return (
       <Screen style={{flex: 1, paddingVertical: 20}}>
@@ -133,27 +112,6 @@ export default class LocationDebug extends React.Component<Props> {
               Email log
             </RText>
           </TouchableOpacity>
-          <View style={{marginTop: 20}}>
-            <View style={{flexDirection: 'row', alignItems: 'center', marginBottom: 10}}>
-              <RText size={20}>{'Latest Locations'}</RText>
-              <TouchableOpacity
-                onPress={this.getLocations}
-                style={{
-                  backgroundColor: syncing ? colors.DARK_GREY : colors.PINK,
-                  padding: 5,
-                  borderRadius: 2,
-                  marginLeft: 10,
-                  width: 100,
-                  alignItems: 'center',
-                }}
-              >
-                <RText size={20} color={colors.WHITE}>
-                  {syncing ? 'SYNCING' : 'SYNC'}
-                </RText>
-              </TouchableOpacity>
-            </View>
-            {this.locations.length ? this.locations.map(this.renderLocation) : null}
-          </View>
         </KeyboardAwareScrollView>
       </Screen>
     )
