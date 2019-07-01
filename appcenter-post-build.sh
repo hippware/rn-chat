5344d92a75752fd67c8e0eedee4200cc0a9f21fa
Eric Kirkham <eric@southerneer.com>
2019-07-01 12:49:35 -0700

    HOTFIX: fix post-build script (included in 4.15.0 build)

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -17,7 +17,7 @@ else
   # yarn detox
 fi
 
-if [ "$APPCENTER_BRANCH" == "deploy-stage" ] || [ "$APPCENTER_BRANCH" == "production" ]
-then
-#  yarn bugsnag
-fi
+# if [ "$APPCENTER_BRANCH" == "deploy-stage" ] || [ "$APPCENTER_BRANCH" == "production" ]
+# then
+# #  yarn bugsnag
+# fi
