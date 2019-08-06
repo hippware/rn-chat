0aa8301be6960d59f062068562f89bd4872306f5
Jinyuan Qiu (Alan) <irfirl@gmail.com>
2019-08-06 13:51:54 -0700

    Bot Icon Adjustments

--- src/components/common/BotIcon.tsx
+++ src/components/common/BotIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, Text, Image, ImageStyle, TextStyle} from 'react-native'
+import {StyleSheet, Text, Image, ImageStyle, TextStyle, Platform} from 'react-native'
 import {PINK} from '../../constants/colors'
 import {oldIcons} from '../../store/IconStore'
 
@@ -14,10 +14,19 @@ const defaultIcon = require('../../../images/mapIcons/question.png')
 
 const BotIcon = ({icon, size, imageStyle, textStyle}: Props) =>
   icon && !oldIcons.includes(icon) ? (
-    <Text style={[styles.icon, textStyle, {fontSize: size}]}>{icon}</Text>
+    <Text style={[styles.icon, textStyle, {fontSize: Platform.OS === 'ios' ? size : size - 5}]}>
+      {icon}
+    </Text>
   ) : (
     <Image
-      style={[{width: size, height: size, borderRadius: 5}, imageStyle]}
+      style={[
+        {
+          width: Platform.OS === 'ios' ? size : size - 5,
+          height: Platform.OS === 'ios' ? size : size - 5,
+          borderRadius: 5,
+        },
+        imageStyle,
+      ]}
       resizeMode="contain"
       source={defaultIcon}
     />
