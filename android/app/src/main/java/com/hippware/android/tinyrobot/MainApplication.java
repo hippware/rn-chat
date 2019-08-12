b72140178b1f9a24449d2e98128149a41f1016a4
Beng Tan <bengtan@users.noreply.github.com>
2019-08-12 11:36:30 +0800

    Configure multidex for API level less than 21.
    See https://developer.android.com/studio/build/multidex.html

--- android/app/src/main/java/com/hippware/android/tinyrobot/MainApplication.java
+++ android/app/src/main/java/com/hippware/android/tinyrobot/MainApplication.java
@@ -1,6 +1,6 @@
 package com.hippware.android.tinyrobot;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -35,7 +35,7 @@ import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
