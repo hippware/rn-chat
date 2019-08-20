e667e10ccb2e17996437bc6508a3a0a92ca58557
Eric Kirkham <eric@southerneer.com>
2019-08-20 11:01:39 -0700

    cleanup unused android references

--- android/app/src/main/java/com/hippware/android/tinyrobot/MainActivity.java
+++ android/app/src/main/java/com/hippware/android/tinyrobot/MainActivity.java
@@ -2,21 +2,15 @@ package com.hippware.android.tinyrobot;
 
 import android.os.Bundle;
 import com.facebook.react.ReactRootView;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactActivity;
-import com.microsoft.codepush.react.CodePush;
 import android.view.MotionEvent;
 import com.rome2rio.android.reactnativetouchthroughview.TouchThroughTouchHandlerInterface;
 import com.rome2rio.android.reactnativetouchthroughview.TouchThroughTouchHandler;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity implements TouchThroughTouchHandlerInterface {
-    private ReactRootView mReactRootView;
-    private ReactInstanceManager mReactInstanceManager;
     private TouchThroughTouchHandler touchThroughTouchHandler = new TouchThroughTouchHandler();
 
     @Override
--- android/app/src/main/java/com/hippware/android/tinyrobot/MainApplication.java
+++ android/app/src/main/java/com/hippware/android/tinyrobot/MainApplication.java
@@ -3,39 +3,14 @@ package com.hippware.android.tinyrobot;
 import androidx.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
-import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.zaguiini.RNPureJwt.RNPureJwtPackage;
-import com.actionsheet.ActionSheetPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.microsoft.codepush.react.CodePush;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.bugsnag.BugsnagReactNative;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.rnfs.RNFSPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.links.RNFirebaseLinksPackage;
-import com.kevinejohn.RNMixpanel.RNMixpanel;
-import com.rome2rio.android.reactnativetouchthroughview.TouchThroughViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import android.util.Log;
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
@@ -56,8 +31,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-	    // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
       packages.add(new RNFirebaseAuthPackage());
       packages.add(new RNFirebaseLinksPackage());
       packages.add(new RNFirebaseMessagingPackage());
