33e6a410f917084dc0921675f9fcea89f7635f4b
Beng Tan <bengtan@users.noreply.github.com>
2019-08-21 20:06:34 +0800

    Add AppState memoryWarning listener.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {View} from 'react-native'
+import {AppState, View} from 'react-native'
 import {Provider} from 'mobx-react'
 import TinyRobotRouter from './components/Router'
 import analytics from './utils/analytics'
@@ -7,6 +7,7 @@ import {createStore} from './store/store'
 import NotificationBanner from './components/NotificationBanner'
 import Connectivity from './components/Connectivity'
 import ErrorHandler from './components/common/ErrorHandler'
+import {bugsnagNotify} from 'src/utils/bugsnagConfig'
 
 const App = () => {
   const [store, setStore] = useState()
@@ -16,6 +17,12 @@ const App = () => {
       setStore(s)
     })
   }, [])
+
+  // This seems to be undocumented event. Not even sure what `state` is.
+  AppState.addEventListener('memoryWarning', (state: any) => {
+    bugsnagNotify(new Error('AppState.memoryWarning fired'), 'memory_warning', {state})
+    analytics.track('memory_warning', {state})
+  })
 
   if (!store) return null
   const {mstStore, ...rest} = store
