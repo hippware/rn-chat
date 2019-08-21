2e78a6ab49d96b334347b32b1041ceb8c3229551
Eric Kirkham <eric@southerneer.com>
2019-08-21 15:20:19 -0700

    hookify BackgroundGradient

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -2,27 +2,24 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import {colors} from '../constants'
-import PropTypes from 'prop-types'
 
-export default class BackgroundGradient extends React.Component<any> {
-  render() {
-    if (this.props.isDay) {
-      return (
-        <LinearGradient
-          colors={[colors.WHITE, 'rgb(241,242,244)', 'rgb(243,244,246)']}
-          locations={[0, 0.2, 1]}
-          style={styles.container}
-        />
-      )
-    } else {
-      return (
-        <LinearGradient
-          colors={['rgb(45,33,55)', 'rgb(48,35,59)']}
-          locations={[0, 1]}
-          style={styles.container}
-        />
-      )
-    }
+const BackgroundGradient = ({isDay}) => {
+  if (isDay) {
+    return (
+      <LinearGradient
+        colors={[colors.WHITE, 'rgb(241,242,244)', 'rgb(243,244,246)']}
+        locations={[0, 0.2, 1]}
+        style={styles.container}
+      />
+    )
+  } else {
+    return (
+      <LinearGradient
+        colors={['rgb(45,33,55)', 'rgb(48,35,59)']}
+        locations={[0, 1]}
+        style={styles.container}
+      />
+    )
   }
 }
 
@@ -35,6 +32,5 @@ const styles = StyleSheet.create({
     right: 0,
   },
 })
-;(BackgroundGradient as any).propTypes = {
-  isDay: PropTypes.bool.isRequired,
-}
+
+export default BackgroundGradient
