4d780c6854fdccbf601d86c10a36c571d4d01c08
Eric Kirkham <eric@southerneer.com>
2019-08-22 17:13:01 -0700

    hold on hookifying DraggablePopupList for now

--- src/components/common/DraggablePopupList.tsx
+++ src/components/common/DraggablePopupList.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react'
+import React, {ReactElement, useRef, useImperativeHandle, forwardRef} from 'react'
 import {View, FlatList, FlatListProps, Animated} from 'react-native'
 import {width, height, k} from '../Global'
 import {TouchThroughView} from 'react-native-touch-through-view'
@@ -11,6 +11,11 @@ interface IProps<T> extends FlatListProps<T> {
   isActive: boolean
   scrollY?: Animated.Value
 }
+
+// todo: hookify this (see commented example below) after we've migrated away from `inject`.
+// This one is complicated because we need to use forwardRef + inject
+// https://github.com/mobxjs/mobx-react/issues/717
+// https://mobx-react.js.org/recipes-inject
 
 @inject('scrollY')
 export default class DraggablePopupList<T> extends React.Component<IProps<T>> {
@@ -40,6 +45,36 @@ export default class DraggablePopupList<T> extends React.Component<IProps<T>> {
 
   scrollToOffset = ({offset, animated}) => this.list.scrollToOffset({offset, animated})
 }
+
+// // todo: how to properly type this?
+// const DraggablePopupList = inject('scrollY')((props: IProps<any>, ref) => {
+//   const list = useRef<FlatList<any>>(null)
+
+//   useImperativeHandle(ref, () => ({
+//     scrollToIndex: args => list.current!.scrollToIndex(args),
+//     scrollToOffset: ({offset, animated}) => list.current!.scrollToOffset({offset, animated}),
+//   }))
+
+//   const {headerInner, style, isActive, ...listProps} = props
+//   const Wrapper = isActive ? TouchThroughWrapper : View
+//   return (
+//     <Wrapper style={{width, height}}>
+//       <FlatList
+//         ref={list}
+//         bounces={false}
+//         keyboardDismissMode="on-drag"
+//         {...listProps}
+//         onScroll={Animated.event([{nativeEvent: {contentOffset: {y: props.scrollY!}}}])}
+//         scrollEventThrottle={60}
+//         style={[{flex: 1}, style]}
+//         ListHeaderComponent={<DraggablePopupListHeader {...props} />}
+//         showsVerticalScrollIndicator={false}
+//       />
+//     </Wrapper>
+//   )
+// })
+
+// export default forwardRef(DraggablePopupList.wrappedComponent as any)
 
 /**
  * This list header wrapper ensures that the user can "touch through" to the map behind the list
