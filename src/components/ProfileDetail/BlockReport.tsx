05cbaf3e3a7910c90c2432314c3bc19a1417d39b
Eric Kirkham <eric@southerneer.com>
2019-09-18 16:41:49 -0700

    hookify BlockReport

--- src/components/ProfileDetail/BlockReport.tsx
+++ src/components/ProfileDetail/BlockReport.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {TouchableOpacity, Alert, Image} from 'react-native'
-import {observer} from 'mobx-react'
+import {observer} from 'mobx-react-lite'
 import ActionSheet from 'react-native-actionsheet'
 import {Actions} from 'react-native-router-flux'
 
@@ -8,15 +8,14 @@ type Props = {
   profile: any
 }
 
-@observer
-class BlockReport extends React.Component<Props> {
-  actionSheet: any
+const BlockReport = observer(({profile}: Props) => {
+  const actionSheet = useRef<ActionSheet>(null)
 
-  onTap = (index: number) => {
+  const onTap = (index: number) => {
     if (index === 0) {
-      Actions.reportUser({userId: this.props.profile.id})
+      Actions.reportUser({userId: profile.id})
     } else if (index === 1) {
-      const {handle} = this.props.profile
+      const {handle} = profile
       Alert.alert(
         `Are you sure you want to block @${handle}?`,
         `If you’re friends, blocking @${handle} will unfriend him/her, and you will no longer be able to view each other's profiles and bots.`,
@@ -27,7 +26,7 @@ class BlockReport extends React.Component<Props> {
             style: 'destructive',
             onPress: async () => {
               // TODO: spinner
-              await this.props.profile.block()
+              await profile.block()
               Actions.pop()
             },
           },
@@ -36,33 +35,31 @@ class BlockReport extends React.Component<Props> {
     }
   }
 
-  render() {
-    if (this.props.profile && this.props.profile.isOwn === false) {
-      return (
-        <TouchableOpacity
-          onPress={() => this.actionSheet.show()}
-          style={{
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            width: 24,
-            height: 24,
-            marginRight: '4%',
-            justifyContent: 'center',
-          }}
-        >
-          <Image source={require('../../../images/ellipsis.png')} />
-          <ActionSheet
-            ref={o => (this.actionSheet = o)}
-            options={['Report', 'Block', 'Cancel']}
-            cancelButtonIndex={2}
-            destructiveButtonIndex={1}
-            onPress={this.onTap}
-          />
-        </TouchableOpacity>
-      )
-    } else return null
-  }
-}
+  if (profile && profile.isOwn === false) {
+    return (
+      <TouchableOpacity
+        onPress={() => actionSheet.current!.show()}
+        style={{
+          position: 'absolute',
+          right: 0,
+          top: 0,
+          width: 24,
+          height: 24,
+          marginRight: '4%',
+          justifyContent: 'center',
+        }}
+      >
+        <Image source={require('../../../images/ellipsis.png')} />
+        <ActionSheet
+          ref={actionSheet}
+          options={['Report', 'Block', 'Cancel']}
+          cancelButtonIndex={2}
+          destructiveButtonIndex={1}
+          onPress={onTap}
+        />
+      </TouchableOpacity>
+    )
+  } else return null
+})
 
 export default BlockReport
