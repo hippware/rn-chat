a8e6837aec2de2a2d2f9bf2f1312cdebafda0f73
Beng Tan <bengtan@users.noreply.github.com>
2019-09-18 11:46:22 +0800

    Give activeBots some time to update itself.

--- third-party/wocky-client/test/geofence.test.ts
+++ third-party/wocky-client/test/geofence.test.ts
@@ -150,7 +150,7 @@ describe('Geofence', () => {
     await enterBot(user1)
     await enterBot(user2)
     await waitFor(() => bot.visitorsSize === 2, 'bot visitors to increment')
-    expect(user1.activeBots.length).toEqual(2)
+    await waitFor(() => user1.activeBots.length === 2, 'active bots to increment')
     // expect(user1.activeBots[0].title).toEqual('Test bot')
     // expect(user1.activeBots[1].title).toEqual('Test bot2')
   })
