0aae7437bfb1623cf3dc2cce48a3a790efb417a5
Eric Kirkham <eric@southerneer.com>
2019-09-25 15:36:14 -0700

    convert inject('homeStore')

--- src/components/BottomPopup.tsx
+++ src/components/BottomPopup.tsx
@@ -1,32 +1,29 @@
 import React from 'react'
 import {View, Image, StyleSheet} from 'react-native'
-import {IHomeStore} from 'src/store/HomeStore'
-import {inject, observer} from 'mobx-react'
+import {useHomeStore} from 'src/utils/injectors'
 
 type Props = {
   children: any
-  homeStore?: IHomeStore
 }
 
-const BottomPopup = inject('homeStore')(
-  observer(({homeStore, children}: Props) => {
-    // TODO: style this with border radius and shadow rather than an image. Allows setting background color to white
-    return (
-      <View style={{paddingTop: 50}}>
-        <Image
-          style={styles.absolute}
-          source={
-            homeStore!.mapType === 'hybrid'
-              ? require('../../images/bottomPopupDarkShadow.png')
-              : require('../../images/bottomPopup.png')
-          }
-          resizeMode="stretch"
-        />
-        {children}
-      </View>
-    )
-  })
-)
+const BottomPopup = ({children}: Props) => {
+  const {mapType} = useHomeStore()
+  // TODO: style this with border radius and shadow rather than an image. Allows setting background color to white
+  return (
+    <View style={{paddingTop: 50}}>
+      <Image
+        style={styles.absolute}
+        source={
+          mapType === 'hybrid'
+            ? require('../../images/bottomPopupDarkShadow.png')
+            : require('../../images/bottomPopup.png')
+        }
+        resizeMode="stretch"
+      />
+      {children}
+    </View>
+  )
+}
 export default BottomPopup
 
 const styles = StyleSheet.create({
