f0e40749681f765101b945025547a0160a502355
Eric Kirkham <eric@southerneer.com>
2019-09-30 15:18:49 -0700

    simplify typing in injectors

--- src/utils/injectors.ts
+++ src/utils/injectors.ts
@@ -4,25 +4,41 @@ import {IWocky} from 'wocky-client'
 import {ILocationStore} from 'src/store/LocationStore'
 import {IHomeStore} from 'src/store/HomeStore'
 import {Analytics} from './analytics'
+import {IFirebaseStore} from 'src/store/FirebaseStore'
 
 // https://mobx-react.js.org/recipes-migration
 
-export function useWocky(): IWocky {
-  const {wocky}: {wocky: IWocky} = React.useContext(MobXProviderContext)
+type AllStores = {
+  wocky: IWocky
+  locationStore: ILocationStore
+  homeStore: IHomeStore
+  analytics: Analytics
+  firebaseStore: IFirebaseStore
+}
+
+const getStores = (): AllStores => React.useContext(MobXProviderContext)
+
+export function useWocky() {
+  const {wocky} = getStores()
   return useObserver(() => wocky)
 }
 
-export function useLocationStore(): ILocationStore {
-  const {locationStore}: {locationStore: ILocationStore} = React.useContext(MobXProviderContext)
+export function useLocationStore() {
+  const {locationStore} = getStores()
   return useObserver(() => locationStore)
 }
 
-export function useHomeStore(): IHomeStore {
-  const {homeStore}: {homeStore: IHomeStore} = React.useContext(MobXProviderContext)
+export function useHomeStore() {
+  const {homeStore} = getStores()
   return useObserver(() => homeStore)
 }
 
-export function useAnalytics(): Analytics {
-  const {analytics}: {analytics: Analytics} = React.useContext(MobXProviderContext)
+export function useAnalytics() {
+  const {analytics} = getStores()
   return analytics
+}
+
+export function useFirebaseStore() {
+  const {firebaseStore} = getStores()
+  return firebaseStore
 }
