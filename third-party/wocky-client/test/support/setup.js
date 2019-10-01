ce4c3751362ed1a35f8e1a40354d88758cec8034
Eric Kirkham <eric@southerneer.com>
2019-10-01 13:19:28 -0700

    remove other unnecessary dependencies (from old test setup)

--- third-party/wocky-client/test/support/setup.js
+++ third-party/wocky-client/test/support/setup.js
@@ -1,10 +1,3 @@
 process.env.NODE_ENV = 'test'
 
-global.window = global
-global.XMLHttpRequest = require('./xmlhttprequest').XMLHttpRequest
-// global.XMLHttpRequest = require('xhr2');
 global.WebSocket = require('websocket').w3cwebsocket
-global.fetch = require('node-fetch')
-global.Promise = require('promise')
-
-global.__DEV__ = true
