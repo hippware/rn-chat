2da58ac1301166cbc218f91b228f3b1e48e122a3
Eric Kirkham <eric@southerneer.com>
2019-10-01 13:48:10 -0700

    inject SelectChatUser

--- src/components/Chats/SelectChatUser.tsx
+++ src/components/Chats/SelectChatUser.tsx
@@ -2,45 +2,39 @@ import React, {useEffect} from 'react'
 import Screen from '../Screen'
 import {ProfileList} from '../people-lists'
 import {Actions} from 'react-native-router-flux'
-import {inject} from 'mobx-react'
 import {observer} from 'mobx-react-lite'
-import {IWocky} from 'wocky-client'
-import {ISearchStore} from '../../store/SearchStore'
 import SearchBar from '../people-lists/SearchBar'
 import {Separator} from '../common'
 import {gray} from '../../constants/colors'
+import {useWocky, useSearchStore} from 'src/utils/injectors'
 
-type Props = {
-  wocky: IWocky
-  searchStore?: ISearchStore
-}
+const SelectChatUser = observer(() => {
+  const {profile} = useWocky()
+  const searchStore = useSearchStore()
 
-const SelectChatUser = inject('wocky', 'searchStore')(
-  observer(({wocky, searchStore}: Props) => {
-    useEffect(() => {
-      searchStore!.localResult.setList(wocky.profile!.friends.list.map(f => ({profile: f.id})))
-    }, [])
+  useEffect(() => {
+    searchStore!.localResult.setList(profile!.friends.list.map(f => ({profile: f.id})))
+  }, [])
 
-    return (
-      <Screen>
-        <SearchBar
-          autoCorrect={false}
-          autoCapitalize="none"
-          onChangeText={text => searchStore!.localResult.setFilter(text)}
-          value={searchStore!.localResult.filter}
-          placeholder="Search name or username"
-        />
-        <Separator width={1} backgroundColor={gray(172)} />
-        <ProfileList
-          selection={searchStore!.localResult}
-          onSelect={profile => {
-            Actions.pop()
-            Actions.chat({item: profile.id})
-          }}
-        />
-      </Screen>
-    )
-  })
-)
+  return (
+    <Screen>
+      <SearchBar
+        autoCorrect={false}
+        autoCapitalize="none"
+        onChangeText={text => searchStore!.localResult.setFilter(text)}
+        value={searchStore!.localResult.filter}
+        placeholder="Search name or username"
+      />
+      <Separator width={1} backgroundColor={gray(172)} />
+      <ProfileList
+        selection={searchStore!.localResult}
+        onSelect={prof => {
+          Actions.pop()
+          Actions.chat({item: prof.id})
+        }}
+      />
+    </Screen>
+  )
+})
 
 export default SelectChatUser
