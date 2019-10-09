3ba1b20d53c04f1442cd09d270ed64ee370e14c1
Eric Kirkham <eric@southerneer.com>
2019-10-08 17:28:09 -0700

    initial HomeBanner storybook

--- storybook/stories/HomeBannerStory.tsx
+++ storybook/stories/HomeBannerStory.tsx
@@ -3,14 +3,41 @@ import {Provider} from 'mobx-react'
 import HomeBanner from 'src/components/Home/HomeBanner'
 
 const stores = {
-  wocky: {} as any,
+  wocky: {
+    connected: true,
+    profile: {
+      locationSharers: {
+        list: [
+          {
+            id: '1',
+            sharedWith: {
+              avatar: {
+                thumbnail: {url: 'https://www.placecage.com/c/300/300'},
+              },
+              handle: 'nick_cage',
+            },
+          },
+          {
+            id: '2',
+            sharedWith: {
+              avatar: {
+                thumbnail: {url: 'https://www.fillmurray.com/300/300'},
+              },
+              handle: 'bill_murray',
+            },
+          },
+        ],
+      },
+      hidden: {
+        enabled: false,
+      },
+    },
+    activeBots: [],
+  } as any,
   analytics: {
     track: () => null,
   },
-  homeStore: {
-    select: () => {},
-    setFocusedLocation: () => {},
-  },
+  homeStore: {},
   navStore: {},
   locationStore: {
     alwaysOn: true,
