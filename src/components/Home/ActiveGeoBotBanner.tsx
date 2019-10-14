3116bb9f6c9d307210a96025ff750b7cfb75ace9
Eric Kirkham <eric@southerneer.com>
2019-10-14 16:52:09 -0700

    remove current location button. for #4213

--- src/components/Home/ActiveGeoBotBanner.tsx
+++ src/components/Home/ActiveGeoBotBanner.tsx
@@ -14,7 +14,7 @@ import {colors} from '../../constants'
 import ActiveGeofenceBot from './ActiveGeofenceBot'
 import HeaderLocationOverlay from './HeaderLocationOverlay'
 import ActiveBannerPlaceholder from './ActiveBannerPlaceholder'
-import {IBot, IWocky} from 'wocky-client'
+import {IBot, IWocky, IOwnProfile} from 'wocky-client'
 import {analyticsGeoWidgetTap} from '../../utils/analytics'
 import {k, isIphoneX, isIphone, minHeight} from '../Global'
 import {Actions} from 'react-native-router-flux'
@@ -46,7 +46,7 @@ const ActiveGeoBotBanner = inject('wocky', 'analytics', 'homeStore', 'navStore')
 
     const {profile, activeBots} = wocky!
     const bannerItems = profile
-      ? [...profile.locationSharers.list.slice(), ...activeBots.slice()]
+      ? [profile, ...profile.locationSharers.list.slice(), ...activeBots.slice()]
       : []
 
     useEffect(() => {
@@ -56,12 +56,14 @@ const ActiveGeoBotBanner = inject('wocky', 'analytics', 'homeStore', 'navStore')
       }).start()
     }, [enabled])
 
-    const renderActiveBot = ({item}: {item: IBot | ILocationShare}) =>
+    const renderBannerItem = ({item}: {item: IBot | ILocationShare | IOwnProfile}) =>
       getType(item).name === 'Bot' ? (
         <ActiveGeofenceBot bot={item as IBot} outerStyle={styles.outer} innerStyle={styles.inner} />
       ) : (
         <ActiveLocationSharer
-          sharer={item as ILocationShare}
+          profile={
+            (item as any).email ? (item as IOwnProfile) : (item as ILocationShare).sharedWith
+          }
           outerStyle={styles.outer}
           innerStyle={styles.inner}
         />
@@ -93,7 +95,7 @@ const ActiveGeoBotBanner = inject('wocky', 'analytics', 'homeStore', 'navStore')
             data={bannerItems}
             horizontal
             keyExtractor={item => item.id}
-            renderItem={renderActiveBot}
+            renderItem={renderBannerItem}
             showsHorizontalScrollIndicator={false}
             ListEmptyComponent={<ActiveBannerPlaceholder />}
             style={{paddingLeft: 8 * k}}
