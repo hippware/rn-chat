62269ea138e576c14f4ef9b1516c1398f363c1b6
Beng Tan <bengtan@users.noreply.github.com>
2019-10-24 11:44:49 +0800

    Enable debugging features for PROD.

--- src/store/LocationStore.ts
+++ src/store/LocationStore.ts
@@ -125,9 +125,7 @@ const LocationStore = types
         config.distanceFilter = 10
       }
 
-      if (__DEV__ || settings.isStaging) {
-        config.logLevel = BackgroundGeolocation.LOG_LEVEL_VERBOSE
-      }
+      config.logLevel = BackgroundGeolocation.LOG_LEVEL_VERBOSE
 
       if (reset) {
         yield BackgroundGeolocation.reset(config)
