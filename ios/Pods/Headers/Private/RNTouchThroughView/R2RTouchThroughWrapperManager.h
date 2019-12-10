48eb0f88b3efe464629015d4c4b264bc7dc4dfeb
Eric Kirkham <eric@southerneer.com>
2019-12-10 11:54:24 -0800

    remove rn-touch-through-view

--- ios/Pods/Headers/Private/RNTouchThroughView/R2RTouchThroughWrapperManager.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-touch-through-view/ios/R2RTouchThroughWrapperManager.h
--- ios/Pods/Headers/Public/RNTouchThroughView/R2RTouchThroughViewManager.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-touch-through-view/ios/R2RTouchThroughViewManager.h
