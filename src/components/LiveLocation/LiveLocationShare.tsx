6021b685c452d683c9cf4e3191510350e90db0d8
Eric Kirkham <eric@southerneer.com>
2020-01-09 13:28:52 -0800

    skip live location duration selection screen

--- src/components/LiveLocation/LiveLocationShare.tsx
+++ src/components/LiveLocation/LiveLocationShare.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react'
 import {inject} from 'mobx-react'
 import {observer} from 'mobx-react'
-import {Actions} from 'react-native-router-flux'
 import Screen from '../Screen'
 import {RText, BottomButton} from '../common'
 import FriendMultiSelect from '../people-lists/FriendMultiSelect'
@@ -9,17 +8,21 @@ import {IWocky} from 'wocky-client'
 import {ISearchStore} from '../../store/SearchStore'
 import {View} from 'react-native'
 import {minHeight} from '../Global'
+import {share, UNTIL_OFF} from './LiveLocationCompose'
+import {ILocationStore} from '../../store/LocationStore'
 
 type Props = {
   wocky: IWocky
   searchStore?: ISearchStore
+  locationStore?: ILocationStore
 }
 
 const LiveLocationShare = inject(
   'wocky',
-  'searchStore'
+  'searchStore',
+  'locationStore'
 )(
-  observer(({wocky, searchStore}: Props) => {
+  observer(({wocky, searchStore, locationStore}: Props) => {
     useEffect(() => {
       searchStore!.localResult.setList(wocky.profile!.sortedFriends.map(f => ({profile: f})))
     }, [])
@@ -34,8 +37,9 @@ const LiveLocationShare = inject(
         <BottomButton
           isDisabled={!selected}
           onPress={() => {
-            Actions.pop()
-            Actions.liveLocationCompose()
+            // Actions.pop()
+            // Actions.liveLocationCompose()
+            share(UNTIL_OFF, null, wocky, selection, locationStore!.location)
           }}
         >
           <RText size={15} color="white">
